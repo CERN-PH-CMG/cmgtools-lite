fa729ba4eaa7dd870d7bff305fd47d3677488624
Jan Steggemann <jan.steggemann@cern.ch>
2016-01-26 14:02:08 +0100

    MVA MET migration

--- H2TauTau/python/objects/mvaMetInputs_cff.py
+++ H2TauTau/python/objects/mvaMetInputs_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 from RecoMET.METPUSubtraction.mvaPFMET_cff import puJetIdForPFMVAMEt, calibratedAK4PFJetsForPFMVAMEt
-
+from JetMETCorrections.Configuration.JetCorrectors_cff import ak4PFL1FastL2L3Corrector, ak4PFL1FastjetCorrector, ak4PFL2RelativeCorrector, ak4PFL3AbsoluteCorrector
 
 puJetIdForPFMVAMEt.jec =  cms.string('AK4PF')
 #process.puJetIdForPFMVAMEt.jets = cms.InputTag("ak4PFJets")
@@ -9,6 +9,10 @@ puJetIdForPFMVAMEt.vertexes = cms.InputTag("offlineSlimmedPrimaryVertices")
 puJetIdForPFMVAMEt.rho = cms.InputTag("fixedGridRhoFastjetAll")
 
 mvaMetInputSequence = cms.Sequence(
-    calibratedAK4PFJetsForPFMVAMEt*
+    ak4PFL1FastjetCorrector *
+    ak4PFL2RelativeCorrector *
+    ak4PFL3AbsoluteCorrector *
+    ak4PFL1FastL2L3Corrector *
+    calibratedAK4PFJetsForPFMVAMEt *
     puJetIdForPFMVAMEt
   )
