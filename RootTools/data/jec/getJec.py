20086902569d87f229c293ff7f4f0ec3e2191df4
Giovanni <gpetruc@gmail.com>
2016-02-04 13:53:56 +0100

    JECs used in 76X production

--- RootTools/data/jec/getJec.py
+++ RootTools/data/jec/getJec.py
@@ -7,12 +7,15 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.GlobalTag.globaltag = 'PHYS14_25_V2::All'  
 # define your favorite global tag (newer GTs in condDBv2)
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag as customiseGlobalTag
-process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = '74X_mcRun2_asymptotic_v2')
+process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = '76X_mcRun2_asymptotic_v12')
+#process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = '76X_dataRun2_v15')
 process.GlobalTag.connect   = 'frontier://FrontierProd/CMS_CONDITIONS'
 process.GlobalTag.pfnPrefix = cms.untracked.string('frontier://FrontierProd/')
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 process.source = cms.Source("EmptySource")
+#process.source.firstRun = cms.untracked.uint32(251883)
+#process.source.firstRun = cms.untracked.uint32(260577)
 process.read = cms.EDAnalyzer('JetCorrectorDBReader',  
     # below is the communication to the database 
     payloadName    = cms.untracked.string('AK4PFchs'),
