7263a59e640893aea4f342c655e32ca577b51f8d
Jan Steggemann <jan.steggemann@cern.ch>
2016-03-08 17:54:07 +0100

    Update for new MVA MET training/latest weight files

--- H2TauTau/python/proto/weights/ScaleFactor.py
+++ H2TauTau/python/proto/weights/ScaleFactor.py
@@ -63,37 +63,49 @@ class ScaleFactor(object):
     def getEfficiencyMC(self, pt, eta):
         return self.getEfficiency(pt, eta, self.eff_mc)
 
-    def getEfficiency(self, pt, eta, eff_dict):
+
+    def findEtaLabel(self, eta, eff_dict):
         eta = abs(eta)
         binNumber = self.etaBinsH.GetXaxis().FindFixBin(eta)
         label = self.etaBinsH.GetXaxis().GetBinLabel(binNumber)
         if label not in eff_dict:
             raise RuntimeError('ERROR in ScaleFactor::get_EfficiencyData(double pt, double eta) from LepEffInterface/src/ScaleFactor.cc : no object corresponding to eta label', label)
+        return label
 
-        g_eff = eff_dict[label]
+    def findPtBin(self, pt, g_eff):
         nPoints = g_eff.GetN()
         ptMax = g_eff.GetX()[nPoints-1] + g_eff.GetErrorXhigh(nPoints-1)
         ptMin = g_eff.GetX()[0] = g_eff.GetErrorXlow(0)
 
         if pt >= ptMax:
-            return g_eff.GetY()[nPoints-1]
+            return nPoints
         elif pt < ptMin:
             print 'WARNING in ScaleFactor::get_EfficiencyData(double pt, double eta) from LepEffInterface/src/ScaleFactor.cc: pT too low (pt = ', pt, '), min value is ', ptMin, '. Returned efficiency = 1. Weight will be 1.'
-            return 1.
+            return -99
         else:
-            ptBin = g_eff.GetXaxis().FindFixBin(pt)
-            eff = g_eff.GetY()[ptBin-1]
-            if eff > 1.:
-                print 'Warning: Efficiency larger 1'
-            elif eff < 0.:
-                print 'Warning: Efficiency negative'
-            return eff
+            return g_eff.GetXaxis().FindFixBin(pt)
+
+    def getEfficiency(self, pt, eta, eff_dict):
+        label = self.findEtaLabel(eta, eff_dict)
+        
+        g_eff = eff_dict[label]
+        ptBin = self.findPtBin(pt, g_eff)
+
+        if ptBin == -99:
+            return 1.
+
+        eff = g_eff.GetY()[ptBin-1]
+        if eff > 1.:
+            print 'Warning: Efficiency larger 1'
+        elif eff < 0.:
+            print 'Warning: Efficiency negative'
+        return eff
 
         return 1.
 
 if __name__ == '__main__':
-    sf = ScaleFactor('$CMSSW_BASE/src/CMGTools/H2TauTau/data/Muon_SingleMu_eff.root')
-    for pt, eta in [(100., 1.5), (50., 0.2), (17., 0.05), (1000., 2.04)]:
+    sf = ScaleFactor('$CMSSW_BASE/src/CMGTools/H2TauTau/data/Electron_Ele23_fall15.root')
+    for pt, eta in [(29.3577, 1.4845), (50., 0.2), (17., 0.05), (1000., 2.04)]:
         print 'Eff data', sf.getEfficiencyData(pt, eta)
         print 'Eff MC', sf.getEfficiencyMC(pt, eta)
         print 'SF', sf.getScaleFactor(pt, eta)
