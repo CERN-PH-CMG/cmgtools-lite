3f4d93a399eb68049c86482f87bb1be177a96b26
Jan Steggemann <jan.steggemann@cern.ch>
2016-05-24 13:48:50 +0200

    Add ML tree preparation/scikit-learn evaluation

--- H2TauTau/plotting/mt/add_mva_values_multicat.py
+++ H2TauTau/plotting/mt/add_mva_values_multicat.py
@@ -36,10 +36,16 @@ train_vars = [
     'svfit_mass'
 ]
 
-mva_name = 'mva'
+# clf0 = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_0.pkl')
+# clf1 = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_1.pkl')
 
-clf0 = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_dyh_0.pkl')
-clf1 = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_dyh_1.pkl')
+clf0_0jet = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_0_0jet.pkl')
+clf1_0jet = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_1_0jet.pkl')
+clf0_1jet = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_0_1jet.pkl')
+clf1_1jet = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_1_1jet.pkl')
+clf0_vbf = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_0_vbf.pkl')
+clf1_vbf = joblib.load('/afs/cern.ch/work/s/steggema/GradientBoostingClassifier_clf_1_vbf.pkl')
+
 
 out_dict = {}
 
@@ -63,17 +69,24 @@ for sample in all_samples:
     trees_done.append(file_name)
 
 
-    file_out_name = file_name.replace('TauMuSVFitMC', 'TauMuSVFitMVA').replace('TauMuSVFitTESUp', 'TauMuSVFitTESUpMVA').replace('TauMuSVFitTESDown', 'TauMuSVFitTESDownMVA')
+    file_out_name = file_name.replace('TauMuSVFitMC', 'TauMuSVFitMultiSepMVA').replace('TauMuSVFitTESUp', 'TauMuSVFitTESUpMultiSepMVA').replace('TauMuSVFitTESDown', 'TauMuSVFitTESDownMultiSepMVA')
 
     ensure_dir(file_out_name.replace('tree.root', ''))
     file_out = TFile(file_out_name, 'RECREATE')
 
 
-    mva_val = array('f', [0.])
+    mva0_val = array('f', [0.])
+    mva1_val = array('f', [0.])
+    mva2_val = array('f', [0.])
 
     tree_out = TTree('tree', 'tree')
 
-    tree_out.Branch(mva_name, mva_val, mva_name+'/F')
+    mva0_name = 'mva0'
+    tree_out.Branch(mva0_name, mva0_val, mva0_name+'/F')
+    mva1_name = 'mva1'
+    tree_out.Branch(mva1_name, mva1_val, mva1_name+'/F')
+    mva2_name = 'mva2'
+    tree_out.Branch(mva2_name, mva2_val, mva2_name+'/F')
 
     ave_mva = 0.
 
@@ -85,15 +98,27 @@ for sample in all_samples:
         split_var = event.delta_phi_l1_l2
 
         if int(split_var * 1000) % 2 == 0:
-            clf = clf0
+            clf = clf0_0jet
+            if event.n_jets > 0.5:
+                if event.vbf_mjj > 500. and event.vbf_deta > 3.5:
+                    clf = clf0_vbf
+                else:
+                    clf = clf0_1jet
         else:
-            clf = clf1
+            clf = clf1_0jet
+            if event.n_jets > 0.5:
+                if event.vbf_mjj > 500. and event.vbf_deta > 3.5:
+                    clf = clf1_vbf
+                else:
+                    clf = clf1_1jet
 
         inputs = []
         for var in train_vars:
             inputs.append(getattr(event, var))
         mva = clf.predict_proba(np.array(inputs).reshape(1, -1))
-        mva_val[0] = mva[0][1]
+        mva0_val[0] = mva[0][0]
+        mva1_val[0] = mva[0][1]
+        mva2_val[0] = mva[0][2]
         tree_out.Fill()
 
         ave_mva += mva[0]
