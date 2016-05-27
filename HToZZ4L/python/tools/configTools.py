853f0ea4e3137fea9a510e4e21e46cbdb9d28701
Giovanni <gpetruc@gmail.com>
2016-05-27 16:46:36 +0200

    HZZ4L: Option to switch off MEs

--- HToZZ4L/python/tools/configTools.py
+++ HToZZ4L/python/tools/configTools.py
@@ -107,6 +107,14 @@ def doTest1(comp, url=None, sequence=None, cache=False):
     if not getHeppyOption('fetch'): setHeppyOption('nofetch')
     return [ comp ]
 
+def switchOffMEs(sequence, verbose=False):
+    import CMGTools.HToZZ4L.analyzers.FourLeptonAnalyzerBase
+    base = CMGTools.HToZZ4L.analyzers.FourLeptonAnalyzerBase.FourLeptonAnalyzerBase
+    for item in sequence:
+        if issubclass(item.class_object, base):
+            if verbose: print "Switch off MEs for %s (%s)" % (item.name, item.class_object)
+            item.doMEs = False
+
 def doTestN(test, selectedComponents):
     if test == '2':
         for comp in selectedComponents:
