0ef6646442218c73c835b6e0d6ed83a290a27bd9
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2016-06-09 02:33:56 +0200

    updates for the synch on several CRs

--- MonoXAnalysis/python/plotter/monojet/synch.py
+++ MonoXAnalysis/python/plotter/monojet/synch.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
 
     parser = OptionParser(usage=usage)
     parser.add_option("-r", "--region", dest="region", default='signal', help='Find the yields for this phase space')
-    parser.add_option("-c", "--category", dest="category", default=None, help='Add the categorization for monoj or monov')
+    parser.add_option("-c", "--category", dest="category", default='', help='Add the categorization for monoj or monov')
     parser.add_option("-d", "--dry-run", dest="dryrun", action="store_true", default=False, help='Do not run the commands, just print them')
     (options, args) = parser.parse_args()
 
     #T='/afs/cern.ch/work/e/emanuele/TREES/SYNCH_80X/'
-    T='/afs/cern.ch/work/e/emanuele/monox/heppy/CMSSW_8_0_5/src/CMGTools/MonoXAnalysis/cfg/SynchTrees/'
+    T='/afs/cern.ch/work/e/emanuele/monox/heppy/CMSSW_8_0_5/src/CMGTools/MonoXAnalysis/cfg/SynchTreesV2/'
 
     coreopt = '-P '+T+' --s2v -j 6 -l 1.0'
     corey = 'mcAnalysis.py ' + coreopt + ' -G '
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     runy = corey + 'monojet/mca-80X-sync.txt --s2v -u '
     cuts = {
-        'signal': 'monojet/monojet_synch.txt',
+        'signal': 'monojet/monojet_twiki.txt',
         'zmumu': 'monojet/zmumu_twiki.txt',
         'wmunu': 'monojet/wmunu_twiki.txt',
         'zee': 'monojet/zee_twiki.txt',
@@ -31,9 +31,15 @@ if __name__ == "__main__":
         }
 
     # categorization
-    monov_cut="-A dphijm monoV 'nFatJetClean>0 && FatJetClean1_pt>250 && abs(FatJetClean1_eta)<2.4 && abs(FatJetClean1_prunedMass-85)<20 && FatJetClean1_tau2/FatJetClean1_tau1<0.6"
-    monoj_cut="-A dphijm monoJ '!(nFatJetClean>0 && FatJetClean1_pt>250 && abs(FatJetClean1_eta)<2.4 && abs(FatJetClean1_prunedMass-85)<20 && FatJetClean1_tau2/FatJetClean1_tau1<0.6)"
-    if options.category != None: cat_cut = monoj_cut if options.category=='monoj' else 'monov'
+    monov_cut="-A recoil monoV 'nFatJetClean>0 && FatJetClean1_pt>250 && abs(FatJetClean1_eta)<2.4 && abs(FatJetClean1_prunedMass-85)<20 && FatJetClean1_tau2/FatJetClean1_tau1<0.6' "
+    monoj_cut="-A recoil monoJ '!(nFatJetClean>0 && FatJetClean1_pt>250 && abs(FatJetClean1_eta)<2.4 && abs(FatJetClean1_prunedMass-85)<20 && FatJetClean1_tau2/FatJetClean1_tau1<0.6)' "
+    # single cuts for monov
+    monov_ak8jetAcc="-A recoil ak8jetAcc 'nFatJetClean>0 && FatJetClean1_pt>250 && abs(FatJetClean1_eta)<2.4' "
+    monov_ak8jetT2T1="-A recoil ak8jetT2T1 'FatJetClean1_tau2/FatJetClean1_tau1<0.6' "
+    monov_ak8jetMpruned="-A recoil ak8jetMpruned 'abs(FatJetClean1_prunedMass-85)<20' "
+    monov_tightMET="-A recoil tightMET 'metNoMu_pt>250' "
+    print "==> Yields for category: ",options.category
+    if options.category != '': cat_cut = monov_ak8jetAcc+monov_ak8jetT2T1+monov_ak8jetMpruned+monov_tightMET if options.category=='monov' else monoj_cut
     else: cat_cut = ''
 
     if options.region not in cuts: raise RuntimeError, "Region "+options.region+" not in the foreseen ones: "+cuts
