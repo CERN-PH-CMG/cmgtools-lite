6b1bc4463b5e58d4d478f26702138890353202c1
claudia seitz <clseitz.physics@gmail.com>
2016-06-28 16:56:19 +0200

    comit to merge

--- SUSYAnalysis/cfg/run_susySingleLepton_v2_cfg.py
+++ SUSYAnalysis/cfg/run_susySingleLepton_v2_cfg.py
@@ -204,8 +204,8 @@ genAna.allGenTaus = True
 isData = True # default, but will be overwritten below
 
 #sample = 'MC'
-sample = 'data'
-#sample = 'Signal'
+#sample = 'data'
+sample = 'Signal'
 test = 1
 
 if sample == "MC":
@@ -350,14 +350,15 @@ elif sample == "data":
   # central samples
   from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import *
 
-  selectedComponents = [SingleElectron_Run2016B_PromptReco_v2, SingleMuon_Run2016B_PromptReco_v2] #, JetHT_Run2016B_PromptReco_v2_HT800Only]
+#  selectedComponents = [SingleElectron_Run2016B_PromptReco_v2, SingleMuon_Run2016B_PromptReco_v2] #, JetHT_Run2016B_PromptReco_v2_HT800Only]
+  selectedComponents = [SingleElectron_Run2016B_PromptReco_v2]
 
   if test!=0 and jsonAna in susyCoreSequence: susyCoreSequence.remove(jsonAna)
   if test==1:
     comp = SingleElectron_Run2016B_PromptReco_v2 #JetHT_Run2016B_PromptReco_v2_HT800Only
 #SingleElectron_Run2016B_PromptReco_v2
 #    comp.files = comp.files[:1]
-    comp.files = comp.files[:1]
+    comp.files = comp.files[10:11]
     #comp.files = comp.files[10:11]
     selectedComponents = [comp]
     comp.splitFactor = 1
