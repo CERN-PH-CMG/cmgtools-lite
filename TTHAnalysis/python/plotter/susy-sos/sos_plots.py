78d5e48d1df18729d8ca41791bc42704b6309d16
botta <cristina.botta@cern.ch>
2016-06-30 15:39:24 +0200

    as in FULL STATUS SOS

--- TTHAnalysis/python/plotter/susy-sos/sos_plots.py
+++ TTHAnalysis/python/plotter/susy-sos/sos_plots.py
@@ -73,6 +73,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-lowmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_EWKino'])             
         if '_ewk10_met200_mm' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_315,T2ttDeg_330 -E ^pt5sublep -E ^MT -E ^mm -E ^highMET")
@@ -84,6 +85,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_EWKino'])
         if '_ewk10_met200_ee' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_315,T2ttDeg_330 -E ^pt5sublep -E ^MT -E ^ee -E ^highMET")
@@ -95,6 +97,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_EWKino'])    
         if '_ewk20_met125_mm' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,T2ttDeg_300,T2ttDeg_315,T2ttDeg_330 -E ^pt5sublep -E ^MT -E ^mm -E ^upperMET")
@@ -106,6 +109,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-lowmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_EWKino'])
         if '_ewk20_met200_mm' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,T2ttDeg_300,T2ttDeg_315,T2ttDeg_330 -E ^pt5sublep -E ^MT -E ^mm -E ^highMET")
@@ -117,6 +121,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_EWKino'])
         if '_ewk20_met200_ee' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,T2ttDeg_300,T2ttDeg_315,T2ttDeg_330 -E ^pt5sublep -E ^MT -E ^ee -E ^highMET")
@@ -128,6 +133,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_EWKino'])
         if '_stop20_met125_mm' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_315 -E ^pt5sublep -E ^mm -E ^upperMET")
@@ -139,6 +145,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-lowmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])
         if '_stop20_met200_mm' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_315 -E ^mm -E ^highMET")
@@ -150,6 +157,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])
         if '_stop20_met200_ee' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_315 -E ^ee -E ^highMET")
@@ -161,6 +169,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])
         if '_stop20_met200_em' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_315 -E ^em -E ^highMET")
@@ -172,6 +181,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])  
         if '_stop35_met125_mm' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_330 -E ^pt5sublep -E ^mm -E ^upperMET")
@@ -183,6 +193,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-lowmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])
         if '_stop35_met200_mm' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_330 -E ^mm -E ^highMET")
@@ -194,6 +205,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])
         if '_stop35_met200_ee' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_330 -E ^ee -E ^highMET")
@@ -205,6 +217,7 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])
         if '_stop35_met200_em' in torun: 
             x = add(x,"--xp TChiNeuWZ_95,TChiNeuWZ_90,TChiNeuWZ_80,T2ttDeg_300,T2ttDeg_330 -E ^em -E ^highMET")
@@ -216,10 +229,11 @@ if __name__ == '__main__':
                     x = x.replace('mca-2los-mc.txt','mca-2los-mc-syst-dy.txt')
             if '_ddbkg' in torun: 
                 x = x.replace('mca-2los-mc.txt','mca-2los-mc-frdata.txt')
+                x = x.replace('mcc-sf1.txt','mcc-sf-highmet.txt')
             runIt(x,'%s/all'%torun,['SR_bins_stop'])      
    
 
- 
+
     if '2los_CR_FF_vars' in torun:
         x = base('2los')
         x = add(x,"--noStackSig --showIndivSigs --xp TChiNeuWZ_95")
@@ -227,7 +241,14 @@ if __name__ == '__main__':
         if '_data' in torun: 
             x = x.replace('mca-2los-mc.txt','mca-2los-mcdata.txt')
             x = add(x,"--showRatio --maxRatioRange 0 3") #--showMCError
+        if '_met200' in torun:             
+            x = add(x,"-E ^highMET -X ^triggerAll -E ^triggerMET -I ^TT ")
+            x = x.replace('-l 5.0','-l 4.0')
+        if '_met125' in torun:             
+            x = add(x,"-E ^mm -E ^upperMET -X  -E ^runRange -X ^triggerAll -E ^triggerDoubleMuMET -I ^TT ")
+            x = x.replace('-l 5.0','-l 1.4')  
         runIt(x,'%s/all'%torun,[],['SR_bins_EWKino','SR_bins_stop'])
+
 
 
     if '2los_FR_Closure_vars' in torun:
@@ -238,6 +259,7 @@ if __name__ == '__main__':
         runIt(x,'%s/all'%torun)
 
     
+
     if '2los_CR_DY_vars' in torun:
         x = base('2los')
         x = add(x,"--noStackSig --showIndivSigs --xp TChiNeuWZ_95")
@@ -267,7 +289,7 @@ if __name__ == '__main__':
 
     if '2los_CR_TT_vars' in torun:
         x = base('2los')
-        x = add(x,"--noStackSig --showIndivSigs --xp TChiNeuWZ_95")
+        x = add(x,"--noStackSig --showIndivSigs --xp TChiNeuWZ_95 --xp TChiNeuWZ_90")
         if '_notrigger' in torun: x = add(x,'-X ^triggerAll')
         if '_data' in torun: x = add(x,"--showRatio --maxRatioRange 0 3") #--showMCError
         if '_met200' in torun:             
@@ -314,9 +336,19 @@ if __name__ == '__main__':
 #-X ^HT -X ^Upsilon_veto -R ^ISRjet noIDISRjet 'Jet1_pt > 25 && fabs(Jet1_eta)<2.4' -R METovHT relaxMETovHT '(met_pt/(htJet25-LepGood1_pt-LepGood2_pt))>(2/3)'
 
 
-
-
-
+    # if '2los_Validation_vars' in torun:
+    #     x = base('2los')
+    #     x = add(x,"--noStackSig --showIndivSigs --xp TChiNeuWZ_95")
+    #     if '_notrigger' in torun: x = add(x,'-X ^triggerAll ')
+    #     x = add(x,"-R ^ledlepPt NoUpledlepPt '30 < LepGood1_pt && LepGood1_pt<70'")   
+    #     x = add(x,"--xP SR_bins_EWKino,SR_bins_stop")
+    #     x = add(x,"-I ^lowMET -E ^upSublepPtUp -R ^ledlepPt NoUpledlepPt '15 < LepGood1_pt'")
+    #     if '_met125' in torun:   
+    #         x = add(x,"-E ^mm -I ^lowMET")
+    #     if '_met200' in torun:   
+    #         x = add(x,"-E ^highMET")    
+    #     runIt(x,'%s/all'%torun)       
+    
 
     #### still to be adapted to SOS        
     # if '2lss_' in torun:
