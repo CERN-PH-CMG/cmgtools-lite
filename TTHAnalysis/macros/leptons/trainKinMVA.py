9c371ee457f9f0a10f36431a721e7c3a953a8d6e
Benjamin Stieger <stiegerb@cern.ch>
2016-07-06 18:46:05 +0200

    Added 2d training; selections and variables are copied from Milos

--- TTHAnalysis/macros/leptons/trainKinMVA.py
+++ TTHAnalysis/macros/leptons/trainKinMVA.py
@@ -29,7 +29,8 @@ def load_dataset(name, trainclass, addw=1, path=None, friends=[]):
                 os.path.join(path, name, 'treeProducerSusyMultilepton/tree.root'))
     print 'Using %s' % fileloc
     infile = ROOT.TFile.Open(fileloc)
-    _allfiles.append(infile)
+    _allfiles.append(infile) # Dirty trick to keep the files and trees in memory
+
     tree = infile.Get('tree')
 
     # Check if tree was loaded
@@ -154,17 +155,161 @@ def train_multiclass(fOutName, options):
 
     fOut.Close()
 
+def train_single(allcuts, variables, dsets, fOutName, options):
+    datasets = []
+    for name, trainclass, addw in dsets:
+        tree, weight = load_dataset(name, trainclass, addw,
+                                    path=options.treepath,
+                                    friends=options.friends)
+        datasets.append((name, trainclass, tree, weight))
+
+    fOut = ROOT.TFile(fOutName,"recreate")
+    fOut.cd()
+    factory = ROOT.TMVA.Factory(options.training, fOut, "!V:!Color:Transformations=I")
+
+    for cut in options.addcuts:
+        allcuts += cut
+
+    factory.AddSpectator("iF0 := iF_Recl[0]","F") # do not remove this!
+    factory.AddSpectator("iF1 := iF_Recl[1]","F") # do not remove this!
+    factory.AddSpectator("iF2 := iF_Recl[2]","F") # do not remove this!
+
+    ## Add the variables
+    for var in variables:
+        factory.AddVariable(var, 'F')
+
+    ## Add the datasets
+    for name,trainclass,tree,weight in datasets:
+        factory.AddTree(tree, trainclass, weight, allcuts)
+
+    fOut.cd()
+    for trainclass in set([x[1] for x in dsets]):
+        factory.SetWeightExpression("genWeight*xsec", trainclass)
+
+    ## Start the training
+    factory.PrepareTrainingAndTestTree(allcuts, "!V")
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDTG',
+                            ':'.join([
+                                '!H',
+                                '!V',
+                                'NTrees=200',
+                                'BoostType=Grad',
+                                'Shrinkage=0.10',
+                                '!UseBaggedGrad',
+                                'nCuts=200',
+                                'nEventsMin=100',
+                                'NNodesMax=5',
+                                'MaxDepth=8',
+                                'NegWeightTreatment=PairNegWeightsGlobal',
+                                'CreateMVAPdfs',
+                                ]))
+    factory.TrainAllMethods()
+    factory.TestAllMethods()
+    factory.EvaluateAllMethods()
+
+    fOut.Close()
+
+def train_2d(training, options):
+    allcuts = ROOT.TCut('1')
+    if '2lss' in training:
+        allcuts += "nLepFO_Recl>=2"
+        allcuts += "LepGood_conePt[iF_Recl[0]]>20"
+        allcuts += "LepGood_conePt[iF_Recl[1]]>10"
+        allcuts += "LepGood_charge[iF_Recl[0]] == LepGood_charge[iF_Recl[1]]"
+        allcuts += "(nBJetLoose25_Recl >= 2 || nBJetMedium25_Recl >= 1)"
+        allcuts += "nJet25_Recl >= 4"
+    elif '3l' in training:
+        allcuts += "nLepFO_Recl>=3"
+        allcuts += "abs(mZ1_Recl-91.2)>10"
+        allcuts += "LepGood_conePt[iF_Recl[0]]>20"
+        allcuts += "LepGood_conePt[iF_Recl[1]]>10"
+        allcuts += "LepGood_conePt[iF_Recl[2]]>10"
+        allcuts += "(nJet25_Recl >= 4 || (met_pt*0.00397 + mhtJet25_Recl*0.00265 - 0.184 > 0.0 + 0.1*(mZ1_Recl > 0)))"
+        allcuts += "nBJetLoose25_Recl >= 2"
+
+    variables = [ # Common variables
+        "max_Lep_eta := max(abs(LepGood_eta[iF_Recl[0]]),abs(LepGood_eta[iF_Recl[1]]))",
+        "numJets_float := nJet25_Recl",
+        "mindr_lep1_jet := mindr_lep1_jet",
+        "mindr_lep2_jet := mindr_lep2_jet",
+        "MT_met_lep1 := MT_met_lep1",
+    ]
+    dsets = [('TTHnobb_pow', 'Signal', 1)]
+
+    if '2lss' in training and 'ttw' in training:
+        variables += ["LepGood_conePt[iF_Recl[1]] := LepGood_conePt[iF_Recl[1]]"]
+        dsets += [('TTW_LO', 'Background', 1)]
+
+    if '2lss' in training and 'ttv' in training:
+        variables += [
+            "LepGood_conePt[iF_Recl[1]] := LepGood_conePt[iF_Recl[1]]",
+            "LepGood_conePt[iF_Recl[0]] := LepGood_conePt[iF_Recl[0]]"
+        ]
+        dsets += [
+            ('TTW_LO', 'Background', 1)
+            ('TTZ_LO', 'Background', 1),
+        ]
+    if '2lss' in training and 'ttbar' in training:
+        variables += [
+            "met := min(met_pt, 400)",
+            "avg_dr_jet : = avg_dr_jet",
+        ]
+        dsets += [
+            ('TTJets_SingleLeptonFromT',        'Background', 0.1),
+            ('TTJets_SingleLeptonFromTbar',     'Background', 0.1),
+            ('TTJets_SingleLeptonFromT_ext',    'Background', 0.9),
+            ('TTJets_SingleLeptonFromTbar_ext', 'Background', 0.9),
+        ]
+
+
+    if '3l' in training and 'ttw' in training:
+        variables += ["LepGood_conePt[iF_Recl[2]] := LepGood_conePt[iF_Recl[2]]"]
+        dsets += [('TTW_LO', 'Background', 1)]
+    if '3l' in training and 'ttv' in training:
+        variables += [
+            "LepGood_conePt[iF_Recl[2]] := LepGood_conePt[iF_Recl[2]]",
+            "LepGood_conePt[iF_Recl[0]] := LepGood_conePt[iF_Recl[0]]"
+        ]
+        dsets += [
+            ('TTW_LO', 'Background', 1)
+            ('TTZ_LO', 'Background', 1),
+        ]
+    if '3l' in training and 'ttbar' in training:
+        variables += [
+            "mhtJet25 := mhtJet25_Recl",
+            "avg_dr_jet : = avg_dr_jet",
+        ]
+        dsets += [
+            ('TTJets_DiLepton',            'Background', 0.1),
+            ('TTJets_DiLepton_ext_skim3l', 'Background', 0.9),
+        ]
+
+    outname = 'kinMVA_%s.root' % training
+    train_single(allcuts, variables, dsets, outname, options)
+
 def main(args, options):
     global _treepath
     _treepath = options.treepath
     if 'MultiClassICHEP16' in options.training:
         train_multiclass(args[0], options)
+        return
+
+    if len(options.training):
+        train_2d(options.training.lower(), options)
+    else:
+        train_2d('2lss_ttv',   options)
+        train_2d('2lss_ttbar', options)
+        train_2d('3l_ttv',     options)
+        train_2d('3l_ttbar',   options)
+
 
 if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] OUTFILE")
     parser.add_option("-T","--training", dest="training",
-                      type="string", default="MultiClassICHEP16")
+                      type="string", default="",
+                      help=('Either "MultiClassICHEP16" or "2lss_ttw",'
+                            '"3l_ttbar", etc. Default will run all 2D trainings'))
     parser.add_option("-P","--treepath", dest="treepath",
                       type="string", default=None)
     parser.add_option("-F","--friend", dest="friends",
