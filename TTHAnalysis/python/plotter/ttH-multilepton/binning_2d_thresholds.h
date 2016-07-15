a93de8ba31025009f22274f6e2518e8bef974846
peruzzim <peruzzim@users.noreply.github.com>
2016-07-15 16:59:53 +0200

    opt binning 3l

--- TTHAnalysis/python/plotter/ttH-multilepton/binning_2d_thresholds.h
+++ TTHAnalysis/python/plotter/ttH-multilepton/binning_2d_thresholds.h
@@ -4,6 +4,6 @@ float cuts_2lss_ttbar2 = 0.4;
 float cuts_2lss_ttV0 = 0.3;
 float cuts_2lss_ttV1 = 0.1;
 float cuts_2lss_ttV2 = 0.4;
-float cuts_3l_ttbar1 = -0.2;
+float cuts_3l_ttbar1 = -0.3;
 float cuts_3l_ttbar2 = 0.3;
 float cuts_3l_ttV1 = 0.25;
