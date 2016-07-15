262e442c770acd1cbbe53649e515a1100b4f398a
peruzzim <peruzzim@users.noreply.github.com>
2016-07-15 15:15:33 +0200

    config for ongoing tests

--- TTHAnalysis/python/plotter/ttH-multilepton/test_2D_BDT_binning.sh
+++ TTHAnalysis/python/plotter/ttH-multilepton/test_2D_BDT_binning.sh
@@ -1,12 +1,13 @@
-directory=$8 # something like cards_testWP_date
-category=$9 # 2lss or 3l or all
-echo "float cuts_2lss_ttbar1 = ${1};" > ttH-multilepton/binning_2d_thresholds.h
-echo "float cuts_2lss_ttbar2 = ${2};" >> ttH-multilepton/binning_2d_thresholds.h
-echo "float cuts_2lss_ttV1 = ${3};" >> ttH-multilepton/binning_2d_thresholds.h
-echo "float cuts_2lss_ttV2 = ${4};" >> ttH-multilepton/binning_2d_thresholds.h
-echo "float cuts_3l_ttbar1 = ${5};" >> ttH-multilepton/binning_2d_thresholds.h
-echo "float cuts_3l_ttbar2 = ${6};" >> ttH-multilepton/binning_2d_thresholds.h
-echo "float cuts_3l_ttV1 = ${7};" >> ttH-multilepton/binning_2d_thresholds.h
+directory=$9 # something like cards_testWP_date
+category=${10} # 2lss or 3l or all
+echo "float cuts_2lss_ttbar0 = ${1};" > ttH-multilepton/binning_2d_thresholds.h
+echo "float cuts_2lss_ttbar1 = ${2};" >> ttH-multilepton/binning_2d_thresholds.h
+echo "float cuts_2lss_ttbar2 = ${3};" >> ttH-multilepton/binning_2d_thresholds.h
+echo "float cuts_2lss_ttV1 = ${4};" >> ttH-multilepton/binning_2d_thresholds.h
+echo "float cuts_2lss_ttV2 = ${5};" >> ttH-multilepton/binning_2d_thresholds.h
+echo "float cuts_3l_ttbar1 = ${6};" >> ttH-multilepton/binning_2d_thresholds.h
+echo "float cuts_3l_ttbar2 = ${7};" >> ttH-multilepton/binning_2d_thresholds.h
+echo "float cuts_3l_ttV1 = ${8};" >> ttH-multilepton/binning_2d_thresholds.h
 
 # run same command with save option first
 echo "running: bash ttH-multilepton/make_cards.sh ${directory} 10 ${category} read"
@@ -18,4 +19,4 @@ res=`combine -M Asymptotic --run blind --rAbsAcc 0.0005 --rRelAcc 0.0005 ${categ
 cd ../..
 eval `scramv1 runtime -sh`
 
-echo "${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${res}" >> results_optMVAWP_${directory}.txt
+echo "${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${res}" >> results_optMVAWP_${directory}.txt
