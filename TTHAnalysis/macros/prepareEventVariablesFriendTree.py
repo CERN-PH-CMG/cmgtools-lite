dc9727260f45080b1b4c0ffebba55637804e803c
Benjamin Stieger <stiegerb@cern.ch>
2016-07-19 15:03:14 +0200

    Adding 80X btag scalefactor event weight friend to prepareEventVariablesFriendTree

--- TTHAnalysis/macros/prepareEventVariablesFriendTree.py
+++ TTHAnalysis/macros/prepareEventVariablesFriendTree.py
@@ -9,26 +9,28 @@ MODULES = []
 utility_files_dir = os.environ["CMSSW_BASE"]+"/src/CMGTools/TTHAnalysis/data/"
 isFastSim = False
 
-#btagSF = utility_files_dir+"/btag/CSVv2_25ns.csv"
-#btagEFF = utility_files_dir+"/btag/btageff__ttbar_powheg_pythia8_25ns.root"
-#btagSF_FastSim = utility_files_dir+"/btag/CSV_13TEV_Combined_20_11_2015_FullSim_FastSim.csv"
+# btag event weights in 80X
+from CMGTools.TTHAnalysis.tools.BTagScaleFactors import BTagScaleFactors
+btagsf_payload = os.path.join(utility_files_dir, "btag", "CSVv2_4invfb.csv")
+btagsf_reader = BTagScaleFactors('btagsf', btagsf_payload, algo='csv')
 
-#from CMGTools.TTHAnalysis.tools.btagRWTs_ND import BTagWeightCalculator,BTagReweightFriend,BTagLeptonReweightFriend
-## btag reweighting in 76X
-#BTagReweight76X = lambda : BTagWeightCalculator(utility_files_dir_tth+"/csv_rwt_fit_hf_76x_2016_02_08.root",
+from CMGTools.TTHAnalysis.tools.bTagEventWeights import BTagEventWeightFriend
+bTagEventWeight = lambda : BTagEventWeightFriend(reader=btagsf_reader)
+MODULES.append( ('eventBTagWeight', bTagEventWeight ))
+
+
+# btag reweighting in 76X
+# from CMGTools.TTHAnalysis.tools.btagRWTs_ND import BTagWeightCalculator,BTagReweightFriend,BTagLeptonReweightFriend
+# BTagReweight76X = lambda : BTagWeightCalculator(utility_files_dir_tth+"/csv_rwt_fit_hf_76x_2016_02_08.root",
 #                                                utility_files_dir_tth+"/csv_rwt_fit_lf_76x_2016_02_08.root")
-#systsBTAG = ["nominal", "_JESUp", "_JESDown", "_LFUp", "_LFDown", "_HFUp", "_HFDown", \
-#                 "_HFStats1Up", "_HFStats1Down", "_HFStats2Up", "_HFStats2Down", \
-#                 "_LFStats1Up", "_LFStats1Down", "_LFStats2Up", "_LFStats2Down", \
-#                 "_cErr1Up", "_cErr1Down", "_cErr2Up", "_cErr2Down" ]
-#for syst in systsBTAG: # should be converted to lambda functions
+# for syst in systsBTAG: # should be converted to lambda functions
 #    MODULES.append( ('btagRWJet%s'%syst, BTagReweightFriend(BTagReweight76X, outlabel='btagCSVWeight%s'%syst.replace('nominal',''), rwtSyst=syst.replace('_','')) ))
 #    MODULES.append( ('btagRWJetUp%s'%syst, BTagReweightFriend(BTagReweight76X, jets=["Jet_jecUp","DiscJet_jecUp"], outlabel='btagCSVWeight%s'%syst.replace('nominal',''), rwtSyst=syst.replace('_','')) ))
 #    MODULES.append( ('btagRWJetDown%s'%syst, BTagReweightFriend(BTagReweight76X, jets=["Jet_jecDown","DiscJet_jecDown"], outlabel='btagCSVWeight%s'%syst.replace('nominal',''), rwtSyst=syst.replace('_','')) ))
 #    MODULES.append( ('btagRWLep%s'%syst, BTagLeptonReweightFriend(BTagReweight76X, outlabel='jetBTagCSVWeight%s'%syst.replace('nominal',''), rwtSyst=syst.replace('_','')) ))
-#
-#from CMGTools.TTHAnalysis.tools.eventBTagRWT import EventBTagRWT
-#MODULES.append( ('eventBTagRWT', lambda: EventBTagRWT() ))
+
+# from CMGTools.TTHAnalysis.tools.eventBTagRWT import EventBTagRWT
+# MODULES.append( ('eventBTagRWT', lambda: EventBTagRWT() ))
 
 #--- Recleaner instances
 
