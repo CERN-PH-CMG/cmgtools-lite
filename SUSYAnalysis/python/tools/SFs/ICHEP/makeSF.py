2312de5cf517a7a4ff21ae851bb480e78bf11d09
Claudia <clseitz@physics.rutgers.edu>
2016-07-25 12:33:29 +0200

    do not use HIP for SF 2D histograms

--- SUSYAnalysis/python/tools/SFs/ICHEP/makeSF.py
+++ SUSYAnalysis/python/tools/SFs/ICHEP/makeSF.py
@@ -5,6 +5,7 @@ from ROOT import *
 
 f1_ele = TFile.Open('GsfElectronToTight.root', 'read') 
 f2_ele = TFile.Open('MVAVLooseElectronToMini.root', 'read')
+f3_ele = TFile.Open('egammaEffi.txt_SF2D.root', 'read')
 
 f1_mu = TFile.Open('TnP_MuonID_NUM_MediumID_DENOM_generalTracks_VAR_map_pt_eta.root', 'read')
 f2_mu = TFile.Open('TnP_MuonID_NUM_MiniIsoTight_DENOM_MediumID_VAR_map_pt_eta.root', 'read')
@@ -12,8 +13,9 @@ f3_mu = TFile.Open('TnP_MuonID_NUM_TightIP3D_DENOM_MediumID_VAR_map_pt_eta.root'
 f4_mu = TFile.Open('general_tracks_and_early_general_tracks_corr_ratio.root', 'read')
 
 
-h1_ele = f1_ele.Get('probe_Ele_pt_probe_sc_abseta_PLOT')
-h2_ele = f2_ele.Get('probe_Ele_pt_probe_sc_abseta_PLOT')
+h1_ele = f1_ele.Get('h2_scaleFactorsEGamma')
+h2_ele = f2_ele.Get('h2_scaleFactorsEGamma')
+h3_ele = f3_ele.Get('EGamma_SF2D')
 
 h12_ele = h1_ele.Clone()
 h12_ele.Multiply(h2_ele)
@@ -22,6 +24,12 @@ h12_ele.SetTitle('CBtight_miniIso0p2_ICHEP')
 h12_ele.SaveAs('CBtight_miniIso0p2_ICHEP.root')
 
 
+h123_ele = h12_ele.Clone()
+etabins = h12_ele.GetNbinsY()
+ptbins = h12_ele.GetNbinsX()
+
+##########################
+####MUONS##############
 h1_mu = f1_mu.Get('pt_abseta_PLOT_pair_probeMultiplicity_bin0')
 h2_mu = f2_mu.Get('pt_abseta_PLOT_pair_probeMultiplicity_bin0_&_Medium2016_pass')
 h3_mu = f3_mu.Get('pt_abseta_PLOT_pair_probeMultiplicity_bin0_&_Medium2016_pass')
@@ -35,19 +43,4 @@ h123_mu.SetName("MediumMuon_miniIso0p2_SIP3D_ICHEP")
 h123_mu.SetTitle("MediumMuon_miniIso0p2_SIP3D_ICHEP")
 h123_mu.SaveAs("MediumMuon_miniIso0p2_SIP3D_ICHEP.root")
 
-h1234_mu = h123_mu.Clone()
-etabins = h123_mu.GetNbinsY()
-ptbins = h123_mu.GetNbinsX()
-
-for eta in range(1,etabins+1):
-    HIPfactor = h4_mu.GetBinContent(eta)
-    for pt in range(1,ptbins+1):
-        before = h123_mu.GetBinContent(pt, eta)
-        after = before * HIPfactor
-        print before, after, HIPfactor
-        h1234_mu.SetBinContent(pt, eta, after)
-
-h1234_mu.SetName("MediumMuon_miniIso0p2_SIP3D_HIP_ICHEP")
-h1234_mu.SetTitle("MediumMuon_miniIso0p2_SIP3D_HIP_ICHEP")
-h1234_mu.SaveAs("MediumMuon_miniIso0p2_SIP3D_HIP_ICHEP.root")
         
