e2cc4f3447c983471b99e7bcf301c74c24c00bb1
Clemens Lange <clemens.lange@cern.ch>
2016-07-27 19:36:39 +0200

    compilation is progress

--- VVResonances/interface/CandidateBoostedDoubleSecondaryVertexComputerLight.h
+++ VVResonances/interface/CandidateBoostedDoubleSecondaryVertexComputerLight.h
@@ -2,72 +2,75 @@
 #define CMGTools_VVResonances_CandidateBoostedDoubleSecondaryVertexComputerLight_h
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Framework/interface/ESHandle.h"
 #include "CommonTools/Utils/interface/TMVAEvaluator.h"
-#include "RecoBTau/JetTagComputer/interface/JetTagComputer.h"
 #include "DataFormats/JetReco/interface/JetCollection.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/Candidate/interface/VertexCompositePtrCandidate.h"
-#include "RecoBTag/SecondaryVertex/interface/TrackKinematics.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
 #include "RecoBTag/SecondaryVertex/interface/V0Filter.h"
 #include "RecoBTag/SecondaryVertex/interface/TrackSelector.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "MagneticField/ParametrizedEngine/src/OAEParametrizedMagneticField.h"
 
 #include "fastjet/PseudoJet.hh"
 
 namespace cmg{
 
-class CandidateBoostedDoubleSecondaryVertexComputerLight : public JetTagComputer {
+    class CandidateBoostedDoubleSecondaryVertexComputerLight {
 
-  public:
-    CandidateBoostedDoubleSecondaryVertexComputerLight(
-        double beta, double R0, double maxSVDeltaRToJet, std::string gbrForestLabel,
-        std::string weightFile, bool useGBRForest, bool useAdaBoost, double k0sMassWindow,
-        int totalHitsMin, double jetDeltaRMax, std::string qualityClass, int pixelHitsMin,
-        double maxDistToAxis, double maxDecayLen, double sip3dSigMin, double sip3dSigMax,
-        double sip2dValMax, double ptMin, double sip2dSigMax, double sip2dSigMin,
-        double sip3dValMax, double sip3dValMin, double sip2dValMin, double normChi2Max
-    );
+      public:
+        CandidateBoostedDoubleSecondaryVertexComputerLight(
+            double beta, double R0, double maxSVDeltaRToJet, std::string gbrForestLabel,
+            std::string weightFile, bool useGBRForest, bool useAdaBoost, double k0sMassWindow,
+            int totalHitsMin, double jetDeltaRMax, std::string qualityClass, int pixelHitsMin,
+            double maxDistToAxis, double maxDecayLen, double sip3dSigMin, double sip3dSigMax,
+            double sip2dValMax, double ptMin, double sip2dSigMax, double sip2dSigMin,
+            double sip3dValMax, double sip3dValMin, double sip2dValMin, double normChi2Max
+        );
 
-    void  initialize() override;
-    float discriminator(const TagInfoHelper & tagInfos) const override;
+        void  initialize();
+        float discriminator(const pat::Jet& jet) const;
+        reco::TransientTrack getTransientTrack(const reco::CandidatePtr& trackRef) const;
 
-  private:
-    void calcNsubjettiness(const reco::JetBaseRef & jet, float & tau1, float & tau2, std::vector<fastjet::PseudoJet> & currentAxes) const;
-    void setTracksPVBase(const reco::TrackRef & trackRef, const reco::VertexRef & vertexRef, float & PVweight) const;
-    void setTracksPV(const reco::CandidatePtr & trackRef, const reco::VertexRef & vertexRef, float & PVweight) const;
-    void etaRelToTauAxis(const reco::VertexCompositePtrCandidate & vertex, fastjet::PseudoJet & tauAxis, std::vector<float> & tau_trackEtaRel) const;
+      private:
+        void calcNsubjettiness(const reco::JetBaseRef & jet, float & tau1, float & tau2, std::vector<fastjet::PseudoJet> & currentAxes) const;
+        void setTracksPVBase(const reco::TrackRef & trackRef, const reco::VertexRef & vertexRef, float & PVweight) const;
+        void setTracksPV(const reco::CandidatePtr & trackRef, const reco::VertexRef & vertexRef, float & PVweight) const;
+        void etaRelToTauAxis(const reco::VertexCompositePtrCandidate & vertex, fastjet::PseudoJet & tauAxis, std::vector<float> & tau_trackEtaRel) const;
 
-    const double beta_;
-    const double R0_;
+        reco::V0Filter *trackPairV0Filter;
+        reco::TrackSelector *trackSelector;
+        OAEParametrizedMagneticField *paramField;
 
-    const double maxSVDeltaRToJet_;
-    const std::string gbrForestLabel_;
-    const edm::FileInPath weightFile_;
-    const bool useGBRForest_;
-    const bool useAdaBoost_;
-    const double maxDistToAxis_;
-    const double maxDecayLen_;
-    reco::V0Filter trackPairV0Filter;
-    reco::TrackSelector trackSelector;
+        const double beta_;
+        const double R0_;
 
-    edm::ESHandle<TransientTrackBuilder> trackBuilder;
-    std::unique_ptr<TMVAEvaluator> mvaID;
+        const double maxSVDeltaRToJet_;
+        const std::string gbrForestLabel_;
+        const edm::FileInPath weightFile_;
+        const bool useGBRForest_;
+        const bool useAdaBoost_;
+        const double maxDistToAxis_;
+        const double maxDecayLen_;
 
-    // static variables
-    static constexpr float dummyZ_ratio             = -3.0f;
-    static constexpr float dummyTrackSip3dSig       = -50.0f;
-    static constexpr float dummyTrackSip2dSigAbove  = -19.0f;
-    static constexpr float dummyTrackEtaRel         = -1.0f;
-    static constexpr float dummyVertexMass          = -1.0f;
-    static constexpr float dummyVertexEnergyRatio   = -1.0f;
-    static constexpr float dummyVertexDeltaR        = -1.0f;
-    static constexpr float dummyFlightDistance2dSig = -1.0f;
+        edm::ESHandle<TransientTrackBuilder> trackBuilder;
+        std::unique_ptr<TMVAEvaluator> mvaID;
 
-    static constexpr float charmThreshold  = 1.5f;
-    static constexpr float bottomThreshold = 5.2f;
-};
+        // static variables
+        static constexpr float dummyZ_ratio             = -3.0f;
+        static constexpr float dummyTrackSip3dSig       = -50.0f;
+        static constexpr float dummyTrackSip2dSigAbove  = -19.0f;
+        static constexpr float dummyTrackEtaRel         = -1.0f;
+        static constexpr float dummyVertexMass          = -1.0f;
+        static constexpr float dummyVertexEnergyRatio   = -1.0f;
+        static constexpr float dummyVertexDeltaR        = -1.0f;
+        static constexpr float dummyFlightDistance2dSig = -1.0f;
+
+        static constexpr float charmThreshold  = 1.5f;
+        static constexpr float bottomThreshold = 5.2f;
+
+    };
 
 } // end namespace cmg
 
--- VVResonances/python/analyzers/HbbTagComputer.py
+++ VVResonances/python/analyzers/HbbTagComputer.py
@@ -18,39 +18,32 @@ class HbbTagComputer( Analyzer ):
         R0 = cms.double(0.8),
         maxSVDeltaRToJet = cms.double(0.7),
         useCondDB = cms.bool(False),
+        gbrForestLabel = cms.string(""),
         weightFile = cms.FileInPath('CMGTools/VVResonances/data/BoostedDoubleSV_AK8_BDT_v3.weights.xml.gz'),
         useGBRForest = cms.bool(True),
         useAdaBoost = cms.bool(False),
         trackPairV0Filter = cms.PSet(k0sMassWindow = cms.double(0.03))
         )
         # deltaRMax = 0.8
+        candidateBDSVAK8ComputerPSet.trackSelection.jetDeltaRMax = cms.double(0.8)
         hbbComputer = ROOT.cmg.CandidateBoostedDoubleSecondaryVertexComputerLight(
-            candidateBDSVAK8ComputerPSet.beta, candidateBDSVAK8ComputerPSet.R0, candidateBDSVAK8ComputerPSet.maxSVDeltaRToJet, candidateBDSVAK8ComputerPSet.gbrForestLabel,
-            candidateBDSVAK8ComputerPSet.weightFile, candidateBDSVAK8ComputerPSet.useGBRForest, candidateBDSVAK8ComputerPSet.useAdaBoost, candidateBDSVAK8ComputerPSet.trackPairV0Filter.k0sMassWindow,
-            candidateBDSVAK8ComputerPSet.trackSelectionBlock.totalHitsMin, candidateBDSVAK8ComputerPSet.trackSelectionBlock.jetDeltaRMax, candidateBDSVAK8ComputerPSet.trackSelectionBlock.qualityClass, candidateBDSVAK8ComputerPSet.trackSelectionBlock.pixelHitsMin,
-            candidateBDSVAK8ComputerPSet.trackSelectionBlock.maxDistToAxis, candidateBDSVAK8ComputerPSet.trackSelectionBlock.maxDecayLen, candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip3dSigMin, candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip3dSigMax,
-            candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip2dValMax, candidateBDSVAK8ComputerPSet.trackSelectionBlock.ptMin, candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip2dSigMax, candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip2dSigMin,
-            candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip3dValMax, candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip3dValMin, candidateBDSVAK8ComputerPSet.trackSelectionBlock.sip2dValMin, candidateBDSVAK8ComputerPSet.trackSelectionBlock.normChi2Max
+            candidateBDSVAK8ComputerPSet.beta.value(), candidateBDSVAK8ComputerPSet.R0.value(), candidateBDSVAK8ComputerPSet.maxSVDeltaRToJet.value(), candidateBDSVAK8ComputerPSet.gbrForestLabel.value(),
+            candidateBDSVAK8ComputerPSet.weightFile.value(), candidateBDSVAK8ComputerPSet.useGBRForest.value(), candidateBDSVAK8ComputerPSet.useAdaBoost.value(), candidateBDSVAK8ComputerPSet.trackPairV0Filter.k0sMassWindow.value(),
+            candidateBDSVAK8ComputerPSet.trackSelection.totalHitsMin.value(), candidateBDSVAK8ComputerPSet.trackSelection.jetDeltaRMax.value(), candidateBDSVAK8ComputerPSet.trackSelection.qualityClass.value(), candidateBDSVAK8ComputerPSet.trackSelection.pixelHitsMin.value(),
+            candidateBDSVAK8ComputerPSet.trackSelection.maxDistToAxis.value(), candidateBDSVAK8ComputerPSet.trackSelection.maxDecayLen.value(), candidateBDSVAK8ComputerPSet.trackSelection.sip3dSigMin.value(), candidateBDSVAK8ComputerPSet.trackSelection.sip3dSigMax.value(),
+            candidateBDSVAK8ComputerPSet.trackSelection.sip2dValMax.value(), candidateBDSVAK8ComputerPSet.trackSelection.ptMin.value(), candidateBDSVAK8ComputerPSet.trackSelection.sip2dSigMax.value(), candidateBDSVAK8ComputerPSet.trackSelection.sip2dSigMin.value(),
+            candidateBDSVAK8ComputerPSet.trackSelection.sip3dValMax.value(), candidateBDSVAK8ComputerPSet.trackSelection.sip3dValMin.value(), candidateBDSVAK8ComputerPSet.trackSelection.sip2dValMin.value(), candidateBDSVAK8ComputerPSet.trackSelection.normChi2Max.value()
         )
+        hbbComputer.initialize()
 
     def beginLoop(self, setup):
         super(HbbTagComputer,self).beginLoop(setup)
 
 
     def Hbbtag(self,jet):
-#        for leptonid in lepton.electronIDs():
-#            print leptonid.first,leptonid.second
-        return jet
+        return hbbComputer.discriminator(jet.sourcePtr())
 
     def process(self, event):
         self.readCollections( event.input )
-
-        # for lepton in event.selectedLeptons:
-        #     if abs(lepton.pdgId())==11:
-        #         lepton.heepID = self.heepID(lepton)
-        #         lepton.heepIDNoIso = self.heepIDNoIso(lepton)
-        #     else:
-        #         lepton.highPtID = self.muonIDHighPt(lepton)
-        #         lepton.highPtTrackID = self.muonIDTrackerHighPt(lepton)
-        #         lepton.highPtIDIso = self.muonIDHighPtIso(lepton)
-        #         lepton.highPtTrackIDIso = self.muonIDTrackerHighPtIso(lepton)
+        for jet in event.selectedJets:
+            jet.Hbbtag = self.Hbbtag(jet)
