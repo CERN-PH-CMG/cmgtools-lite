5c15d26d72853ff106cefa9f70c68cc3d4e7a146
Marco Cipriani <cippy91@gmail.com>
2016-09-15 22:55:56 +0200

    avoid duplication of files to resubmit when there are zombies

--- MonoXAnalysis/scripts/cmgCheckProduction.sh
+++ MonoXAnalysis/scripts/cmgCheckProduction.sh
@@ -30,21 +30,41 @@ cmgListChunksToResub -t JSONAnalyzer/RLTInfo.root -z >> clean.sh
 echo "# Preparing the script to run..."
 sort clean.sh | uniq > clean2.sh
 
+# if a file is zombie, a comment is added at the end of the line. 
+#This is undesirable because there might be equal chunks not filtered by "uniq" due to the comment at the end of one
+# To remove them, we can exploit the fact that the duplicated line with the comment would be sorted as the second line for each pair
+# The algorithm compare each line with the previous one (for the very first line, a junk string is used as the previous one). 
+# if the read line contains the previous, then it is not written in the file
+
+echo "# File with commands to resubmit/remove failed chunks" > clean2polished.sh
+currentLine="VeryFirstLineToBeUsedInTheAlgorithmBelow" # leave it as a junk string 
+# starts reading clean2.sh
+while read line
+do
+    previousLine=$currentLine  # backup the previous line
+    currentLine=$line          # read the current line
+
+    if [ "$currentLine" == "$previousLine    # zombie" ]; then
+	echo "Skipping ====> $currentLine <====  because already present in file"
+    else
+	echo "$currentLine" >> clean2polished.sh  # save this line if it contains the previous one
+    fi 
+done < ./clean2.sh
+
 if [[ "$RM" == 0 ]]; then
     echo "#Changing queue from 8nh to 2nd"
-    sed 's|cmgResubChunk -q 8nh |cmgResubChunk -q 2nd |g' clean2.sh > clean2bckp.sh
+    sed 's|cmgResubChunk -q 8nh |cmgResubChunk -q 2nd |g' clean2polished.sh > chunksToResub.sh
 else
-    sed 's|cmgResubChunk -q 8nh |rm -r |g' clean2.sh > clean2bckp.sh
+    sed 's|cmgResubChunk -q 8nh |rm -r |g' clean2polished.sh > chunksToResub.sh
 fi;
 
-if [ $saveList = "true" ]; then 
-    echo "#Saving file to resubmit/remove bad chunks"
-    cp clean2bckp.sh chunksToResub.sh 
+if [ $saveList = "false" ]; then 
+    rm chunksToResub.sh 
 fi
 
 rm clean.sh
 rm clean2.sh
-rm clean2bckp.sh
+rm clean2polished.sh
 
 echo "# DONE."
 
