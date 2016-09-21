a795090a5b2cd5c15b8ff4c279bd31fb002210ce
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2016-09-21 16:46:58 +0200

    adding plotting and reorganising samples names

--- MonoXAnalysis/python/plotter/vbfdm/analysis.py
+++ MonoXAnalysis/python/plotter/vbfdm/analysis.py
@@ -11,13 +11,15 @@ if __name__ == "__main__":
     parser.add_option("-r", "--region", dest="region", default='signal', help='Find the yields for this phase space')
     parser.add_option("-d", "--dry-run", dest="dryrun", action="store_true", default=False, help='Do not run the commands, just print them')
     parser.add_option("-s", "--synch", dest="synch", action="store_true", default=False, help='Do not apply any scale factor, bare yields')
+    parser.add_option("-p", "--plot", dest="plot", action="store_true", default=False, help='Make the plots, not the yields')
     (options, args) = parser.parse_args()
 
     T='/data1/emanuele/monox/TREES_MET_80X_V4/'
     anaOpts = []
 
-    corey = 'mcAnalysis.py '
-    coreopt = ' -P '+T+' --s2v -j 6 -l 2.3 -G'
+    corey = 'mcAnalysis.py ' if options.plot == False else 'mcPlots.py '
+    coreopt = ' -P '+T+' --s2v -j 6 -l 24.47 -G'
+    plotopt = ' -f --poisson --pdir plots --showRatio --maxRatioRange 0.5 1.5 '
     anaOpts += [coreopt]
 
     fev = ' -F mjvars/t \"'+T+'/friends/evVarFriend_{cname}.root\" '
@@ -25,8 +27,6 @@ if __name__ == "__main__":
     anaOpts += [fev, fsf]
     if options.synch == True: anaOpts += '-u'
     
-    anaOptsString = ' '.join(anaOpts)
-
     runy = corey + 'vbfdm/mca-80X-sync.txt --s2v '
     cuts = {
         'signal': 'vbfdm/vbfdm.txt',
@@ -36,6 +36,12 @@ if __name__ == "__main__":
         'wenu': 'vbfdm/wenu.txt',
         'gjets': 'vbfdm/gjets.txt',
         }
+    
+    plotfile = re.split(".txt",cuts[options.region])[0]+"_plots.txt"
+    if options.plot: 
+        anaOpts += [plotfile, plotopt]
+
+    anaOptsString = ' '.join(anaOpts)
 
     if options.region not in cuts: raise RuntimeError, "Region "+options.region+" not in the foreseen ones: "+cuts
     weights = ['puw']
@@ -43,6 +49,6 @@ if __name__ == "__main__":
     
     weightsString = " -W '" + "*".join(weights) + "'"
 
-    command = 'python ' + runy + cuts[options.region] + anaOptsString + weightsString + ' --sp vbfH125 -X trigger -X metfilters '
+    command = 'python ' + runy + cuts[options.region] + anaOptsString + weightsString + ' -X trigger -X metfilters '
     if options.dryrun: print command
     else: os.system(command)
