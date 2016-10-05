efa2da129f60585bcb10908e6bb591fb0b92ecab
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2016-10-05 17:50:24 +0200

    make the shape datacards for the SR and 4 lepton CRs

--- MonoXAnalysis/python/plotter/vbfdm/make_cards.sh
+++ MonoXAnalysis/python/plotter/vbfdm/make_cards.sh
@@ -1,29 +1,78 @@
 #!/bin/bash
 
 if [[ "$HOSTNAME" == "cmsphys06" ]]; then
-    T="/data1/emanuele/monox/TREES_150515_MET200SKIM";
+    TBASE="/data1/emanuele/monox/";
     J=6;
 else
-    T="/cmshome/dimarcoe/TREES_060515_MET200SKIM";
-    J=6;
+    TBASE="/afs/cern.ch/work/e/emanuele/TREES/";
+    J=4;
 fi
 
-ASIMOV=""
-if echo "$1" | grep -q "asimov"; then ASIMOV="$1"; shift; fi
+METSKIM="TREES_MET_80X_V4"
+LEPSKIM="TREES_1LEP_80X_V4"
 
-OPTIONS=" -P $T --s2v -j $J -l 5.0 -f "
-SYSTS="systsEnv.txt"
+TREEMET="${TBASE}/$METSKIM"
+TREELEP="${TBASE}/$LEPSKIM"
 
-if [[ $ASIMOV == "asimov" ]] ; then
-    OPTIONS="${OPTIONS} --asimov "
+if [[ "X$1" == "X" ]]; then echo "Provide output directory name!"; exit; fi
+OUTNAME=$1; shift;
+echo "output will go into ${OUTNAME} "
+if [[ "X$1" == "X" ]]; then echo "Provide luminosity!"; exit; fi
+LUMI="$1"; shift
+echo "Normalizing to ${LUMI}/fb";
+
+OPTIONS=" --s2v -j $J -l ${LUMI} -f -X trigger -X metfilters "
+test -d $OUTNAME || mkdir -p $OUTNAME
+OPTIONS="${OPTIONS} --od cards/$OUTNAME ";
+
+SYST="vbfdm/systsEnv.txt"
+
+VARIABLE="'abs(JetClean1_eta-JetClean2_eta)' '[0.5,1,2,2.5,3,3.5,4,4.5,5,6,7,10]'"
+
+if [[ "$2" == "save" ]]; then
+DOFILE="--savefile activate"
+fi
+if [[ "$2" == "read" ]]; then
+DOFILE="--infile activate"
 fi
 
-if [[ "$1" == "" ]] ; then
-    X="monojet"
-    OPTIONS="${OPTIONS} -F mjvars/t \"$T/0_eventvars_mj_v1/evVarFriend_{cname}.root\" "
-    OPT_SHAPE="${OPTIONS} --od cards/shape "
-    OPT_COUNT="${OPTIONS} -A \"lep veto\" \"met500\" \"metNoMu_pt>500\" --od cards/counting "
-    echo "python makeShapeCards.py mca-Phys14.txt sr/monojet.txt  'metNoMu_pt' '[200,250,300,350,400,500,650,1000]' $SYSTS $OPT_SHAPE -o ${X} "
-    echo "python makeCountingCards.py mca-Phys14.txt sr/monojet.txt $SYSTS $OPT_COUNT -o ${X} "
+if [[ "$1" == "all" || "$1" == "SR" ]] ; then
+    OPTIONS_SR="${OPTIONS} -P \"$TREEMET\" -F mjvars/t \"$TREEMET/friends/evVarFriend_{cname}.root\" --FMC sf/t \"$TREEMET/friends/sfFriend_{cname}.root\" -W 'puw*SF_trigmetnomu*SF_BTag*SF_NLO_QCD*SF_NLO_EWK' "
+    command="python makeShapeCards.py ${DOFILE} vbfdm/mca-80X-sync.txt vbfdm/vbfdm.txt ${VARIABLE} $SYST $OPTIONS_SR --od ${OUTNAME} --processesFromCR ZNuNu,W --region SR --unbinned"
+    echo "===> EXECUTING " $command
+    eval $command
+    echo "Done at $(date)"
+fi;
+
+if [[ "$1" == "all" || "$1" == "ZM" ]] ; then
+    OPTIONS_ZM="${OPTIONS} -P \"$TREEMET\" -F mjvars/t \"$TREEMET/friends/evVarFriend_{cname}.root\" --FMC sf/t \"$TREEMET/friends/sfFriend_{cname}.root\" -W 'puw*SF_trigmetnomu*SF_LepTightLoose*SF_BTag*SF_NLO_QCD*SF_NLO_EWK' "
+    command="python makeShapeCards.py ${DOFILE} vbfdm/mca-80X-muonCR.txt vbfdm/zmumu.txt ${VARIABLE} $SYST $OPTIONS_ZM --od ${OUTNAME} --correlateProcessCR 'ZNuNu,SR,rfac_ZLL_full,templates/rfactors_ZNuNuSR_Over_zmumuCR.root' --region ZM --unbinned --xp ZLL,EWKZLL "
+    echo "===> EXECUTING " $command
+    eval $command    
+    echo "Done at $(date)"
+fi;
+
+
+if [[ "$1" == "all" || "$1" == "ZE" ]] ; then
+    OPTIONS_ZE="${OPTIONS} -P \"$TREELEP\" -F mjvars/t \"$TREELEP/friends/evVarFriend_{cname}.root\" --FMC sf/t \"$TREELEP/friends/sfFriend_{cname}.root\" -W 'puw*SF_LepTightLoose*SF_BTag*SF_NLO_QCD*SF_NLO_EWK' "
+    command="python makeShapeCards.py ${DOFILE} vbfdm/mca-80X-electronCR.txt vbfdm/zee.txt ${VARIABLE} $SYST $OPTIONS_ZE --od ${OUTNAME} --correlateProcessCR 'ZNuNu,SR,rfac_ZLL_full,templates/rfactors_ZNuNuSR_Over_zeeCR.root' --region ZE --unbinned --xp ZLL,EWKZLL "
+    echo "===> EXECUTING " $command
+    eval $command    
+    echo "Done at $(date)"
+fi;
+
+if [[ "$1" == "all" || "$1" == "WM" ]] ; then
+    OPTIONS_WM="${OPTIONS} -P \"$TREEMET\" -F mjvars/t \"$TREEMET/friends/evVarFriend_{cname}.root\" --FMC sf/t \"$TREEMET/friends/sfFriend_{cname}.root\" -W 'puw*SF_trigmetnomu*SF_LepTight*SF_BTag*SF_NLO_QCD*SF_NLO_EWK' "
+    command="python makeShapeCards.py ${DOFILE} vbfdm/mca-80X-muonCR.txt vbfdm/wmunu.txt ${VARIABLE} $SYST $OPTIONS_WM --od ${OUTNAME} --correlateProcessCR 'W,SR,rfac_W_full,templates/rfactors_WSR_Over_wmunuCR.root' --region WM --unbinned --xp W,EWKW "
+    echo "===> EXECUTING " $command
+    eval $command
+    echo "Done at $(date)"
+fi;
+
+if [[ "$1" == "all" || "$1" == "WE" ]] ; then
+    OPTIONS_WE="${OPTIONS} -P \"$TREELEP\" -F mjvars/t \"$TREELEP/friends/evVarFriend_{cname}.root\" --FMC sf/t \"$TREELEP/friends/sfFriend_{cname}.root\" -W 'puw*SF_LepTight*SF_BTag*SF_NLO_QCD*SF_NLO_EWK' "
+    command="python makeShapeCards.py ${DOFILE} vbfdm/mca-80X-electronCR.txt vbfdm/wenu.txt ${VARIABLE} $SYST $OPTIONS_WE --od ${OUTNAME} --correlateProcessCR 'W,SR,rfac_W_full,templates/rfactors_WSR_Over_wenuCR.root' --region WE --unbinned --xp W,EWKW "
+    echo "===> EXECUTING " $command
+    eval $command
     echo "Done at $(date)"
 fi;
