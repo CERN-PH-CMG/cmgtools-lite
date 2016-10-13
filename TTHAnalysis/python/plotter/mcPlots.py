a50c0dcfe950990347d8ef44901c99177cef81b2
Giovanni <gpetruc@gmail.com>
2016-10-13 10:15:16 +0200

    mcPlots: verbosity fixes

--- TTHAnalysis/python/plotter/mcPlots.py
+++ TTHAnalysis/python/plotter/mcPlots.py
@@ -782,8 +782,8 @@ class PlotMaker:
                 for p in itertools.chain(reversed(mca.listBackgrounds(allProcs=True)), reversed(mca.listSignals(allProcs=True)), extraProcesses):
                     if p in pmap: 
                         plot = pmap[p]
-                        if plot.Integral() == 0:
-                            print 'Warning: plotting histo %s with zero integral, there might be problems in the following'%p
+                        #if plot.Integral() == 0:
+                        #    print 'Warning: plotting histo %s with zero integral, there might be problems in the following'%p
                         if plot.Integral() < 0:
                             print 'Warning: plotting histo %s with negative integral (%f), the stack plot will probably be incorrect.'%(p,plot.Integral())
                         if 'TH1' in plot.ClassName():
@@ -1060,6 +1060,7 @@ class PlotMaker:
                                 dump.write("\n")
                             dump.close()
                         else:
+                            savErrorLevel = ROOT.gErrorIgnoreLevel; ROOT.gErrorIgnoreLevel = ROOT.kWarning;
                             if "TH2" in total.ClassName() or "TProfile2D" in total.ClassName():
                                 pmap["total"] = total
                                 for p in mca.listSignals(allProcs=True) + mca.listBackgrounds(allProcs=True) + ["signal", "background", "data", "total"]:
@@ -1084,6 +1085,7 @@ class PlotMaker:
                                         c1.Print("%s/%s_data_%s.%s" % (fdir, outputName, p, ext))
                             else:
                                 c1.Print("%s/%s.%s" % (fdir, outputName, ext))
+                            ROOT.gErrorIgnoreLevel = savErrorLevel;
                 c1.Close()
 
 def getEvenBinning(histo):
