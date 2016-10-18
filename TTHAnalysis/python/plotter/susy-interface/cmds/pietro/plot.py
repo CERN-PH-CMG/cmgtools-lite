96b1f34baf0d45830ca19a5280f244d6fc64b24a
Pietro Vischia <pietro.vischia@gmail.com>
2016-10-18 15:32:37 +0200

    Plot a few tau regions

--- TTHAnalysis/python/plotter/susy-interface/cmds/pietro/plot.py
+++ TTHAnalysis/python/plotter/susy-interface/cmds/pietro/plot.py
@@ -1,33 +1,73 @@
-#!/bin/bash
+import os
+import subprocess
+import string
 
-INDIR="/pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/"
+"""
+"""
+def command(cmd, pretend):
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
+        newCmd = 'python {cmd}'.format(cmd=p.split('python')[1])
+        if pretend:
+                print newCmd
+        else:
+                os.system(newCmd)
+                
+        print "Done."
+"""
+"""
 
-if [ "$1" == "generalplots" ]; then
-
-    #python mcPlots.py susy-ewkino/3l/mca_ewkino.txt susy-ewkino/3l/cuts_ewkino.txt susy-ewkino/3l/plots_ewkino.txt -P /pool/ciencias/HeppyTrees/RA7/8011_July22/ --neg --s2v --tree treeProducerSusyMultilepton -f -j 4 --legendWidth 0.20 --legendFontSize 0.035 --mcc susy-ewkino/3l/mcc_ewkino.txt --load-macro susy-ewkino/3l/functionsEWK.cc --showRatio -l 12.9 --pdir   ~/www/susyRA7/ -F sf/t {P}/leptonJetReCleanerSusyEWK3L/evVarFriend_{cname}.root -F sf/t {P}/leptonBuilderEWK/evVarFriend_{cname}.root -F sf/t {P}/eventBTagWeight/evVarFriend_{cname}.root -p data -p fakes_appldata -p promptsub -p prompt_.* -p rares_.* -p convs --sP lep.*._pt --sP *._eta --sP flavor3l --sP charge3l -A alwaystrue BRcut 'BR==1' -X blinding --perBin --ratioOffset 0.03 --print C,png,pdf,txt --plotgroup rares_ttX+=rares_ttW --plotgroup rares_ttX+=rares_ttZ --plotgroup fakes_appldata+=promptsub --cms --legendHeader 'A: OSSF' -W 'puw2016_nInt_12p9fb(nTrueInt)*triggerSF()*leptonSF()*eventBTagSF'
-    #python susy-interface/plotmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/8011_June29/  ~/www/susyRA7/ -l 12.9 --make data --plots lep -o SR
-    #python susy-interface/plotmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/  ~/www/susyRA7/ -l 12.9 --make data --plots lep -o SR
-
-    python susy-interface/plotmaker.py 3l 3lA ${INDIR}  ~/www/susyRA7/ -l 12.9 --make data --plots br -o SR --pretend
-
-elif [ "$1" == "tauopt" ]; then
-
-
-    # Regions:
+"""
+"""
+        # Regions:
     # 
     # C:         3lC         SR-42            OSSF + 1tau
     # D:         3lD         SR-56            e mu tau
     # E:         3lE         SR-70            SS + 1 tau
     # F:         3lF         SR-80            A: OSSF, F: e/mu + 1tau
     # I:         3lI         SR-96            3l + 1tau
-    python susy-interface/plotmaker.py 3l 3lA
+
+regions = {
+    'C': ['3lC', 'SR-42', 'OSSF + 1tau'             ], 
+    'D': ['3lD', 'SR-56', 'e mu tau'                ],
+    'E': ['3lE', 'SR-70', 'SS + 1 tau'              ],
+    'F': ['3lF', 'SR-80', 'A: OSSF, F: e/mu + 1tau' ],
+    'I': ['4lI', 'SR-96', '3l + 1tau'               ]
+    }
+
+"""
+"""
 
 
 
+import optparse
+# Command line options
+usage = 'usage: %prog [--newData]'
+parser = optparse.OptionParser(usage)
+parser.add_option('-i', '--input',          dest='inputDir',       help='input directory',        default='/pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/',           type='string')
+parser.add_option('-o', '--output',         dest='outputDir',      help='output directory',       default='~/www/susyRA7/',           type='string')
+parser.add_option('-a', '--action',         dest='action',         help='which action to perform', default='generalplots', type='string')
+parser.add_option('-p', '--pretend',        dest='pretend',        help='only print commands out', action='store_true')
+
+(opt, args) = parser.parse_args()
+
+inputDir  = opt.inputDir
+outputDir = opt.outputDir
+action    = opt.action
+pretend   = opt.pretend
+blind = '--flags "-X blinding"'
 
 
-fi
+if(action=='generalplots'):
+    
+    cmd = 'python susy-interface/plotmaker.py 3l 3lA {inputDir} {outputDir} -l 12.9 --make data --plots br -o SR {blind} --pretend'.format(inputDir=inputDir,outputDir=outputDir,blind=blind)
+    command(cmd, pretend)
 
+elif(action=='tauopt'):
+    
+    for region in regions:
+        cmd = 'python susy-interface/plotmaker.py 3l {region} {inputDir} {outputDir} -l 12.9 --make data --plots br -o {sr} {blind} --pretend'.format(region=regions[region][0],inputDir=inputDir,outputDir=outputDir,blind=blind,sr=regions[region][1])
+        command(cmd,pretend)
+        
 
-exit 0
+print 'Everything is done now'
