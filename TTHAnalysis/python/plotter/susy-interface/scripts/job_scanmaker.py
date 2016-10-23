09267cbaf9a12fc29a934e22ff27b6f4475214a5
Constantin Heidegger <constantin.heidegger@cern.ch>
2016-10-23 19:28:15 +0200

    updates for central merge

--- TTHAnalysis/python/plotter/susy-interface/scripts/job_scanmaker.py
+++ TTHAnalysis/python/plotter/susy-interface/scripts/job_scanmaker.py
@@ -57,7 +57,7 @@ def div(num, den):
 	if den==0: return 1
 	return float(num)/den
 
-def doBlackMagicMetShit(infile, outfile, sig, jec, met, wVars):
+def doMetVariation(infile, outfile, sig, jec, met, wVars):
 	global mass1, mass2, q2file, q2syntax, q2acc
 	## Q2ACC
 	if q2file:
@@ -164,7 +164,7 @@ cmd(cmdbase.format(MCA=mcadir + "/mca_acc_"+name+".txt", SYS="", O=outdir + "/ac
 
 
 ## get central value of acceptance 
-doBlackMagicMetShit(accdir + "/common/SR.input.root", accdir + "/acc_SR.input.root", sig, thejec, themet, wVars)
+doMetVariation(accdir + "/common/SR.input.root", accdir + "/acc_SR.input.root", sig, thejec, themet, wVars)
 
 
 ## prepare the proper job
