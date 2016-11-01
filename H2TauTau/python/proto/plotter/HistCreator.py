b7df1bdd14dd3360513ae7f83b835b04b73c43a5
Jan Steggemann <jan.steggemann@cern.ch>
2016-11-01 15:25:57 +0100

    Add tree writing facility to plotting and remove stitching code

--- H2TauTau/python/proto/plotter/HistCreator.py
+++ H2TauTau/python/proto/plotter/HistCreator.py
@@ -1,6 +1,8 @@
 import pickle
 import hashlib
 
+from array import array
+
 # Adds MultiDraw method to ROOT.TTree
 import CMGTools.H2TauTau.proto.plotter.MultiDraw
 
@@ -10,7 +12,7 @@ from CMGTools.H2TauTau.proto.plotter.DataMCPlot import DataMCPlot
 
 from CMGTools.RootTools.DataMC.Histogram import Histogram
 
-from ROOT import TH1F
+from ROOT import TH1F, TFile, TTree, TTreeFormula
 
 
 def initHist(hist, vcfg):
@@ -213,6 +215,99 @@ def createHistogram(hist_cfg, all_stack=False, verbose=False, friend_func=None):
     return plot
 
 
+def fillIntoTree(out_tree, branches, cfg, hist_cfg, plot, verbose, friend_func):
+    if isinstance(cfg, HistogramCfg):
+        # Loop over sub-cfgs and fill them
+        for sub_cfg in cfg.cfgs:
+            fillIntoTree(out_tree, branches, sub_cfg, cfg, plot, verbose, friend_func)
+        return
+
+    vcfgs = hist_cfg.vars
+
+    file_name = '/'.join([cfg.ana_dir, cfg.dir_name, cfg.tree_prod_name, 'tree.root'])
+
+    # Attaches tree to plot
+    ttree = plot.readTree(file_name, cfg.tree_name, verbose=verbose, friend_func=friend_func)
+
+    norm_cut = hist_cfg.cut
+    shape_cut = hist_cfg.cut
+
+    if cfg.norm_cut:
+        norm_cut = cfg.norm_cut
+
+    if cfg.shape_cut:
+        shape_cut = cfg.shape_cut
+
+    weight = hist_cfg.weight
+    if cfg.weight_expr:
+        weight = '*'.join([weight, cfg.weight_expr])
+
+    if hist_cfg.weight:
+        norm_cut = '({c}) * {we}'.format(c=norm_cut, we=weight)
+        shape_cut = '({c}) * {we}'.format(c=shape_cut, we=weight)
+
+    # and this one too
+    sample_weight = 1.
+    if not cfg.is_data:
+        sample_weight = hist_cfg.lumi*cfg.xsec/cfg.sumweights*cfg.scale
+
+    full_weight = array('f', [0.])
+    out_tree.Branch('full_weight', full_weight, 'full_weight/F')
+    formula = TTreeFormula('weight_formula', weight, ttree)
+    formula.GetNdata()
+
+    # Add weight as tree variable
+    # Then loop over ttree
+    # And save this to the other tree
+    # 
+
+    # Create TTreeFormulas for all vars
+    for var in vcfgs:
+        if var.drawname != var.name and not hasattr(var, 'formula'):
+            var.formula = TTreeFormula('formula'+var.name, var.drawname, ttree)
+            var.formula.GetNdata()
+
+    for i in xrange(ttree.GetEntries()):
+        ttree.GetEntry(i)
+        w = formula.EvalInstance()
+        if w == 0.:
+            continue
+        full_weight[0] = w * sample_weight
+        for branch, var in zip(branches, vcfgs):
+            branch[0] = var.formula.EvalInstance() if hasattr(var, 'formula') else getattr(ttree, var.name)
+        out_tree.Fill()
+
+
+    if shape_cut != norm_cut:
+        print 'WARNING: different norm and shape cuts currently not supported in HistCreator.createTrees'
+
+
+def createTrees(hist_cfg, out_dir, verbose=False, friend_func=None):
+    '''Writes out TTrees from histogram configuration for each contribution. 
+    Takes list of variables attached to histogram config (hist_cfg.vars) to 
+    create branches.
+    '''
+    plot = DataMCPlot(hist_cfg.name) # Used to cache TTrees
+    vcfgs = hist_cfg.vars
+    for cfg in hist_cfg.cfgs:
+        
+        out_file = TFile('/'.join([out_dir, hist_cfg.name + '_' + cfg.name + '.root']), 'RECREATE')
+
+        out_tree = TTree('tree', '')
+
+        # Create branches for all variables
+        branches = [array('f', [0.]) for i in xrange(len(vcfgs))]
+        for branch_name, branch in zip([v.name for v in vcfgs], branches):
+            out_tree.Branch(branch_name, branch, branch_name+'/F')
+
+        fillIntoTree(out_tree, branches, cfg, hist_cfg, plot, verbose, friend_func)
+
+        out_file.cd()
+        out_file.Write()
+        out_file.Close()
+    return plot
+
+
 def setSumWeights(sample, weight_dir='MCWeighter'):
     if isinstance(sample, HistogramCfg) or sample.is_data:
         return
@@ -227,93 +322,3 @@ def setSumWeights(sample, weight_dir='MCWeighter'):
         # print 'Warning: could not find sum weights information for sample', sample.name
         pass
 
-
-class StitchingWeightForW(object):
-
-    def __init__(self, Ninc=0, N1=0, N2=0, N3=0, N4=0):
-
-        self.k_factor = 1.22125248
-        self.sample_dict = {
-            'WJetsToLNu_LO': {'LOxs': 50380},
-            'W1JetsToLNu_LO': {'LOxs': 9644.5},
-            'W2JetsToLNu_LO': {'LOxs': 3144.5},
-            'W3JetsToLNu_LO': {'LOxs': 954.8},
-            'W4JetsToLNu_LO': {'LOxs': 485.6},
-        }
-
-        self.eff_l_inc = Ninc/self.sample_dict['WJetsToLNu_LO']['LOxs']
-        self.eff_l_1jet = N1/self.sample_dict['W1JetsToLNu_LO']['LOxs']
-        self.eff_l_2jet = N2/self.sample_dict['W2JetsToLNu_LO']['LOxs']
-        self.eff_l_3jet = N3/self.sample_dict['W3JetsToLNu_LO']['LOxs']
-        self.eff_l_4jet = N4/self.sample_dict['W4JetsToLNu_LO']['LOxs']
-
-        self.eff_l = [
-            self.eff_l_1jet,
-            self.eff_l_2jet,
-            self.eff_l_3jet,
-            self.eff_l_4jet
-        ]
-
-    def returnWeight(self, Nparton):
-
-        if Nparton < 0 or Nparton > 4:
-            print '[ERROR] Not correct Nparton'
-
-        if Nparton == 0:
-            return self.k_factor/self.eff_l_inc
-        else:
-            return self.k_factor/(self.eff_l_inc + self.eff_l[Nparton-1])
-
-
-class StitchingWeightForDY(object):
-
-    def __init__(self, Ninc=0, Ninc_ext=0, N1=0, N2=0, N3=0, N4=0, Nhigh=0):
-
-        self.k_factor = 1.21622931
-
-        self.sample_dict = {
-            'DYJetsToLL_M50_LO': {'LOxs': 4954},
-            'DY1JetsToLL_M50_LO': {'LOxs': 1012.5},
-            'DY2JetsToLL_M50_LO': {'LOxs': 332.8},
-            'DY3JetsToLL_M50_LO': {'LOxs': 101.8},
-            'DY4JetsToLL_M50_LO': {'LOxs': 54.8},
-            'DYJetsToTauTau_M150_LO': {'LOxs': 6.7}
-        }
-
-        self.eff_l_inc = Ninc/self.sample_dict['DYJetsToLNu_LO']['LOxs']
-        self.eff_l_inc_ext = Ninc_ext/self.sample_dict['DYJetsToLNu_LO']['LOxs']
-        self.eff_l_high = Nhigh/self.sample_dict['DYJetsToTauTau_M150_LO']['LOxs']
-        self.eff_l_1jet = N1/self.sample_dict['DY1JetsToLNu_LO']['LOxs']
-        self.eff_l_2jet = N2/self.sample_dict['DY2JetsToLNu_LO']['LOxs']
-        self.eff_l_3jet = N3/self.sample_dict['DY3JetsToLNu_LO']['LOxs']
-        self.eff_l_4jet = N4/self.sample_dict['DY4JetsToLNu_LO']['LOxs']
-
-        self.eff_l = [
-            self.eff_l_1jet,
-            self.eff_l_2jet,
-            self.eff_l_3jet,
-            self.eff_l_4jet,
-        ]
-
-    def returnWeight(self, Nparton, Mass, Decay):
-
-        if Nparton < 0 or Nparton > 4:
-            print '[ERROR] Not correct Nparton'
-
-        if Decay == 'TauTau':
-            if Nparton == 0:
-                if Mass < 150:
-                    return self.k_factor/(self.eff_l_inc + self.eff_l_inc_ext)
-                else:
-                    return self.k_factor/(self.eff_l_inc + self.eff_l_inc_ext + self.eff_l_high)
-            else:
-                if Mass < 150:
-                    return self.k_factor/(self.eff_l_inc + self.eff_l_inc_ext + self.eff_l[Nparton-1])
-                else:
-                    return self.k_factor/(self.eff_l_inc + self.eff_l_inc_ext + self.eff_l[Nparton-1] + self.eff_l_high)
-
-        else:
-            if Nparton == 0:
-                return self.k_factor/(self.eff_l_inc + self.eff_l_inc_ext)
-            else:
-                return self.k_factor/(self.eff_l_inc + self.eff_l_inc_ext + self.eff_l[Nparton-1])
