416d725579ef55d92f3779da39ab054123a9ffde
Pietro Vischia <pietro.vischia@gmail.com>
2016-11-02 19:16:26 +0100

    Susy interface does not like commas

--- TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/makeFTs.sh
+++ TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/makeFTs.sh
@@ -9,7 +9,8 @@ if [ "$1" == "btag" ]; then
 
 elif [ "$1" == "taus" ]; then
     python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules leptonJetReCleanerNoCleanTausSusyEWK3L --accept WZZ -q local --direct --pretend
-    python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules tauFakesBuilderEWKMini,tauFakesBuilderEWKRecl   --accept WZZ -q local --direct --pretend
+    python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules tauFakesBuilderEWKMini   --accept WZZ -q local --direct --pretend
+    python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules tauFakesBuilderEWKRecl   --accept WZZ -q local --direct --pretend
 
 fi
 
