19c16e66b0ae1065a46007c204950fb6780c6cce
botta <cristina.botta@cern.ch>
2016-11-10 16:34:49 +0100

    small updates for SOS

--- TTHAnalysis/python/plotter/susy-sos/functionsSOS.cc
+++ TTHAnalysis/python/plotter/susy-sos/functionsSOS.cc
@@ -1,3 +1,19 @@
+#include <cmath>
+#include <map>
+#include "Math/GenVector/LorentzVector.h"
+#include "Math/GenVector/PtEtaPhiM4D.h"
+#include "Math/GenVector/PxPyPzM4D.h"
+#include "Math/GenVector/Boost.h"
+#include "TLorentzVector.h"
+#include "TH2Poly.h"
+#include "TGraphAsymmErrors.h"
+#include "TH1F.h"
+#include "TFile.h"
+#include "PhysicsTools/Heppy/interface/Davismt2.h"
+#include "TSystem.h"
+
+
+
 float mass_tautau( float Met_Pt, float Met_Phi,  float l1_Pt, float l1_Eta, float l1_Phi, float l2_Pt, float l2_Eta, float l2_Phi ) {
   typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> > PtEtaPhiMVector;
   typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>   > PxPyPzMVector;
@@ -33,16 +49,6 @@ int SR_bins_stop(float ptlep1){
 }
 
 
-float metmm_pt(int pdg1, float pt1, float phi1, int pdg2, float pt2, float phi2, float metpt, float metphi) {
-  if (abs(pdg1)==13 && abs(pdg2)==13) return pt_3(pt1,phi1,pt2,phi2,metpt,metphi);
-  else if (abs(pdg1)==13 && !(abs(pdg2)==13)) return pt_2(pt1,phi1,metpt,metphi);
-  else if (!(abs(pdg1)==13) && abs(pdg2)==13) return pt_2(pt2,phi2,metpt,metphi);
-  else if (!(abs(pdg1)==13) && !(abs(pdg2)==13)) return metpt;
-  else return -99;
-}
-
-
-
 float eleWPVVL(float pt, float etaSc, float mva){
   if (pt<=10 && ((abs(etaSc)<0.8 && mva>-0.265) || (abs(etaSc)>=0.8 && abs(etaSc)<1.479 && mva > -0.556) || (abs(etaSc)>=1.479 && mva>-0.6))) return 1;
   else if (pt>10 && ((abs(etaSc)<0.8 && mva > 0.87) || (abs(etaSc)>=0.8 && abs(etaSc)<1.479 && mva > 0.30) || (abs(etaSc)>=1.479 && mva >-0.30))) return 1;
@@ -56,3 +62,4 @@ float eleWPT(float pt, float etaSc, float mva){
   else return 0;
 }
 
+void functionsSOS() {}
--- TTHAnalysis/python/plotter/susy-sos/sos_plots.py
+++ TTHAnalysis/python/plotter/susy-sos/sos_plots.py
@@ -23,9 +23,9 @@ def base(selection):
     if dowhat == "plots": CORE+=" --lspam 'CMS Preliminary' --legendWidth 0.14 --legendFontSize 0.04"
     GO = ""
     if selection=='2los':
-        if (dowhat != "limits") : GO="susy-sos/susy-sos/mca-2los-mc.txt susy-sos/2los_tight.txt "
+        if (dowhat != "limits") : GO="susy-sos/mca-2los-mc.txt susy-sos/2los_tight.txt "
         GO="%s %s"%(CORE,GO) 
-        GO="%s -L susy-sos/lepton_trigger_SF.cc -W 'leptonSF_SOS(LepGood1_pdgId,LepGood1_pt,LepGood1_eta,0)*leptonSF_SOS(LepGood2_pdgId,LepGood2_pt,LepGood2_eta,0)*triggerSF_SOS(met_pt,metmm_pt(LepGood1_pdgId,LepGood1_pt,LepGood1_phi,LepGood2_pdgId,LepGood2_pt,LepGood2_phi,met_pt,met_phi),0)*puw2016_vtx_13fb(nVert)*eventBTagSF'"%GO 
+        GO="%s -L susy-sos/functionsSOS.cc -L susy-sos/lepton_trigger_SF.cc -W 'leptonSF_SOS(LepGood1_pdgId,LepGood1_pt,LepGood1_eta,0)*leptonSF_SOS(LepGood2_pdgId,LepGood2_pt,LepGood2_eta,0)*triggerSF_SOS(met_pt,metmm_pt(LepGood1_pdgId,LepGood1_pt,LepGood1_phi,LepGood2_pdgId,LepGood2_pt,LepGood2_phi,met_pt,met_phi),0)*puw2016_vtx_13fb(nVert)*eventBTagSF'"%GO 
         if dowhat == "plots": GO+=" susy-sos/2los_plots.txt"
     else:
         raise RuntimeError, 'Unknown selection'
