4964e69b8984d057cecbef793e3b77a52cc25768
Pietro Vischia <pietro.vischia@gmail.com>
2016-11-19 00:46:36 +0100

    Commands for tau fts

--- TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/makeFTs.sh
+++ TTHAnalysis/python/plotter/susy-interface/cmds/tau-ewkino/makeFTs.sh
@@ -8,9 +8,19 @@ if [ "$1" == "btag" ]; then
     python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules eventBTagWeight --accept WZZ -q local --direct --pretend
 
 elif [ "$1" == "taus" ]; then
-    # python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules leptonJetReCleanerNoCleanTausSusyEWK3L --accept WZZ -q local --direct --pretend
-    python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules tauFakesBuilderEWKMini --accept WZZ -q local --direct --pretend
-    # python susy-interface/friendmaker.py 3l 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules tauFakesBuilderEWKRecl   --accept WZZ -q local --direct --pretend
+    MODULE=""
+    if [ "$2" == "" ]; then 
+        MODULE="leptonJetReCleanerNoCleanTausSusyEWK3L"
+    elif [ "$2" == "tauMini" ]; then
+        MODULE="tauFakesBuilderEWKMini"
+    elif [ "$2" == "tauRecl" ]; then
+        MODULE="tauFakesBuilderEWKRecl"
+    fi
+    PRETEND=" -q local --direct --pretend"
+    PRETEND=" -q batch --direct "
+    ONLY=" --accept WZZ "
+    ONLY="" 
+    python susy-interface/friendmaker.py taustudies 3lA /pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/ /pool/ciencias/HeppyTrees/RA7/estructura/testbtag/ --modules ${MODULE} ${ONLY} ${PRETEND}
 
 fi
 
