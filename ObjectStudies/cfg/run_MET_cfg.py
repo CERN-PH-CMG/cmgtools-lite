1ceb06764064cf58043cfa2674e61cbffb632c33
maria <dalfonso@cern.ch>
2016-11-20 16:20:14 +0100

    updated METconfig

--- ObjectStudies/cfg/run_MET_cfg.py
+++ ObjectStudies/cfg/run_MET_cfg.py
@@ -17,15 +17,14 @@ triggers_mumu_noniso_Dominick = ['HLT_Mu30_TkMu11_v3','HLT_Mu50_v4', 'HLT_TkMu50
 triggers_ee_noniso_Dominick = ['HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW_v6','HLT_Ele105_CaloIdVT_GsfTrkIdT_v6']
 
 ##goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276384_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
-goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
-
+goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
 
 #-------- INITIAL FLAG
-isDiJet=False
-isMonoJet=False
-isZSkim=False
-is1L=False
-is1PH=False
+isDiJet = False
+isMonoJet = False
+isZSkim = False
+is1L = False
+is1PH = False
 isEle = False # default is diMuon
 is25ns = True
 
@@ -62,7 +61,7 @@ elif test==2:
     selectedComponents = [ DYJetsToLL_M50 ]
     for comp in selectedComponents:
         comp.splitFactor = 1
-        comp.files = ['root://eoscms///store/mc/RunIISpring16MiniAODv2/TTbarDMJets_pseudoscalar_Mchi-1_Mphi-100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/40000/C06A61EE-EF25-E611-870A-02163E011A12.root']
+        comp.files = ['root://eoscms////store/relval/CMSSW_8_0_20/RelValTTbar_13/MINIAODSIM/PU25ns_80X_mcRun2_asymptotic_2016_TrancheIV_v4_Tr4GT_v4-v1/00000/A8C282AE-D37A-E611-8603-0CC47A4C8ECE.root']
 
 #elif test==2:
 #    selectedComponents = [ TTJets_50ns ]
@@ -137,9 +136,9 @@ elif test==7:
 elif test==13:
     isZSkim=True
     if isEle:
-        selectedComponents = [ DoubleEG_Run2016B_PromptReco_v2, DoubleEG_Run2016C_PromptReco_v2, DoubleEG_Run2016D_PromptReco_v2 ]
+        selectedComponents = [ DoubleEG_Run2016B_PromptReco_v2, DoubleEG_Run2016C_PromptReco_v2, DoubleEG_Run2016D_PromptReco_v2, DoubleEG_Run2016E_PromptReco_v2, DoubleEG_Run2016F_PromptReco_v1,DoubleEG_Run2016G_PromptReco_v1, DoubleEG_Run2016H_PromptReco_v2, DoubleEG_Run2016H_PromptReco_v3 ]
     else:
-        selectedComponents = [ DoubleMuon_Run2016B_PromptReco_v2, DoubleMuon_Run2016C_PromptReco_v2, DoubleMuon_Run2016D_PromptReco_v2 ]
+        selectedComponents = [  DoubleMuon_Run2016B_PromptReco_v2, DoubleMuon_Run2016C_PromptReco_v2, DoubleMuon_Run2016D_PromptReco_v2, DoubleMuon_Run2016E_PromptReco_v2, DoubleMuon_Run2016F_PromptReco_v1, DoubleMuon_Run2016G_PromptReco_v1, DoubleMuon_Run2016H_PromptReco_v2, DoubleMuon_Run2016H_PromptReco_v3 ]
     for comp in selectedComponents:
 #        comp.splitFactor = 1
 #        comp.files = comp.files[5:10]
@@ -222,7 +221,7 @@ elif test==19:
 elif test==23:
     isZSkim=True
     is25ns=True
-    selectedComponents = [ DYJetsToLL_M50, TTJets_DiLepton, TTJets_DiLepton_ext, ZZTo4L, ZZTo2L2Q, ZZTo2L2Nu, WWTo2L2Nu, WZTo2L2Q, WZTo3LNu ] + TriBosons + [ TBar_tWch, T_tWch, TToLeptons_tch_powheg, TBarToLeptons_tch_powheg, TToLeptons_sch_amcatnlo + TTJets_SingleLeptonFromTbar, TTJets_SingleLeptonFromT]
+    selectedComponents = [ DYJetsToLL_M50, TTJets_DiLepton, TTJets_DiLepton_ext, ZZTo4L, ZZTo2L2Q, ZZTo2L2Nu, WWTo2L2Nu, WZTo2L2Q, WZTo3LNu ] + TriBosons + [ TBar_tWch, T_tWch, TToLeptons_tch_powheg, TBarToLeptons_tch_powheg, TToLeptons_sch_amcatnlo, TTJets_SingleLeptonFromTbar, TTJets_SingleLeptonFromT]
     for comp in selectedComponents:
 # no trigger on MC for now
 #        if isEle:
@@ -231,7 +230,7 @@ elif test==23:
 #            comp.triggers = triggers_mumu
 #        comp.splitFactor = 1
 #        comp.files = comp.files[:1]
-        comp.splitFactor = 200
+        comp.splitFactor = 1000
         comp.files = comp.files[:]
 
 elif test==25:
@@ -279,6 +278,13 @@ metSequence = cfg.Sequence(
 ###---- to switch off the comptrssion
 #treeProducer.isCompressed = 0
 
+# replace the trigger for the reHLT
+#if test==23:
+#    triggerFlagsAna.processName = 'HLT2'
+if test==2:
+    triggerFlagsAna.processName = 'RECO'
+    eventFlagsAna.processName = 'RECO'
+
 # -------------------- lepton modules below needed for the Muon Selection
 
 if isZSkim:
@@ -320,16 +326,21 @@ if is1PH and test==17:
 # -------------------- FINE TUNE CONTENT
 # --------------------
 
-if isZSkim or is1PH:
+if isZSkim or is1PH or test==2:
     met_globalObjects.update({
             "met_jecUp" : NTupleObject("met_jecUp", metType, help="PF E_{T}^{miss}, after type 1 corrections with JEC up variation"),
             "met_jecDown" : NTupleObject("met_jecDown", metType, help="PF E_{T}^{miss}, after type 1 corrections with JEC down variation"),
             "met_shifted_UnclusteredEnUp" : NTupleObject("met_shifted_UnclusteredEnUp", metType, help="PF E_{T}^{miss}, after type 1 corrections with met unclustered Up"),
             "met_shifted_UnclusteredEnDown" : NTupleObject("met_shifted_UnclusteredEnDown", metType, help="PF E_{T}^{miss}, after type 1 corrections with met unclustered Down"),
+            "met_shifted_JetResUp" : NTupleObject("met_shifted_JetResUp", metType, help="PF E_{T}^{miss}, after type 1 corrections with jet resolution Up"),
+            "met_shifted_JetResDown" : NTupleObject("met_shifted_JetResDown", metType, help="PF E_{T}^{miss}, after type 1 corrections with jet resolution Down"),
+            #####
             "metPuppi_jecUp" : NTupleObject("metPuppi_jecUp", metType, help="PF E_{T}^{miss}, after type 1 corrections with JEC up variation (Puppi)"),
             "metPuppi_jecDown" : NTupleObject("metPuppi_jecDown", metType, help="PF E_{T}^{miss}, after type 1 corrections with JEC down variation (Puppi)"),
             "metPuppi_shifted_UnclusteredEnUp" : NTupleObject("metPuppi_shifted_UnclusteredEnUp", metType, help="PF E_{T}^{miss}, after type 1 corrections with met unclustered Up"),
             "metPuppi_shifted_UnclusteredEnDown" : NTupleObject("metPuppi_shifted_UnclusteredEnDown", metType, help="PF E_{T}^{miss}, after type 1 corrections with met unclustered Down"),
+            "metPuppi_shifted_JetResUp" : NTupleObject("metPuppi_shifted_JetResUp", metType, help="PF E_{T}^{miss}, after type 1 corrections with jet resolution Up"),
+            "metPuppi_shifted_JetResDown" : NTupleObject("metPuppi_shifted_JetResDown", metType, help="PF E_{T}^{miss}, after type 1 corrections with jet resolution Down"),
             })
 
 if is1PH:
@@ -428,18 +439,16 @@ import subprocess
 if comp.isData:
     ## DATA 25ns
     removeResiduals = False
-    uncFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jec/Spring16_25nsV6_DATA_Uncertainty_AK4PFchs.txt'
-    jecDBFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jec/Spring16_25nsV6_DATA.db'
-    jecEra    = 'Spring16_25nsV6_DATA'
-    jerDBFile = os.environ['CMSSW_BASE']+'/src/PhysicsTools/PatUtils/data/JER/Spring16_25nsV6_MC.db'
+    jecDBFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jec/Spring16_25nsV8_DATA.db'
+    jecEra    = 'Spring16_25nsV8_DATA'
+    jerDBFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jer/Spring16_25nsV6_MC.db'
     jerEra    = 'Spring16_25nsV6'
 else:
     ## MC 25ns
     removeResiduals = False
-    uncFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jec/Spring16_25nsV6_MC_Uncertainty_AK4PFchs.txt'
-    jecDBFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jec/Spring16_25nsV6_MC.db'
-    jecEra    = 'Spring16_25nsV6_MC'
-    jerDBFile = os.environ['CMSSW_BASE']+'/src/PhysicsTools/PatUtils/data/JER/Spring16_25nsV6_MC.db'
+    jecDBFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jec/Spring16_25nsV8_MC.db'
+    jecEra    = 'Spring16_25nsV8_MC'
+    jerDBFile = os.environ['CMSSW_BASE']+'/src/CMGTools/RootTools/data/jer/Spring16_25nsV6_MC.db'
     jerEra    = 'Spring16_25nsV6'
 
 preprocessorFile = "$CMSSW_BASE/tmp/MetType1_jec_%s.py"%(jecEra)
