12bbe9a0619f50d4876e92c686d09cca39e1ae76
Jan Steggemann <jan.steggemann@cern.ch>
2017-01-19 17:47:57 +0100

    Add summer 16 sample folder

---
+++ H2TauTau/python/proto/samples/summer16/higgs.py
@@ -0,0 +1,90 @@
+import copy
+import re 
+from CMGTools.RootTools.yellowreport.YRParser import yrparser13TeV
+# from CMGTools.H2TauTau.proto.samples.sampleShift import sampleShift
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+
+creator = ComponentCreator()
+
+HiggsGGH125 = creator.makeMCComponent('HiggsGGH125', '/GluGluHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', 'CMS', '.*root', 1.0)
+# HiggsGGH130 = creator.makeMCComponent('HiggsGGH130', '/GluGluHToTauTau_M130_13TeV_powheg_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM', 'CMS', '.*root', 1.0)
+
+# HiggsVBF120 = creator.makeMCComponent('HiggsVBF125', '/VBFHToTauTau_M120_13TeV_powheg_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM', 'CMS', '.*root', 1.0)
+HiggsVBF125 = creator.makeMCComponent('HiggsVBF125', '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', 'CMS', '.*root', 1.0)
+# HiggsVBF130 = creator.makeMCComponent('HiggsVBF125', '/VBFHToTauTau_M130_13TeV_powheg_pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM', 'CMS', '.*root', 1.0)
+
+
+
+
+HiggsTTH125 = creator.makeMCComponent('HiggsTTH125', '/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM', 'CMS', '.*root', 1.0)
+
+
+#############
+
+mc_higgs_vbf = [
+    HiggsVBF125,
+    # HiggsVBFtoWW125,
+]
+
+mc_higgs_ggh = [
+    HiggsGGH125,
+    # HiggsGGHtoWW125,
+]
+
+mc_higgs_vh = [
+    # HiggsVH125,
+    # HiggsVHtoWW125
+]
+
+mc_higgs_tth = [
+    HiggsTTH125
+]
+
+mc_higgs = copy.copy(mc_higgs_vbf)
+mc_higgs.extend(mc_higgs_ggh)
+
+mc_higgs.extend(mc_higgs_vh)
+mc_higgs.extend(mc_higgs_tth)
+
+
+pattern = re.compile('Higgs(\D+)(\d+)')
+for h in mc_higgs:
+    m = pattern.match( h.name )
+    process = m.group(1)
+    
+    isToWW = False 
+    isInclusive = False
+    if 'toWW' in process :
+        process = process.replace('toWW', '')
+        isToWW = True
+    if 'Inclusive' in process:
+        process = process.replace('Inclusive', '')
+        isInclusive = True
+          
+    mass = float(m.group(2))
+    xSection = 0.
+    try:
+        if process == 'VH':
+            xSection += yrparser13TeV.get(mass)['WH']['sigma']
+            xSection += yrparser13TeV.get(mass)['ZH']['sigma']
+        else:
+            xSection += yrparser13TeV.get(mass)[process]['sigma']
+    except KeyError:
+        print 'Higgs mass', mass, 'not found in cross section tables. Interpolating linearly at +- 1 GeV...'
+        if process=='VH':
+            xSection += 0.5 * (yrparser13TeV.get(mass-1.)['WH']['sigma'] + xSection + yrparser13TeV.get(mass+1.)['WH']['sigma'])
+            xSection += 0.5 * (yrparser13TeV.get(mass-1.)['ZH']['sigma'] + yrparser13TeV.get(mass+1.)['ZH']['sigma'])
+        else:
+            xSection += 0.5 * (yrparser13TeV.get(mass-1.)[process]['sigma'] + yrparser13TeV.get(mass+1.)[process]['sigma'])
+
+    if isToWW :
+        br = yrparser13TeV.get(mass)['H2B']['WW']
+    elif isInclusive:
+        br = 1.
+    else :
+        br = yrparser13TeV.get(mass)['H2F']['tautau']
+      
+    h.xSection = xSection*br
+    h.branchingRatio = br
+    print h.name, 'sigma*br =', h.xSection, 'sigma =', xSection, 'br =', h.branchingRatio
+
---
+++ H2TauTau/python/proto/samples/summer16/htt_common.py
@@ -0,0 +1,139 @@
+from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import WJetsToLNu,  WWTo2L2Nu, QCD_Mu5, QCDPtEMEnriched, QCDPtbcToE, TBar_tch_powheg, DYNJets, WNJets, T_tch_powheg, DYJetsToLL_M10to50_LO #DYJetsToLL_M50_LO WJetsToLNu_LO
+
+from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import DYJetsToLL_M50_LO_ext as DYJetsToLL_M50_LO
+from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import TBar_tWch_ext as TBar_tWch
+from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import T_tWch_ext as T_tWch
+from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import WJetsToLNu_LO
+# WJetsToLNu_HT100to200, WJetsToLNu_HT200to400, WJetsToLNu_HT400to600, WJetsToLNu_HT600toInf, QCD_Mu15,  DYJetsToTauTau_M150_LO, 
+from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import TT_pow
+from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import SingleMuon_Run2016B_23Sep2016, SingleElectron_Run2016B_23Sep2016, MuonEG_Run2016B_23Sep2016, Tau_Run2016B_23Sep2016
+from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import SingleMuon_Run2016C_23Sep2016, SingleElectron_Run2016C_23Sep2016, MuonEG_Run2016C_23Sep2016, Tau_Run2016C_23Sep2016
+from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import SingleMuon_Run2016D_23Sep2016, SingleElectron_Run2016D_23Sep2016, MuonEG_Run2016D_23Sep2016, Tau_Run2016D_23Sep2016
+from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import SingleMuon_Run2016E_23Sep2016, SingleElectron_Run2016E_23Sep2016, MuonEG_Run2016E_23Sep2016, Tau_Run2016E_23Sep2016
+from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import SingleMuon_Run2016F_23Sep2016, SingleElectron_Run2016F_23Sep2016, MuonEG_Run2016F_23Sep2016, Tau_Run2016F_23Sep2016
+from CMGTools.RootTools.samples.samples_13TeV_DATA2016 import SingleMuon_Run2016G_23Sep2016, SingleElectron_Run2016G_23Sep2016, MuonEG_Run2016G_23Sep2016, Tau_Run2016G_23Sep2016
+from CMGTools.RootTools.samples.samples_13TeV_RunIISummer16MiniAODv2 import ZZTo4L, WZTo1L3Nu, WWTo1L1Nu2Q, WZTo1L1Nu2Q, ZZTo2L2Q, WZTo2L2Q, VVTo2L2Nu, WZTo3LNu_amcatnlo
+
+# from CMGTools.RootTools.samples.samples_13TeV_RunIISpring16MiniAODv2 import DYJetsToLL_M10to50_ext1
+# DY1JetsToLL_M50_LO, DY2JetsToLL_M50_LO, DY3JetsToLL_M50_LO, DY4JetsToLL_M50_LO,
+
+from CMGTools.H2TauTau.proto.samples.spring16.higgs import HiggsGGH125, HiggsVBF125, HiggsTTH125
+from CMGTools.H2TauTau.proto.samples.spring16.higgs_susy import mc_higgs_susy_gg, mc_higgs_susy_bb
+
+from CMGTools.H2TauTau.proto.samples.spring16.higgs_susy import HiggsSUSYGG160 as ggh160
+
+# 21 July
+
+json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt'
+lumi = 12918.1407
+
+# 15 November
+json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
+# lumi = 36220.
+
+
+# lumi_2016G = 1659.026988066 # 2016 G
+lumi_2016G = 1080.463515944 # 2016 G HLT_IsoMu22_v3
+
+json_tt = json
+lumi_tt = lumi
+
+# Set cross sections to HTT values
+
+VVTo2L2Nu.xSection = 11.95
+WWTo1L1Nu2Q.xSection = 49.997
+ZZTo2L2Q.xSection = 3.22
+ZZTo4L.xSection = 1.212
+WZTo3LNu_amcatnlo.xSection = 5.26
+WZTo2L2Q.xSection = 5.595
+WZTo1L3Nu.xSection = 3.05
+WZTo1L1Nu2Q.xSection = 10.71
+
+w_xsec = 61526.7
+dy_xsec = 5765.4
+
+DYJetsToLL_M50_LO.xSection = dy_xsec
+# DYJetsToLL_M50.xSection = dy_xsec
+
+DYJetsToLL_M50_LO.nevents = [1000.] # Temporary entry
+WJetsToLNu_LO.nevents = [1000.] # Temporary entry
+
+# From https://twiki.cern.ch/twiki/pub/CMS/HiggsToTauTauWorking2015/DYNjetWeights.xls r3
+dy_weight_dict = {
+    (0, 0): 0.669144882/dy_xsec,
+    (0, 150): 0.001329134/dy_xsec,
+    (1, 0): 0.018336763/dy_xsec,
+    (1, 150): 0.001241603/dy_xsec,
+    (2, 0): 0.019627356/dy_xsec,
+    (2, 150): 0.001247156/dy_xsec,
+    (3, 0): 0.021024291/dy_xsec,
+    (3, 150): 0.001252443/dy_xsec,
+    (4, 0): 0.015530181/dy_xsec,
+    (4, 150): 0.001226594/dy_xsec,
+}
+
+def getDYWeight(n_jets, m_gen):
+    if m_gen > 150.:
+        return dy_weight_dict[(n_jets, 150)]
+    return dy_weight_dict[(n_jets, 0)]
+
+for sample in [DYJetsToLL_M50_LO] + DYNJets: # + [DYJetsToTauTau_M150_LO]:
+    # sample.fractions = [0.7, 0.204374, 0.0671836, 0.0205415, 0.0110539]
+
+    sample.weight_func = getDYWeight
+    sample.xSection = dy_xsec
+
+# From https://twiki.cern.ch/twiki/pub/CMS/HiggsToTauTauWorking2015/DYNjetWeights.xls r3
+w_weight_dict = {
+    0:1.304600668/w_xsec,
+    1:0.216233816/w_xsec,
+    2:0.115900663/w_xsec,
+    3:0.058200264/w_xsec,
+    4:0.06275589/w_xsec
+}
+
+def getWWeight(n_jets, m_gen_dummy):
+    return w_weight_dict[n_jets]
+
+for sample in [WJetsToLNu_LO] + WNJets: # 
+
+    sample.weight_func = getWWeight
+    # sample.xSection = w_xsec
+
+WJetsHT = [] # WJetsToLNu_HT100to200, WJetsToLNu_HT200to400, WJetsToLNu_HT400to600, WJetsToLNu_HT600toInf
+
+# Backgrounds
+diboson_nlo = [ZZTo4L, WZTo1L3Nu, WWTo1L1Nu2Q, ZZTo2L2Q,  WZTo2L2Q, WZTo1L1Nu2Q, VVTo2L2Nu, WZTo3LNu_amcatnlo]
+
+essential = [TT_pow, DYJetsToLL_M50_LO, DYJetsToLL_M10to50_LO, TBar_tWch, T_tWch, TBar_tch_powheg, T_tch_powheg, WJetsToLNu_LO]  # WJetsToLNu, 
+
+# Build default background list
+backgrounds = essential
+backgrounds += DYNJets
+# backgrounds += [DYJetsToTauTau_M150_LO, DYJetsToLL_M10to50_ext1]
+backgrounds += WNJets
+backgrounds += diboson_nlo
+backgrounds += []
+
+backgrounds_mu = backgrounds[:]
+# backgrounds_mu += [QCD_Mu15]
+
+backgrounds_ele = backgrounds[:]
+backgrounds_ele += QCDPtEMEnriched
+backgrounds_ele += QCDPtbcToE
+
+# Data
+data_single_muon = [SingleMuon_Run2016B_23Sep2016, SingleMuon_Run2016C_23Sep2016, SingleMuon_Run2016D_23Sep2016, SingleMuon_Run2016E_23Sep2016, SingleMuon_Run2016F_23Sep2016, SingleMuon_Run2016G_23Sep2016]
+data_single_electron = [SingleElectron_Run2016B_23Sep2016, SingleElectron_Run2016C_23Sep2016, SingleElectron_Run2016D_23Sep2016, SingleElectron_Run2016E_23Sep2016, SingleElectron_Run2016F_23Sep2016, SingleElectron_Run2016G_23Sep2016]
+data_muon_electron = [MuonEG_Run2016B_23Sep2016, MuonEG_Run2016C_23Sep2016, MuonEG_Run2016D_23Sep2016, MuonEG_Run2016E_23Sep2016, MuonEG_Run2016F_23Sep2016, MuonEG_Run2016G_23Sep2016]
+data_tau = [Tau_Run2016B_23Sep2016, Tau_Run2016C_23Sep2016, Tau_Run2016D_23Sep2016, Tau_Run2016E_23Sep2016, Tau_Run2016F_23Sep2016, Tau_Run2016G_23Sep2016]
+
+for sample in data_single_muon + data_single_electron + data_muon_electron + data_tau:
+    sample.json = json
+    sample.lumi = lumi if sample != SingleMuon_Run2016G_23Sep2016 else lumi_2016G
+
+# Signals
+sm_signals = [HiggsGGH125, HiggsVBF125, HiggsTTH125]
+mssm_signals = mc_higgs_susy_bb + mc_higgs_susy_gg
+
+sync_list = [ggh160, HiggsVBF125]
