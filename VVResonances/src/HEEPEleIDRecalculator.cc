1172a6ec0c1732bced24b6469decd8da345c1c88
Clemens Lange <clemens.lange@cern.ch>
2017-01-28 20:27:28 +0100

    fix further brackets for HEEPEleIDRecalculator

--- VVResonances/src/HEEPEleIDRecalculator.cc
+++ VVResonances/src/HEEPEleIDRecalculator.cc
@@ -2,19 +2,19 @@
 
 using namespace cmg;
 bool HEEPEleIDRecalculator::id(const pat::Electron& e) {
-        if (fabs(e.superCluster()->eta()<1.4442))
+        if (fabs(e.superCluster()->eta()) < 1.4442)
                 return e.ecalDriven() &&
-                       fabs(e.deltaEtaSeedClusterTrackAtVtx())<0.004 &&
+                       fabs(e.deltaEtaSeedClusterTrackAtVtx()) < 0.004 &&
                        fabs( e.deltaPhiSuperClusterTrackAtVtx())< 0.06 &&
                        (e.hadronicOverEm()<1.0/e.superCluster()->energy()+0.05) &&
-                       (e.e2x5Max()/e.e5x5()>0.94 || e.e1x5()/e.e5x5()>0.83) && fabs(e.dB())<0.02;
+                       (e.e2x5Max()/e.e5x5() > 0.94 || e.e1x5()/e.e5x5() > 0.83) && fabs(e.dB()) < 0.02;
 
-        if (fabs(e.superCluster()->eta()>1.566))
-                return fabs(e.superCluster()->eta())<2.5 &&
-                       e.ecalDriven() && fabs(e.deltaEtaSeedClusterTrackAtVtx())<0.006 &&
-                       fabs( e.deltaPhiSuperClusterTrackAtVtx())< 0.06 &&
-                       (e.hadronicOverEm()<5.0/e.superCluster()->energy()+0.05) &&
-                       fabs(e.dB())<0.05 && e.full5x5_sigmaIetaIeta()<0.03;
+        if (fabs(e.superCluster()->eta()) > 1.566)
+                return fabs(e.superCluster()->eta()) < 2.5 &&
+                       e.ecalDriven() && fabs(e.deltaEtaSeedClusterTrackAtVtx()) < 0.006 &&
+                       fabs(e.deltaPhiSuperClusterTrackAtVtx()) < 0.06 &&
+                       (e.hadronicOverEm() < 5.0/e.superCluster()->energy()+0.05) &&
+                       fabs(e.dB()) < 0.05 && e.full5x5_sigmaIetaIeta() < 0.03;
 
         return false;
 }
@@ -27,7 +27,7 @@ bool HEEPEleIDRecalculator::iso(const pat::Electron& e,const double rho, const s
                 return false;
 
         float neutralIso = e.dr03EcalRecHitSumEt()+e.dr03HcalDepth1TowerSumEt();
-        if (fabs(e.superCluster()->eta()<1.4442))
+        if (fabs(e.superCluster()->eta()) < 1.4442)
                 return neutralIso<2.0+0.03*e.et()+0.28*rho;
 
         if (fabs(e.superCluster()->eta()) > 1.566) {
