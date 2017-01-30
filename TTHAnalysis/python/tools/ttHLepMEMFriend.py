a6ba83feb3f3f356dbb83d20256238db35dc1106
peruzzim <peruzzim@users.noreply.github.com>
2017-01-30 10:49:12 +0100

    updated MEM friend tree producer

--- TTHAnalysis/python/tools/ttHLepMEMFriend.py
+++ TTHAnalysis/python/tools/ttHLepMEMFriend.py
@@ -8,11 +8,12 @@ def _MEMInit():
         print "Loaded MEM interface"
 
 class ttHLepMEMFriend:
-    def __init__(self, config, recllabel='Recl'):
-        self._procs = [ "TTLL", "TTHfl", "TTHsl", "TTW", "TTWJJ", "TTbarfl", "TTbarsl", "TTH" ]
-        self._posts = [ "", "_nHypAll", "_nNull", "_time", "_err", "_chi2" ]
-        self.systsJEC = {0:"", 1:"_jecUp", -1:"_jecDown"}
+    def __init__(self, config, blooseWP, recllabel='Recl'):
+        self._procs = [ "TTLL", "TTHfl", "TTHsl", "TTW"]#, "TTWJJ", "TTbarfl", "TTbarsl", "TTH" ]
+        self._posts = [ ""]#, "_nHypAll", "_nNull", "_time", "_err", "_chi2" ]
+        self.systsJEC = {0:"", 1:"_jecUp", -1:"_jecDown"} # not really used for the moment
         self.inputlabel = '_'+recllabel
+        self.blooseWP = blooseWP
         _MEMInit()
         self.mem = ROOT.MEMFriendTreeCERN();
         self.mem.init(config)
@@ -20,44 +21,44 @@ class ttHLepMEMFriend:
     def listBranches(self):
         return ["MEM_"+p+x for p in self._procs for x in self._posts ]
     def __call__(self,event):
+
+        null_output = dict([("MEM_"+p+x,0) for p in self._procs for x in self._posts ])
+
         nFO = getattr(event,"nLepFO"+self.inputlabel)
-        if nFO < 3: return dict([("MEM_"+p+x,0) for p in self._procs for x in self._posts ])
+        if nFO < 3: return null_output
 
         all_leps = [l for l in Collection(event,"LepGood","nLepGood")]
-        chosen = getattr(event,"iF"+self.inputlabel)
+        chosen = getattr(event,"iLepFO"+self.inputlabel)
         leps = [all_leps[chosen[i]] for i in xrange(nFO)]
         leps.sort(key = lambda lep: -lep.pt)
         for lep in leps[:3]:
             print "lepton pdgId %d pt %.7f eta %.7f phi %.7f mass %.7f" % (lep.pdgId, lep.pt, lep.eta, lep.phi, lep.mass)
 
         _var = 0
-        jetsc = [j for j in Collection(event,"Jet"+self.systsJEC[_var],"nJet"+self.systsJEC[_var])]
-        jetsd = [j for j in Collection(event,"DiscJet"+self.systsJEC[_var],"nDiscJet"+self.systsJEC[_var])]
-        _ijets_list = getattr(event,"iJSel"+self.inputlabel+self.systsJEC[_var])
-        _ijets = [ij for ij in _ijets_list]
-        jets = [ (jetsc[ij] if ij>=0 else jetsd[-ij-1]) for ij in _ijets]
+        jets = [j for j in Collection(event,"JetSel"+self.inputlabel+self.systsJEC[_var],"nJetSel"+self.inputlabel+self.systsJEC[_var])]
         jets.sort(key = lambda j : j.btagCSV, reverse=True)
 
         for i,jet in enumerate(jets):
             print "Jet pt %.7f eta %.7f phi %.7f mass %.7f btag %.3f" % (jet.pt, jet.eta, jet.phi, jet.mass, jet.btagCSV)
 
-        if len(jets) < 2: return dict([("MEM_"+p+x,0) for p in self._procs for x in self._posts ])
+        if len(jets) < 2: return null_output
 
         self.mem.clear();
 
         for lep in leps[:3]:
             self.mem.addLepton(lep.p4(), lep.pdgId)
 
-        bjets = jets[:2]
+        bjets = filter(lambda j : j.btagCSV>self.blooseWP, jets)[:2]
+        if len(bjets)<2: return null_output
+        ljets = filter(lambda j : j not in bjets, jets)
         for j in bjets: 
             #print 'chosen b-jet:',j.pt
             self.mem.addBJet(j.p4(), j.btagCSV)
 
-        if len(jets) >= 4:
+        if len(ljets)>=2:
             print "CATEGORY: ","3l_2b_2j"
             ok = self.mem.setCategory("3l_2b_2j")
             if not ok: raise RuntimeError, "Hypothesis not found"
-            ljets = jets[2:]
             pairs = [(ljets[i1],ljets[i2],(ljets[i1].p4()+ljets[i2].p4()).M()) for i1 in xrange(len(ljets)-1) for i2 in xrange(i1+1,len(ljets)) ]
             def fillTwoPairs(pairs):
                 print 'choosing among %d pairs'%len(pairs)
@@ -77,14 +78,14 @@ class ttHLepMEMFriend:
             #print 'chosen for mw are', [x.pt for x in chosen_mw]," removing from list"
             #pairs = [x for x in pairs if (x[0] not in chosen_mw and x[1] not in chosen_mw)]
             #fillTwoPairs(pairs)
-        elif len(jets) == 3:
+        elif len(ljets)==1:
             print "CATEGORY: ","3l_2b_1j"
             ok = self.mem.setCategory("3l_2b_1j")
             if not ok: raise RuntimeError, "Hypothesis not found"
             #print 'chosen highest pt jet:',jets[2].pt
             self.mem.addJet("jetHighestPt", jets[2].p4(), jets[2].btagCSV)
             #return dict([("MEM_"+p,0) for p in self._procs ]) # FIXME
-        elif len(jets) <= 2:
+        elif len(ljets)==0:
             print "CATEGORY: ","3l_2b_0j"
             ok = self.mem.setCategory("3l_2b_0j")
             if not ok: raise RuntimeError, "Hypothesis not found"
@@ -103,7 +104,10 @@ class ttHLepMEMFriend:
         for pair in ret0:
             print pair.first, pair.second
         print "This was for event ",event.run,event.lumi,event.evt
-        return dict([("MEM_"+p.first,p.second) for p in ret0 ])
+        myout = {}
+        for k in self.listBranches(): myout[k]=0
+        myout.update( dict([("MEM_"+p.first,p.second) for p in ret0 ]) )
+        return myout
         
 if __name__ == '__main__':
     from sys import argv
@@ -114,13 +118,14 @@ if __name__ == '__main__':
     class Tester(Module):
         def __init__(self, name):
             Module.__init__(self,name,None)
-            self.sf = ttHLepMEMFriend(argv[3])
+            self.sf = ttHLepMEMFriend(argv[3], blooseWP = 0.5426)
         def analyze(self,ev):
             print "\nrun %6d lumi %4d event %d: leps %d" % (ev.run, ev.lumi, ev.evt, ev.nLepGood)
             print self.sf(ev)
     el = EventLoop([ Tester("tester") ])
-    el.loop([tree], maxEvents = 5)
+    el.loop([tree], maxEvents = 50)
 
 MODULES = [ 
-    ( 'MEM_3l', lambda : ttHLepMEMFriend("/afs/cern.ch/work/g/gpetrucc/ttH/marco/CMSSW_8_0_11/src/IPHCNtuple/MEM/test/JobsReco/config.cfg") ),
+    ( 'MEM_3l', lambda : ttHLepMEMFriend("/afs/cern.ch/user/p/peruzzi/work/CMSSW_8_0_25/src/IPHCNtuple/MEM/test/JobsReco/config.cfg",
+                                         blooseWP = 0.5426) ),
 ]
