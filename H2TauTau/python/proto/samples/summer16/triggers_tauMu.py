2047d4d3b4ae41d1ecbf2622006aecf1ed1316ec
Jan Steggemann <jan.steggemann@cern.ch>
2017-01-31 13:23:34 +0100

    Update mt/tt trigger paths

--- H2TauTau/python/proto/samples/summer16/triggers_tauMu.py
+++ H2TauTau/python/proto/samples/summer16/triggers_tauMu.py
@@ -1,37 +1,44 @@
 # 2016 data
 data_triggers = [
-    'HLT_IsoMu22_v5',
-    'HLT_IsoMu22_v4',
-    'HLT_IsoMu22_v3',
-    'HLT_IsoMu22_v2',
-    'HLT_IsoMu22_v1',
-    'HLT_IsoTkMu22_v5',
-    'HLT_IsoTkMu22_v4',
-    'HLT_IsoTkMu22_v3',
-    'HLT_IsoTkMu22_v2',
-    'HLT_IsoTkMu22_v1',
+    'HLT_IsoMu24_v5',
+    'HLT_IsoMu24_v4',
+    'HLT_IsoMu24_v3',
+    'HLT_IsoMu24_v2',
+    'HLT_IsoMu24_v1',
+    'HLT_IsoTkMu24_v5',
+    'HLT_IsoTkMu24_v4',
+    'HLT_IsoTkMu24_v3',
+    'HLT_IsoTkMu24_v2',
+    'HLT_IsoTkMu24_v1',
     'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v1',
     'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v2',
     'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v3',
-    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v4'
+    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v4',
+    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1_v1',
+    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1_v2',
+    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1_v3',
+    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1_v4'
     ]
 
 data_triggerfilters = [
-    'hltL3crIsoL1sMu20L1f0L2f10QL3f22QL3trkIsoFiltered0p09', # IsoMu22
-    'hltL3fL1sMu20L1f0Tkf22QL3trkIsoFiltered0p09', # IsoTkMu22
+    'hltL3crIsoL1sMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p09', # IsoMu24
+    'hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p09', # IsoTkMu24
     'hltL3crIsoL1sMu18erTauJet20erL1f0L2f10QL3f19QL3trkIsoFiltered0p09', # IsoMu19_eta2p1_LooseIsoPFTau20
     'hltOverlapFilterIsoMu19LooseIsoPFTau20'# IsoMu19_eta2p1_LooseIsoPFTau20
 ]
 
 mc_triggers = [
-    'HLT_IsoMu22_v3',
-    'HLT_IsoTkMu22_v3',
-    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v2'
+    'HLT_IsoMu24_v4',
+    'HLT_IsoTkMu24_v4',
+    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v5',
+    'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1_v5',
+    'HLT_IsoMu21_eta2p1_LooseIsoPFTau20_SingleL1_v5'
     ]
 
 mc_triggerfilters = [
-    'hltL3crIsoL1sMu20L1f0L2f10QL3f22QL3trkIsoFiltered0p09',
-    'hltL3fL1sMu20L1f0Tkf22QL3trkIsoFiltered0p09',
+    # FIXME - to be checked
+    'hltL3crIsoL1sMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p09', #IsoMu24
+    'hltL3fL1sMu22L1f0Tkf24QL3trkIsoFiltered0p09', # IsoTkMu24
     'hltOverlapFilterIsoMu19LooseIsoPFTau20',
     'hltPFTau20TrackLooseIsoAgainstMuon'
 ]
--- H2TauTau/python/proto/samples/summer16/triggers_tauTau.py
+++ H2TauTau/python/proto/samples/summer16/triggers_tauTau.py
@@ -1,26 +1,39 @@
 data_triggers = [
-    # 2015C
-    'HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v1',
-    'HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v2',
-    'HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v3',
-    'HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v4',
-    'HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v5',
-    # 2015D
-    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v1',
-    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v2',
-    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v3',
-    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v4',
-    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v5',
+    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v2', # up to run 274733
+    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v3', # up to run 276837
+    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v4', # up to run 278240
+    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v5', # up to run 280960
+    'HLT_DoubleMediumCombinedIsoPFTau35_Trk1_eta2p1_Reg_v2', # up to run 282544
+    'HLT_DoubleMediumCombinedIsoPFTau35_Trk1_eta2p1_Reg_v3', # up to run 284044
+
+    'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v1', # runs 274748-276244
+    'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v2', # runs 276271-278240
+    'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v3', # runs 278270-280960
+    'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v5', # runs 281010-284044
     ]
 
 data_triggerfilters = [
-    # 'hltDoublePFTau35TrackPt1MediumIsolationDz02Reg'
+    'hltDoublePFTau35TrackPt1MediumIsolationDz02Reg', # same trigger filter for all
+    'hltDoublePFTau35TrackPt1MediumCombinedIsolationDz02Reg',
+    'hltPFTau140TrackPt50LooseAbsOrRelVLooseIso'
 ]
 
 mc_triggers = [
+    'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg_v5',
+    'HLT_DoubleMediumCombinedIsoPFTau35_Trk1_eta2p1_Reg_v2',
+    # 'HLT_DoubleMediumCombinedIsoPFTau40_Trk1_eta2p1_Reg_v1', # higher threshold
+    'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v5',
+    'HLT_VLooseIsoPFTau120_Trk50_eta2p1_v5',
+    # 'HLT_PFTau140_eta2p1_v5' # prescaled
+    
     ]
 
 mc_triggerfilters = [
+    'hltDoublePFTau35TrackPt1MediumIsolationDz02Reg', # 
+    'hltDoublePFTau35TrackPt1MediumCombinedIsolationDz02Reg',
+    # 'hltDoublePFTau35TrackPt1MediumCombinedIsolationL1HLTMatchedReg',
+    'hltPFTau140TrackPt50LooseAbsOrRelVLooseIso',
+    'hltPFTau120TrackPt50LooseAbsOrRelVLooseIso',
 ]
 
 embed_triggers = [
