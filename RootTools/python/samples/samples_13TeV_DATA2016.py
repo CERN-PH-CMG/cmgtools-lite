70cf028684169ac7457a6f1d2a998b0c8c8d29be
Clemens Lange <clemens.lange@cern.ch>
2017-02-13 18:02:44 +0100

    remove Run2016B_03Feb2017_v1 since it only contains runs before the golden JSON run range

--- RootTools/python/samples/samples_13TeV_DATA2016.py
+++ RootTools/python/samples/samples_13TeV_DATA2016.py
@@ -262,21 +262,6 @@ dataSamples_23Sep2016 = dataSamples_Run2016B_23Sep2016 + dataSamples_Run2016C_23
 ### Dataset corresponding to the full Run2016 with re-reco + prompt
 dataSamples_23Sep2016PlusPrompt = dataSamples_23Sep2016 + dataSamples_Run2016H_v2 + dataSamples_Run2016H_v3
 
-### ----------------------------- Run2016B v1 03Feb2017 ----------------------------------------
-
-JetHT_Run2016B_03Feb2017_v1       = kreator.makeDataComponent("JetHT_Run2016B_03Feb2017_v1"      , "/JetHT/Run2016B-03Feb2017_ver1-v1/MINIAOD"         , "CMS", ".*root", json)
-HTMHT_Run2016B_03Feb2017_v1       = kreator.makeDataComponent("HTMHT_Run2016B_03Feb2017_v1"      , "/HTMHT/Run2016B-03Feb2017_ver1-v1/MINIAOD"         , "CMS", ".*root", json)
-MET_Run2016B_03Feb2017_v1         = kreator.makeDataComponent("MET_Run2016B_03Feb2017_v1"        , "/MET/Run2016B-03Feb2017_ver1-v1/MINIAOD"           , "CMS", ".*root", json)
-SingleElectron_Run2016B_03Feb2017_v1 = kreator.makeDataComponent("SingleElectron_Run2016B_03Feb2017_v1", "/SingleElectron/Run2016B-03Feb2017_ver1-v1/MINIAOD", "CMS", ".*root", json)
-SingleMuon_Run2016B_03Feb2017_v1  = kreator.makeDataComponent("SingleMuon_Run2016B_03Feb2017_v1" , "/SingleMuon/Run2016B-03Feb2017_ver1-v1/MINIAOD"    , "CMS", ".*root", json)
-SinglePhoton_Run2016B_03Feb2017_v1= kreator.makeDataComponent("SinglePhoton_Run2016B_03Feb2017_v1"  , "/SinglePhoton/Run2016B-03Feb2017_ver1-v1/MINIAOD"  , "CMS", ".*root", json)
-DoubleEG_Run2016B_03Feb2017_v1    = kreator.makeDataComponent("DoubleEG_Run2016B_03Feb2017_v1"   , "/DoubleEG/Run2016B-03Feb2017_ver1-v1/MINIAOD"      , "CMS", ".*root", json)
-MuonEG_Run2016B_03Feb2017_v1     = kreator.makeDataComponent("MuonEG_Run2016B_03Feb2017_v1"     , "/MuonEG/Run2016B-03Feb2017_ver1-v1/MINIAOD"        , "CMS", ".*root", json)
-DoubleMuon_Run2016B_03Feb2017_v1  = kreator.makeDataComponent("DoubleMuon_Run2016B_03Feb2017_v1" , "/DoubleMuon/Run2016B-03Feb2017_ver1-v1/MINIAOD"    , "CMS", ".*root", json)
-Tau_Run2016B_03Feb2017_v1  = kreator.makeDataComponent("Tau_Run2016B_03Feb2017_v1" , "/Tau/Run2016B-03Feb2017_ver1-v1/MINIAOD"    , "CMS", ".*root", json)
-
-dataSamples_Run2016B_03Feb2017_v1 = [JetHT_Run2016B_03Feb2017_v1, HTMHT_Run2016B_03Feb2017_v1, MET_Run2016B_03Feb2017_v1, SingleElectron_Run2016B_03Feb2017_v1, SingleMuon_Run2016B_03Feb2017_v1, SinglePhoton_Run2016B_03Feb2017_v1, DoubleEG_Run2016B_03Feb2017_v1, MuonEG_Run2016B_03Feb2017_v1, DoubleMuon_Run2016B_03Feb2017_v1, Tau_Run2016B_03Feb2017_v1]
-
 ### ----------------------------- Run2016B v2 03Feb2017 ----------------------------------------
 
 JetHT_Run2016B_03Feb2017_v2       = kreator.makeDataComponent("JetHT_Run2016B_03Feb2017_v2"      , "/JetHT/Run2016B-03Feb2017_ver2-v2/MINIAOD"         , "CMS", ".*root", json)
@@ -402,7 +387,7 @@ dataSamples_Run2016H_03Feb2017_v3 = [JetHT_Run2016H_03Feb2017_v3, HTMHT_Run2016H
 samples = dataSamples_PromptReco
 
 ### Summary of 03Feb2017
-dataSamples_03Feb2017 = dataSamples_Run2016B_03Feb2017_v1 + dataSamples_Run2016B_03Feb2017_v2 + dataSamples_Run2016C_03Feb2017 + dataSamples_Run2016D_03Feb2017 + dataSamples_Run2016E_03Feb2017 + dataSamples_Run2016F_03Feb2017 + dataSamples_Run2016G_03Feb2017 + dataSamples_Run2016H_03Feb2017_v2 + dataSamples_Run2016H_03Feb2017_v3
+dataSamples_03Feb2017 = dataSamples_Run2016B_03Feb2017_v2 + dataSamples_Run2016C_03Feb2017 + dataSamples_Run2016D_03Feb2017 + dataSamples_Run2016E_03Feb2017 + dataSamples_Run2016F_03Feb2017 + dataSamples_Run2016G_03Feb2017 + dataSamples_Run2016H_03Feb2017_v2 + dataSamples_Run2016H_03Feb2017_v3
 
 
 dataSamples = dataSamples_PromptReco + dataSamples_23Sep2016 + dataSamples_03Feb2017
