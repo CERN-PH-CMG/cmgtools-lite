e865ff11dd510178a73d3c41debc2982ed1e6466
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-02-21 01:39:37 +0100

    first modifications for Run2 muon SFs

--- MonoXAnalysis/python/tools/lep_SF.py
+++ MonoXAnalysis/python/tools/lep_SF.py
@@ -1,84 +1,78 @@
 #!/usr/bin/env python
 from CMGTools.TTHAnalysis.treeReAnalyzer import *
-from CMGTools.MonoXAnalysis.tools.eventVars_monojet import EventVarsMonojet
 import re,os
 
 class Lep_SF_one:
-    def __init__(self,h2d):
-        self.h2d = h2d.Clone("Lep_one_"+h2d.GetName())
-        self.h2d.SetDirectory(None)
-        self.xmin = h2d.GetXaxis().GetXmin()+1e-3
-        self.xmax = h2d.GetXaxis().GetXmax()-1e-3
-        self.ymin = h2d.GetYaxis().GetXmin()+1e-3
-        self.ymax = h2d.GetYaxis().GetXmax()-1e-3
+    def __init__(self,hists):
+        self.hists = []
+        for h in hists:
+            h2d = h.Clone("Lep_one_"+h.GetName())
+            h2d.SetDirectory(None)
+            self.hists.append(h2d)
+            # all the hists need to have the same range, take the last one
+            self.xmin = h.GetXaxis().GetXmin()+1e-3
+            self.xmax = h.GetXaxis().GetXmax()-1e-3
+            self.ymin = h.GetYaxis().GetXmin()+1e-3
+            self.ymax = h.GetYaxis().GetXmax()-1e-3
     def fetch(self,pt,eta,pdgid):
         if abs(pdgid)==11:
-            xbin = self.h2d.GetXaxis().FindBin(min(max(eta,self.xmin),self.xmax))
-            ybin = self.h2d.GetYaxis().FindBin(min(max(pt,self.ymin),self.ymax))
+            xbin = self.hists[0].GetXaxis().FindBin(min(max(eta,self.xmin),self.xmax))
+            ybin = self.hists[0].GetYaxis().FindBin(min(max(pt,self.ymin),self.ymax))
         else:
-            xbin = self.h2d.GetXaxis().FindBin(min(max(pt,self.xmin),self.xmax))
-            ybin = self.h2d.GetYaxis().FindBin(min(max(abs(eta),self.ymin),self.ymax))
-        return self.h2d.GetBinContent(xbin,ybin)
+            xbin = self.hists[0].GetXaxis().FindBin(min(max(pt,self.xmin),self.xmax))
+            ybin = self.hists[0].GetYaxis().FindBin(min(max(abs(eta),self.ymin),self.ymax))
+        sfone = 1.0
+        for h2d in self.hists:
+            sfone *= h2d.GetBinContent(xbin,ybin)
+        return sfone
     def __call__(self,lepton):        
         return self.fetch(lepton.pt, lepton.eta,lepton.pdgId)
 
 class Lep_SF_both:
     def __init__(self,hists_el,hists_mu):
-            self.sf_el = Lep_SF_one(hists_el.Get("EGamma_SF2D"))
-            self.sf_mu = Lep_SF_one(hists_mu.Get("pt_abseta_PLOT_pair_probeMultiplicity_bin0"))
+        thists_el = [hists_el[0].Get("EGamma_SF2D")]
+        thists_mu = []
+        thists_mu.append(hists_mu[0].Get("Lep_effIsoMu24_OR_IsoTkMu24_PtEtaBins_abseta_pt_ratio"))
+        thists_mu.append(hists_mu[1].Get("Lep_effMC_NUM_TightID_DEN_genTracks_PAR_pt_eta_abseta_pt_ratio"))
+        thists_mu.append(hists_mu[2].Get("Lep_effTightISO_TightID_pt_eta_abseta_pt_ratio"))
+        self.sf_el = Lep_SF_one(thists_el)
+        self.sf_mu = Lep_SF_one(thists_mu)
     def __call__(self,lepton):
         sf = self.sf_mu if abs(lepton.pdgId) == 13 else self.sf_el
         return sf(lepton)
 
 class Lep_SF_Event:
-    def __init__(self,evtSel,hists_el,hists_mu,nlep):
-        self.sf_tight = Lep_SF_both(hists_el[0],hists_mu[0])
-        self.evtSel = evtSel
-        if nlep == 2: 
-            self.sf_loose = Lep_SF_both(hists_el[1],hists_mu[1])
+    def __init__(self,hists_el,hists_mu):
+        self.sf = Lep_SF_both(hists_el,hists_mu)
     def __call__(self,event,nlep,flatSyst=0.0):
         leps = Collection(event,"LepGood","nLepGood",2)
         sfev = 1.0
-        # the following is not completely correct for asymmetric selection, it should be the full combinatorics
-        if self.sf_loose != None:
-            for i,l in enumerate(leps):
-                if i >= nlep: break
-                if self.evtSel.lepIdTight(l): sfev *= self.sf_tight(l) * (1+flatSyst)
-                elif self.evtSel.lepIdVeto(l): sfev *= self.sf_loose(l) * (1+flatSyst)
-                else: sfev *= 1.0
-        else:
-            for i,l in enumerate(leps):
-                if i >= nlep: break
-                sfev *= self.sf_tight(l)
+        for i,l in enumerate(leps):
+            if i >= nlep: break
+            sfev *= self.sf(l)
         return sfev
 
 class AllLepSFs:
     def __init__(self):
-        self.f_el = [ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/monox/leptonsf/eff_eleID_CutsID_Medium_ICHEP16.root"),
-                     ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/monox/leptonsf/eff_eleID_CutsID_Veto_ICHEP16.root") ]
-        self.f_mu = [ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/monox/leptonsf/eff_muID_medium_ICHEP16.root"),
-                     ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/monox/leptonsf/eff_muID_loose_ICHEP16.root") ]
-        self.evtSel = EventVarsMonojet()
-        self.sf_TightLoose = Lep_SF_Event(self.evtSel,self.f_el,self.f_mu,2)
-        self.sf_Tight = Lep_SF_Event(self.evtSel,self.f_el,self.f_mu,1)
-        files = self.f_el + self.f_mu
-        for f in files: f.Close()
+        self.f_el = [ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/wmass/leptonsf/eff_eleID_CutsID_Loose_Moriond17.root")]
+        self.f_mu = [ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/wmass/leptonsf/MuonTriggerEfficienciesAndSF_RunBtoH_Weighted.root"),
+                     ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/wmass/leptonsf/MuonIDEfficienciesAndSF_BCDEFGH_Weighted.root"),
+                     ROOT.TFile("/afs/cern.ch/work/e/emanuele/public/wmass/leptonsf/MuonIsoEfficienciesAndSF_BCDEFGH_Weighted.root") ] 
+        self.sf_Tight = Lep_SF_Event(self.f_el,self.f_mu)
+        f_lep = self.f_el + self.f_mu
+        for f in f_lep: f.Close()
     def listBranches(self):
-        return [ 'LepTightLoose', 'LepTight', 'LepTightLooseUp', 'LepTightUp', 'LepTightLooseDown', 'LepTightDown' ]
+        return [ 'LepTight_1l', 'LepTight_2l' ]
     def __call__(self,event):
         return {
-            'LepTightLoose' : self.sf_TightLoose(event,2),
-            'LepTight' : self.sf_TightLoose(event,1),
-            'LepTightLooseUp' : self.sf_TightLoose(event,2,0.02),
-            'LepTightUp' : self.sf_TightLoose(event,1,0.02),
-            'LepTightLooseDown' : self.sf_TightLoose(event,2,-0.02),
-            'LepTightDown' : self.sf_TightLoose(event,1,-0.02),
+            'LepTight_1l' : self.sf_Tight(event,1),
+            'LepTight_2l' : self.sf_Tight(event,2),
         }
 
 if __name__ == '__main__':
     from sys import argv
     file = ROOT.TFile(argv[1])
-    tree = file.Get("ttHLepTreeProducerBase")
+    tree = file.Get("tree")
     class Tester(Module):
         def __init__(self, name):
             Module.__init__(self,name,None)
