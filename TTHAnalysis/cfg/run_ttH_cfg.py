dfaf2794d1e8af55c6fbb22bd80112f4632806cf
peruzzim <peruzzim@users.noreply.github.com>
2017-09-12 17:13:07 +0200

    Updates to ttH cfg

--- TTHAnalysis/cfg/run_ttH_cfg.py
+++ TTHAnalysis/cfg/run_ttH_cfg.py
@@ -26,9 +26,6 @@ forcedFineSplitFactor = getHeppyOption("fineSplitFactor",-1)
 isTest = getHeppyOption("test",None) != None and not re.match("^\d+$",getHeppyOption("test"))
 selectedEvents=getHeppyOption("selectEvents","")
 
-runFRMC = True
-#runData = True; runDataQCD = True
-#runData = True
 sample = "main"
 #if runDataQCD or runFRMC: sample="qcd1l"
 #sample = "z3l"
@@ -217,15 +214,15 @@ from CMGTools.RootTools.samples.autoAAAconfig import *
 selectedComponents = [TTLep_pow]
 
 
-sig_ttv = [TTHnobb_pow,TTWToLNu_ext,TTWToLNu_ext2,TTZToLLNuNu_ext,TTZToLLNuNu_m1to10] # signal + TTV
-ttv_lo = [TTW_LO,TTZ_LO] # TTV LO
-rares = [ZZTo4L,GGHZZ4L,VHToNonbb,tZq_ll_ext,WpWpJJ,WWDouble,TTTT,tWll] # rares
-single_t = [TToLeptons_sch_amcatnlo,T_tch_powheg,TBar_tch_powheg,T_tWch_ext,TBar_tWch_ext] # single top + tW
-convs = [WGToLNuG_amcatnlo_ext,ZGTo2LG_ext,TGJets,TTGJets] # X+G
+sig_ttv = [TTHnobb_pow,TTHnobb_mWCutfix_ext,TTWToLNu_ext,TTWToLNu_ext2,TTZToLLNuNu_ext,TTZToLLNuNu_m1to10] # signal + TTV
+ttv_lo = [TTW_LO,TTZ_LO,TTWWTo2LSS2Nu_LO] # TTV LO
+rares = [ZZTo4L,GGHZZ4L,VHToNonbb,tZq_ll_ext,WpWpJJ,WWDoubleTo2L,TTTT,tWll] # rares
+single_t = [TToLeptons_sch_amcatnlo,T_tch_powheg,TBar_tch_powheg,T_tWch_ext,TBar_tWch_ext,THQ,THW] # single top + tW
+convs = [WGToLNuG_amcatnlo_ext,WGToLNuG_amcatnlo_ext2,ZGTo2LG_ext,TGJets,TGJets_ext,TTGJets,TTGJets_ext] # X+G
 v_jets = [WJetsToLNu_LO,DYJetsToLL_M10to50_LO,DYJetsToLL_M50_LO_ext,WWTo2L2Nu] # V+jets
 tt_1l = [TTJets_SingleLeptonFromT,TTJets_SingleLeptonFromT_ext,TTJets_SingleLeptonFromTbar,TTJets_SingleLeptonFromTbar_ext] # TT 1l
-tt_2l = [TTJets_DiLepton]#,TTJets_DiLepton_ext] # TT 2l
-boson = [WZTo3LNu]+TriBosons # multi-boson
+tt_2l = [TTJets_DiLepton,TTJets_DiLepton_ext,TT_pow] # TT 2l
+boson = [WZTo3LNu,WZTo3LNu_amcatnlo]+TriBosons # multi-boson
 
 samples_slow = sig_ttv + ttv_lo + rares + convs + boson + tt_2l
 samples_fast = single_t + v_jets + tt_1l
@@ -235,7 +232,7 @@ cropToLumi([T_tch_powheg,TBar_tch_powheg],50)
 configureSplittingFromTime(samples_fast,50,6)
 configureSplittingFromTime(samples_slow,100,6)
 
-#selectedComponents = samples_slow+samples_fast
+selectedComponents = samples_slow+samples_fast
 
 if scaleProdToLumi>0: # select only a subset of a sample, corresponding to a given luminosity (assuming ~30k events per MiniAOD file, which is ok for central production)
     target_lumi = scaleProdToLumi # in inverse picobarns
@@ -254,29 +251,26 @@ if runData and not isTest: # For running on data
     is50ns = False
     dataChunks = []
 
-#    json = os.environ['CMSSW_BASE']+'/src/CMGTools/TTHAnalysis/data/json/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt' # 12.9/fb #276811 ICHEP LastRun
     json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt' # 36.5/fb
 
     for era in 'BCDEFGH': dataChunks.append((json,filter(lambda dset: 'Run2016'+era in dset.name,dataSamples_23Sep2016PlusPrompt),'2016'+era,[],False))
-    #for era in 'C': dataChunks.append((json,filter(lambda dset: 'Run2016'+era in dset.name,dataSamples_23Sep2016PlusPrompt),'2016'+era,[],False))
-    #for era in 'C': dataChunks.append((json,filter(lambda dset: 'Run2016'+era in dset.name,[DoubleMuon_Run2016C_02Feb2017]),'2016'+era,[],False))
 
     DatasetsAndTriggers = []
     selectedComponents = [];
     exclusiveDatasets = True; # this will veto triggers from previous PDs in each PD, so that there are no duplicate events
  
     DatasetsAndTriggers.append( ("DoubleMuon", triggers_mumu_iso + triggers_mumu_ss + triggers_mumu_ht + triggers_3mu + triggers_3mu_alt) )
-    #DatasetsAndTriggers.append( ("DoubleEG",   triggers_ee + triggers_ee_ht + triggers_3e) )
-    #DatasetsAndTriggers.append( ("MuonEG",     triggers_mue + triggers_mue_ht + triggers_2mu1e + triggers_2e1mu) )
-    #DatasetsAndTriggers.append( ("SingleMuon", triggers_1mu_iso + triggers_1mu_noniso) )
-    #DatasetsAndTriggers.append( ("SingleElectron", triggers_1e) )
+    DatasetsAndTriggers.append( ("DoubleEG",   triggers_ee + triggers_ee_ht + triggers_3e) )
+    DatasetsAndTriggers.append( ("MuonEG",     triggers_mue + triggers_mue_ht + triggers_2mu1e + triggers_2e1mu) )
+    DatasetsAndTriggers.append( ("SingleMuon", triggers_1mu_iso + triggers_1mu_noniso) )
+    DatasetsAndTriggers.append( ("SingleElectron", triggers_1e) )
 
     if runDataQCD: # for fake rate measurements in data
         FRTrigs_mu = triggers_FR_1mu_noiso
         FRTrigs_el = triggers_FR_1e_noiso
         DatasetsAndTriggers = [
-            #("DoubleMuon", FRTrigs_mu ),
-            #("DoubleEG",   FRTrigs_el ),
+            ("DoubleMuon", FRTrigs_mu ),
+            ("DoubleEG",   FRTrigs_el ),
             ("SingleMuon", triggers_FR_muNoIso ),
             #("JetHT",   triggers_FR_jet )
         ]
@@ -322,7 +316,6 @@ if runData and not isTest: # For running on data
         susyCoreSequence.remove(jsonAna)
     if runDataQCD: # for fake rate measurements in data
          configureSplittingFromTime(selectedComponents, 3.5, 2, maxFiles=15)
-    #configureSplittingFromTime(selectedComponents, 10, 1, maxFiles=2)
 
 #printSummary(selectedComponents)
 
@@ -500,8 +493,7 @@ if test == '1':
         filesPerJob = max(len(comp.files)/comp.splitFactor, 1)
         comp.files = comp.files[:filesPerJob]
     else:
-        #comp.files = comp.files[:1]
-        comp.files = comp.files[8:9]
+        comp.files = comp.files[:1]
     comp.splitFactor = 1
     comp.fineSplitFactor = 1
     selectedComponents = [ comp ]
