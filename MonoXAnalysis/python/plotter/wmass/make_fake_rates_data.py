b7fe50b183b10cf978842b755cd1debbae3da5ca
Marco Cipriani <cippy91@gmail.com>
2017-11-07 16:36:30 +0100

    add options and other fixes

--- MonoXAnalysis/python/plotter/wmass/make_fake_rates_data.py
+++ MonoXAnalysis/python/plotter/wmass/make_fake_rates_data.py
@@ -5,27 +5,37 @@ import re, sys, os, os.path, subprocess
 from optparse import OptionParser
 parser = OptionParser(usage="%prog testname ")
 parser.add_option("--mu", dest="useMuon", default=False, action='store_true', help="Do fake rate for muons");
+parser.add_option("--mediumWP", dest="useMediumWP", default=False, action='store_true', help="Use medium working point in numerator for electrons");
 parser.add_option("--qcdmc", dest="addQCDMC", default=False, action='store_true', help="Add QCD MC in plots (but do not subtract from data)");
 parser.add_option("--singleEtaBin", dest="singleEtaBin", default=-1.0, type='float', help="Use a single eta bin (pass the upper eta boundary)");
 parser.add_option("--charge", dest="charge", default="", type='string', help="Select charge: p for positive, n for negative");
+parser.add_option("--useSRtrees", dest="useSRtrees", default=False, action='store_true', help="Use trees of signal region instead of fake-rate trees (and apply HLT_SingleEl)");
+parser.add_option("--test", dest="test", default="", type='string', help="pass the name of a folder (mandatory) to store test FR plots. It is created in plots/fake-rate/test/");
 (options, args) = parser.parse_args()
-
-chargeSelection = ""
-if options.charge != "":
-    if options.charge == "p":
-        chargeSelection = "-A onelep positive 'LepGood1_pdgId < 0'"
-    elif options.charge == "n":
-        chargeSelection = "-A onelep negative 'LepGood1_pdgId > 0'"
-    else:
-        print "%s is not a valid input for charge setting: use p or n" % options.charge
-        quit()
 
 useMuon = options.useMuon
 addQCDMC = options.addQCDMC  # trying to add QCD MC to graphs to be compared
+charge = str(options.charge)
+useSRtrees = options.useSRtrees
+testDir = str(options.test)
+useMediumWP = options.useMediumWP
+
 if useMuon:
     addQCDMC = True
 
-T='/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/TREES_1LEP_80X_V3_FR/'  # WARNING, for the moment it is stored on eos, not in pccmsrm29 or in lxplus
+chargeSelection = ""
+if charge != "":
+    if charge == "p":
+        chargeSelection = "-A onelep positive 'LepGood1_pdgId < 0'"
+    elif charge == "n":
+        chargeSelection = "-A onelep negative 'LepGood1_pdgId > 0'"
+    else:
+        print "%s is not a valid input for charge setting: use p or n" % charge
+        quit()
+
+T="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/TREES_1LEP_80X_V3_FR/"  # WARNING, for the moment it is stored on eos, not in pccmsrm29 or in lxplus
+if useSRtrees:
+    T="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/TREES_1LEP_80X_V3/" 
 objName='tree' # name of TTree object in Root file, passed to option --obj in tree2yield.py
 # if 'pccmsrm29' in os.environ['HOSTNAME']: T = T.replace('/data1/emanuele/wmass','/u2/emanuele')
 # elif 'lxplus' in os.environ['HOSTNAME']: T = T.replace('/data1/emanuele/wmass','/afs/cern.ch/work/e/emanuele/TREES/')
@@ -41,6 +51,8 @@ CUTFILE=BASECONFIG+'/qcd1l.txt'
 XVAR="pt_coarse"
 FITVAR="mtfix"
 NUM="FullSel"
+if useMediumWP:
+    NUM="FullSel_mediumID"
 BARREL="00_15"; ENDCAP="15_25"; ETA="1.479";
 if useMuon:
     BASECONFIG="wmass/wmass_mu"
@@ -51,21 +63,36 @@ if useMuon:
     NUM="MuonTightIso"
     BARREL="00_12"; ENDCAP="12_24"; ETA="1.2";
 
+if useSRtrees:
+    MCA = MCA.replace('mca-qcd1l','mca-qcd1l_SRtrees')
+    CUTFILE = CUTFILE.replace('qcd1l','qcd1l_SRtrees')
+
 OPTIONS = MCA+" "+CUTFILE+" -f -P "+T+" --obj "+objName+" --s2v -j "+str(J)+" -l "+str(luminosity)
 # no friends for the moment
-#OPTIONS += ' -F mjvars/t "'+T+'/friends/evVarFriend_{cname}.root" '
+OPTIONS += ' -F Friends '+T+'/friends/tree_Friend_{cname}.root '
+#OPTIONS += ' -F Friends '+T+'/friends/tree_FRFriend_{cname}.root '
+
+# event weight (NB: not needed for data, and sf not good for MC since here we have fake electrons)
+# puwBF should be used, but for the moment I don't
+#OPTIONS += ' -W "LepGood1_effSF*puwBF" '
 
 if options.singleEtaBin > 0.0:
     ALL="00_" + "{0:.1f}".format(options.singleEtaBin).replace(".","p")
     ETA=options.singleEtaBin
 
 PBASE = "plots/fake-rate/el/"
+if useSRtrees:
+    PBASE = "plots/fake-rate/el_SRtrees/"
 if useMuon:
     PBASE = "plots/fake-rate/mu/"
+    if useSRtrees:
+        PBASE = "plots/fake-rate/mu_SRtrees/"
+if testDir != "":
+    PBASE = PBASE.replace('plots/fake-rate/','plots/fake-rate/test/'+str(testDir)+'/')
 
-if options.charge == "p":
+if charge == "p":
     PBASE = PBASE + "pos/"
-elif options.charge == "n":
+elif charge == "n":
     PBASE = PBASE + "neg/"
 else:
     PBASE = PBASE + "comb/"
@@ -74,7 +101,9 @@ else:
 # check if Diboson and Top samples are present for the FR trees at 13 TeV
 EWKSPLIT="-p 'W_fake,W,Z,data'"
 if addQCDMC:
-    EWKSPLIT="-p 'QCD,W,Z,data'"
+    EWKSPLIT="-p 'QCD,data'" # "-p 'QCD,W,Z,data'"
+#if useSRtrees:
+#    EWKSPLIT = EWKSPLIT.replace('W,Z,data','W,Z,Top,DiBosons,data')
 
 MCEFF="  python wmass/dataFakeRate.py "+ OPTIONS + " " + EWKSPLIT + " --groupBy cut wmass/make_fake_rates_sels.txt wmass/make_fake_rates_xvars.txt  "
 if addQCDMC:
@@ -85,11 +114,11 @@ else:
 MCEFF += "--sP "+NUM+" --sP "+XVAR+"  --sP "+FITVAR+" "+FITVAR+"  --ytitle 'Fake rate' "
 MCEFF += " --fixRatioRange --maxRatioRange 0.7 1.29 " # ratio for other plots
 LEGEND=" --legend=TL --fontsize 0.05 --legendWidth 0.4"
-RANGES=" --showRatio  --ratioRange 0.00 2.99 "
+RANGES=" --showRatio  --ratioRange 0.50 1.99 "
 if useMuon:
     RANGES+=" --yrange 0 1.0  --xcut 25 100 "
 else:
-    RANGES+=" --yrange 0.1 1.0  --xcut 25 100 "
+    RANGES+=" --yrange 0.0 0.8  --xcut 25 100 "
 
 MCEFF += (LEGEND+RANGES)
 
