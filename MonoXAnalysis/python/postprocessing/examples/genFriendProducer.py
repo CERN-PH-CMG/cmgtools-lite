995351f0963fc9323b74010ca0a62f6a19dc7ec8
mdunser <marc.dunser@cern.ch>
2017-12-05 09:31:54 +0100

    adapted genQEDJetHelper and gen-friends to include LHE W and W-decay flavor. added muon SFs to the lepSF producer

--- MonoXAnalysis/python/postprocessing/examples/genFriendProducer.py
+++ MonoXAnalysis/python/postprocessing/examples/genFriendProducer.py
@@ -32,7 +32,7 @@ class SimpleVBoson:
         return sqrt(2*self.pt1*self.pt2*(cosh(self.deta)-cos(self.dphi)))
 
 class KinematicVars:
-    def __init__(self,beamE=7000):
+    def __init__(self,beamE=6500):
         self.beamE = beamE
     def CSFrame(self,dilepton):
         pMass = 0.938272
@@ -51,6 +51,14 @@ class KinematicVars:
         boostedLep.Boost(-dilep.BoostVector())
         csframe = self.CSFrame(dilep)
         return cos(boostedLep.Angle(csframe[2]))
+    def cosThetaCM(self,lplus,lminus):
+        dilep = lplus + lminus
+        boostedLep = ROOT.TLorentzVector(lminus)
+        boostedLep.Boost(-dilep.BoostVector())
+        modw = sqrt(dilep.X()*dilep.X() + dilep.Y()*dilep.Y() + dilep.Z()*dilep.Z())
+        modm = sqrt(boostedLep.X()*boostedLep.X() + boostedLep.Y()*boostedLep.Y() + boostedLep.Z()*boostedLep.Z())
+        cos = (dilep.X()*boostedLep.X() + dilep.Y()*boostedLep.Y() + dilep.Z()*boostedLep.Z())/modw/modm
+        return cos
     def phiCS(self,lplus,lminus):
         dilep = lplus + lminus
         boostedLep = ROOT.TLorentzVector(lminus)
@@ -64,7 +72,7 @@ class GenQEDJetProducer(Module):
     def __init__(self,deltaR):
         self.deltaR = deltaR
         self.vars = ("pt","eta","phi","mass","pdgId")
-        self.genwvars = ("charge","pt","eta","phi","mass","y","costcs","phics")
+        self.genwvars = ("charge","pt","eta","phi","mass","y","costcs","phics","costcm","decayId")
         if "genQEDJetHelper_cc.so" not in ROOT.gSystem.GetLibraries():
             print "Load C++ Worker"
             ROOT.gROOT.ProcessLine(".L %s/src/CMGTools/MonoXAnalysis/python/postprocessing/helpers/genQEDJetHelper.cc+" % os.environ['CMSSW_BASE'])
@@ -76,6 +84,9 @@ class GenQEDJetProducer(Module):
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.initReaders(inputTree) # initReaders must be called in beginFile
         self.out = wrappedOutputTree
+        self.out.branch("weightGen", "F")
+        self.out.branch("partonId1", "I")
+        self.out.branch("partonId2", "I")
         self.out.branch("nGenLepDressed", "I")
         self.out.branch("nGenPromptNu", "I")
         for V in self.vars:
@@ -83,13 +94,14 @@ class GenQEDJetProducer(Module):
             self.out.branch("GenPromptNu_"+V, "F", lenVar="nGenPromptNu")
         for V in self.genwvars:
             self.out.branch("genw_"+V, "F")
+            self.out.branch("lhew_"+V, "F")
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
 
     def initReaders(self,tree): # this function gets the pointers to Value and ArrayReaders and sets them in the C++ worker class
         self.nGenPart = tree.valueReader("nGenPart")
-        for B in ("pt","eta","phi","mass","pdgId","isPromptHard") : setattr(self,"GenPart_"+B, tree.arrayReader("GenPart_"+B))
-        self._worker.setGenParticles(self.nGenPart,self.GenPart_pt,self.GenPart_eta,self.GenPart_phi,self.GenPart_mass,self.GenPart_pdgId,self.GenPart_isPromptHard)
+        for B in ("pt","eta","phi","mass","pdgId","isPromptHard","motherId") : setattr(self,"GenPart_"+B, tree.arrayReader("GenPart_"+B))
+        self._worker.setGenParticles(self.nGenPart,self.GenPart_pt,self.GenPart_eta,self.GenPart_phi,self.GenPart_mass,self.GenPart_pdgId,self.GenPart_isPromptHard,self.GenPart_motherId)
         self._ttreereaderversion = tree._ttreereaderversion # self._ttreereaderversion must be set AFTER all calls to tree.valueReader or tree.arrayReader
 
     def analyze(self, event):
@@ -103,7 +115,19 @@ class GenQEDJetProducer(Module):
         neutrinos = self._worker.promptNeutrinos()
         lepPdgIds = self._worker.dressedLeptonsPdgId()
         nuPdgIds = self._worker.promptNeutrinosPdgId()
+        lheWs    = self._worker.lheWs()
+        lheWPdgIds = self._worker.lheWsPdgId()
+        lheLeps    = self._worker.lheLeps()
+        lheLepPdgIds = self._worker.lheLepsPdgId()
         
+        if hasattr(event,"genWeight"):
+            self.out.fillBranch("weightGen", getattr(event, "genWeight"))
+            self.out.fillBranch("partonId1", getattr(event, "id1"))
+            self.out.fillBranch("partonId2", getattr(event, "id2"))
+        else:
+            self.out.fillBranch("weightGen", -999.)
+            self.out.fillBranch("partonId1", -999 )
+            self.out.fillBranch("partonId2", -999 )
         retL={}
         retL["pt"] = [dl.Pt() for dl in dressedLeptons]
         retL["eta"] = [dl.Eta() for dl in dressedLeptons]
@@ -126,6 +150,25 @@ class GenQEDJetProducer(Module):
             self.out.fillBranch("GenPromptNu_"+V, retN[V])
         self.out.fillBranch("GenPromptNu_pdgId", [pdgId for pdgId in nuPdgIds])
 
+        if len(lheWs):
+            if len(lheWs) > 1:
+                print 'WARNING: MORE THAN 1 W BOSON FOUND. filling first in list'
+            self.out.fillBranch("lhew_charge" , float(np.sign(lheWPdgIds[0])))
+            self.out.fillBranch("lhew_pt"     , lheWs[0].Pt())
+            self.out.fillBranch("lhew_eta"    , lheWs[0].Eta())
+            self.out.fillBranch("lhew_phi"    , lheWs[0].Phi())
+            self.out.fillBranch("lhew_y"      , lheWs[0].Rapidity())
+            self.out.fillBranch("lhew_mass"   , lheWs[0].M())
+            (lplus,lminus) = (neutrinos[0],lheLeps[0]) if lheLepPdgIds[0]<0 else (lheLeps[0],neutrinos[0])
+            kv = KinematicVars()
+            self.out.fillBranch("lhew_costcm" , kv.cosThetaCM(lplus,lminus))
+            self.out.fillBranch("lhew_costcs" , kv.cosThetaCS(lplus,lminus))
+            self.out.fillBranch("lhew_phics"  , kv.phiCS(lplus,lminus))
+            self.out.fillBranch("lhew_decayId", abs(nuPdgIds[0]))
+        else:
+            for V in self.genwvars:
+                self.out.fillBranch("lhew_"+V, -999)
+
         if len(dressedLeptons) and len(neutrinos):
             genw = dressedLeptons[0] + neutrinos[0]
             self.out.fillBranch("genw_charge",float(-1*np.sign(lepPdgIds[0])))
@@ -137,9 +180,18 @@ class GenQEDJetProducer(Module):
             kv = KinematicVars()
             # convention for phiCS: use l- direction for W-, use neutrino for W+
             (lplus,lminus) = (neutrinos[0],dressedLeptons[0]) if lepPdgIds[0]<0 else (dressedLeptons[0],neutrinos[0])
+            self.out.fillBranch("genw_costcm",kv.cosThetaCM(lplus,lminus))
             self.out.fillBranch("genw_costcs",kv.cosThetaCS(lplus,lminus))
             self.out.fillBranch("genw_phics",kv.phiCS(lplus,lminus))
+            self.out.fillBranch("genw_decayId", abs(nuPdgIds[0]))
         else:
+            ##if not len(dressedLeptons): 
+            ##    print '================================'
+            ##    print 'no dressed leptons found!'
+            ##    print 'no dressed leptons fround :lumi:evt: {a}:{b}:{c}'.format(a=getattr(event, "run"),b=getattr(event, "lumi"),c=getattr(event, "evt"))
+            ##else:
+            ##    print '================================'
+            ##    print 'no neutrinos found, in run:lumi:evt: {a}:{b}:{c}'.format(a=getattr(event, "run"),b=getattr(event, "lumi"),c=getattr(event, "evt"))
             for V in self.genwvars:
                 self.out.fillBranch("genw_"+V, -999)
 
