8b8cfda27ec1cd40f6c758b78fa115312287e434
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2018-01-10 16:52:31 +0100

    update the symm matrix names

--- WMass/python/plotter/symmetrizeMatrix.py
+++ WMass/python/plotter/symmetrizeMatrix.py
@@ -3,6 +3,8 @@ import ROOT
 ROOT.gROOT.SetBatch()
 ROOT.gStyle.SetOptStat(0)
 
+ROOT.gStyle.SetPalette(1)
+
 infile = ROOT.TFile('multidimfit.root', 'read')
 
 fitresult = infile.Get('fit_mdf')
@@ -12,7 +14,8 @@ h2_corr = fitresult.correlationHist()
 c = ROOT.TCanvas()
 
 h2_corr.Draw('colz')
-c.SaveAs('~/www/private/w-helicity-13TeV/correlationMatrix_original_2017-12-14.pdf')
+c.SaveAs('./correlationMatrix_FR1p01_otherbkgnuis_original.pdf')
+c.SaveAs('./correlationMatrix_FR1p01_otherbkgnuis_original.png')
 
 bins = {}
 labels = []
@@ -20,7 +23,7 @@ labels = []
 for ix in range(1,h2_corr.GetXaxis().GetNbins()+1):
     labels.append(h2_corr.GetXaxis().GetBinLabel(ix))
 
-h2_new = h2_corr.Clone('correlationMatrix')
+h2_new = h2_corr.Clone('correlationMatrix_FR1p01_otherbkgnuis')
 h2_new.Reset()
 
 #l_sorted = sorted(labels, key = lambda x: int(x.split('_')[-1]) if not (x =='Wpt' or x =='longNuisance') else x[-1] )
@@ -32,7 +35,6 @@ h2_new.Reset()
 
 
 l_sorted_new = [
-'norm_Wp_right_Wp_el_Ybin_2_13',
 'norm_Wp_right_Wp_el_Ybin_3_12',
 'norm_Wp_right_Wp_el_Ybin_4_11',
 'norm_Wp_right_Wp_el_Ybin_5_10',
@@ -43,17 +45,20 @@ l_sorted_new = [
 'norm_Wp_left_Wp_el_Ybin_5_10',
 'norm_Wp_left_Wp_el_Ybin_4_11',
 'norm_Wp_left_Wp_el_Ybin_3_12',
-'norm_Wp_left_Wp_el_Ybin_2_13',
 'CMS_We_FRe_norm',
 'CMS_We_VV',
-'CMS_We_elescale', 
-'CMS_We_lepEff',  
+'CMS_We_elescale',
+'CMS_We_lepEff',
 'lumi_8TeV',   
 'norm_long_Wp_el'   ]
 
 
 for il,l in enumerate(l_sorted_new):
-    new_l = l.lstrip('norm_').replace('right','WR ').replace('left','WL ')
+    new_l = l.lstrip('norm_').replace('right','WR').replace('left','WL').replace('Ybin_','')
+    new_l = new_l.replace('Wp_WL_Wp','WpL')
+    new_l = new_l.replace('Wp_WR_Wp','WpR')
+    new_l = new_l.replace('Wp_WL_Wp','WpL')
+    new_l = new_l.replace('Wp_WR_Wp','WpR')
     #print(new_l)
     h2_new.GetXaxis().SetBinLabel(il+1, new_l)
     h2_new.GetYaxis().SetBinLabel(il+1, new_l)
@@ -64,7 +69,8 @@ for il,l in enumerate(l_sorted_new):
         h2_new.SetBinContent(il+1, il2+1, h2_corr.GetBinContent(binx, biny))
 
 h2_new.Draw('colz')
-c.SaveAs('~/www/private/w-helicity-13TeV/correlationMatrix_new_2017-12-14.pdf')
+c.SaveAs('./correlationMatrix_FR1p01_otherbkgnuis_new.pdf')
+c.SaveAs('./correlationMatrix_FR1p01_otherbkgnuis_new.png')
 
 
 #print(labels)
