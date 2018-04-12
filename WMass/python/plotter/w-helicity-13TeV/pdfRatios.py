85aca76e2eab3371c0422cae2b05ac8546daebe9
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2018-04-12 17:16:05 +0200

    moved to systRatios.py

--- WMass/python/plotter/w-helicity-13TeV/pdfRatios.py
+++ WMass/python/plotter/w-helicity-13TeV/pdfRatios.py
@@ -1,118 +1,93 @@
+#!/usr/bin/env python
+# USAGE: python systRatios.py  cards/XXX/ el -s 'CMS_We_FRe_pt' -o plots/
 import ROOT, os, copy
 from array import array
-
-
-def getbinning(splitline):
-    bins = splitline[5]
-    if '*' in bins:
-        etabins = list( float(i) for i in bins.split('*')[0].replace('\'[','').replace(']','').split(',') )
-        ptbins  = list( float(i) for i in bins.split('*')[1].replace('[','').replace(']\'','').split(',') )
-        nbinseta = len(etabins)-1
-        nbinspt  = len( ptbins)-1
-        binning = [nbinseta, etabins, nbinspt, ptbins]
-    else:
-        bins = bins.replace('\'','')
-        nbinseta = int( bins.split(',')[0] )
-        nbinspt  = int( bins.split(',')[3] )
-        etamin   = float( bins.split(',')[1] )
-        etamax   = float( bins.split(',')[2] )
-        ptmin    = float( bins.split(',')[4] )
-        ptmax    = float( bins.split(',')[5] )
-    return binning
-
-ROOT.gStyle.SetOptStat(0)
-ROOT.gStyle.SetPadRightMargin(0.13)
-
-def roll1Dto2D(h1d, histo):#,h2dname):#,plotfile,options):
-    for i in xrange(1,h1d.GetNbinsX()+1):
-        xbin = i % histo.GetNbinsX()
-        if not xbin: xbin = xbin+histo.GetNbinsX()
-        ybin = i / histo.GetNbinsX() + (1 if i%histo.GetNbinsX() else 0)
-        val = h1d.GetBinContent(i)
-        histo.SetBinContent(xbin,ybin,h1d.GetBinContent(i))
-    return histo
-
-def unroll2Dto1D(h):
-    nbins = h.GetNbinsX() * h.GetNbinsY()
-    goodname = h.GetName()
-    h.SetName(goodname+"_oldbinning")
-    newh = ROOT.TH1D(goodname,h.GetTitle(),nbins,0.5,nbins+0.5)
-    if 'TH2' not in h.ClassName(): raise RuntimeError, "Calling rebin2Dto1D on something that is not TH2"
-    for i in xrange(h.GetNbinsX()):
-        for j in xrange(h.GetNbinsY()):
-            bin = 1 + i + j*h.GetNbinsX()
-            newh.SetBinContent(bin,h.GetBinContent(i+1,j+1))
-            newh.SetBinError(bin,h.GetBinError(i+1,j+1))
-    for bin in range(1,nbins+1):
-        if newh.GetBinContent(bin)<0:
-            print 'Warning: cropping to zero bin %d in %s (was %f)'%(bin,newh.GetName(),newh.GetBinContent(bin))
-            newh.SetBinContent(bin,0)
-    newh.SetLineWidth(h.GetLineWidth())
-    newh.SetLineStyle(h.GetLineStyle())
-    newh.SetLineColor(h.GetLineColor())
-    return newh
+from testRolling import getbinning,roll1Dto2D,unroll2Dto1D,dressed2D
 
 ROOT.gROOT.SetBatch()
 
-
-
-
 canv = ROOT.TCanvas()
 
-for charge in ['minus']: #, 'plus']:
-    shapesfile = '/afs/cern.ch/user/e/emanuele/w/wmass/heppy/CMSSW_8_0_25/src/CMGTools/WMass/python/plotter/cards/helicity_2018_03_09_testpdfsymm/Wel_{ch}_shapes.root'.format(ch=charge)
-    infile = ROOT.TFile(shapesfile, 'read')
-    keylist = infile.GetListOfKeys()
-    siglist = list( i.GetName() for i in keylist if 'Ybin_' in i.GetName() )
-    nY = max( int(i.split('_')[-2]) for i in siglist if 'pdf' in i)
-    nPDF = max( int(i.split('_')[-1].replace('pdf','').replace('Down','').replace('Up','')) for i in siglist if 'pdf' in i)
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser(usage="%prog [options] shapesdir channel")
+    parser.add_option('-o','--outdir', dest='outdir', default='.', type='string', help='outdput directory to save the matrix')
+    parser.add_option('-s','--syst', dest='systematics', default=None, type='string', help='systematics of which drawing the ratio (comma-separated list of systematics')
+    (options, args) = parser.parse_args()
+    channel = args[1]
 
-    for pol in ['right', 'left']:
-        jobsdir = os.path.dirname(shapesfile)+'/jobs/'
-        jobfile_name = 'W{ch}_el_Ybin_0.sh'.format(ch=charge)
+    systs = [] if not options.systematics else options.systematics.split(',')
+    print "Will consider these possible systematics: ",systs
+
+    outname = options.outdir
+    if not os.path.exists(outname):
+        os.system("mkdir -p "+outname)
+        if os.path.exists("/afs/cern.ch"): os.system("cp /afs/cern.ch/user/g/gpetrucc/php/index.php "+outname)
+
+    for charge in ['minus','plus']:
+        print "===> RUNNING CHARGE = ",charge
+        chs = '+' if charge == 'plus' else '-'
+        shapesfile = "{indir}/W{flav}_{ch}_shapes.root".format(indir=args[0],flav=channel,ch=charge)
+        infile = ROOT.TFile(shapesfile, 'read')
+        keylist = infile.GetListOfKeys()
+        siglist = list( i.GetName() for i in keylist if 'Ybin_' in i.GetName() )
+        nY = max( int(i.split('_')[-2]) for i in siglist if 'pdf' in i)
+        nPDF = max( int(i.split('_')[-1].replace('pdf','').replace('Down','').replace('Up','')) for i in siglist if 'pdf' in i)
+
+        bkgs = ['data_fakes','Flips','DiBosons','Top','TauDecaysW','Z','W%s_long'%charge] # other than W_{L,R}
+        wlr = ['W{ch}_{p}_W{ch}_{flav}_Ybin_{i}'.format(ch=charge,p=pol,flav=channel,i=iy) for pol in ['left','right'] for iy in xrange(nY+1)]
+        procs=wlr+bkgs
+
+        jobsdir = args[0]+'/jobs/'
+        jobfile_name = 'W{ch}_{flav}_Ybin_0.sh'.format(ch=charge,flav=channel)
         tmp_jobfile = open(jobsdir+jobfile_name, 'r')
         tmp_line = tmp_jobfile.readlines()[-1].replace(', ',',').split()
-
         binning = getbinning(tmp_line)
 
-        histo_central = infile.Get('x_W{ch}_{pol}_W{ch}_el_Ybin_0'.format(ch=charge,pol=pol))
-        for iy in range(1,nY+1):
-            histo_central.Add(infile.Get('x_W{ch}_{pol}_W{ch}_el_Ybin_{i}'.format(ch=charge,pol=pol,i=iy)))
-        #for ybin in range(1):nY+1): 
-        ratios = {}
-        for ip in range(1,nPDF+1):
-            histo_pdfi = infile.Get('x_W{ch}_{pol}_W{ch}_el_Ybin_0_pdf{ip}Up'.format(ch=charge,pol=pol,ip=ip))
-            for iy in range(1,nY+1):
-                histo_pdfi.Add(infile.Get('x_W{ch}_{pol}_W{ch}_el_Ybin_{iy}_pdf{ip}Up'.format(ch=charge,pol=pol,iy=iy,ip=ip)))
-            ratio_pdfi = copy.deepcopy(histo_pdfi)
-            ratio_pdfi.Divide(histo_central)
-
-            for ib in range(1, ratio_pdfi.GetNbinsX()+1):
-                ratio_pdfi.SetBinContent(ib, abs(1.-ratio_pdfi.GetBinContent(ib)))
-
-            chs = '+' if charge == 'plus' else '-'
-
-            if len(binning) == 4:
-                n1 = binning[0]; bins1 = array('d', binning[1])
-                n2 = binning[2]; bins2 = array('d', binning[3])
-                h2_1 = ROOT.TH2F('h2_1', 'W{ch} {pol} : pdf {ip}'.format(ip=ip,pol=pol,ch=chs) , n1, bins1, n2, bins2 )
-            else:
-                n1 = binning[0]; min1 = binning[1]; max1 = binning[2]
-                n2 = binning[3]; min2 = binning[4]; max2 = binning[5]
-                h2_1 = ROOT.TH2F('h2_1', 'W{ch} {pol} : pdf {ip}'.format(ip=ip,pol=pol,ch=chs) , n1, min1, max1, n2, min2, max2)
-
-            h2_backrolled_1 = roll1Dto2D(ratio_pdfi, h2_1 )
-            h2_backrolled_1 .GetXaxis().SetTitle('lepton #eta')
-            h2_backrolled_1 .GetYaxis().SetTitle('lepton p_{T} (GeV)')
-            h2_backrolled_1 .GetZaxis().SetRangeUser(0.1*h2_backrolled_1.GetMaximum(),1.1*h2_backrolled_1.GetMaximum())
-
-            h2_backrolled_1.GetZaxis().SetRangeUser(0.,0.02)
-            h2_backrolled_1.Draw('colz')
+        ratios={}
+        if any('pdf' in syst for syst in systs):
+            for pol in ['right', 'left']:
+                print "\tPOLARIZATION ",pol
+                histo_central = infile.Get('x_W{ch}_{pol}_W{ch}_{flav}_Ybin_0'.format(ch=charge,pol=pol,flav=channel))
+                for iy in xrange(1,nY+1):
+                    histo_central.Add(infile.Get('x_W{ch}_{pol}_W{ch}_{flav}_Ybin_{i}'.format(ch=charge,pol=pol,flav=channel,i=iy)))
+                for ip in xrange(1,nPDF+1):
+                    histo_pdfi = infile.Get('x_W{ch}_{pol}_W{ch}_{flav}_Ybin_0_pdf{ip}Up'.format(ch=charge,pol=pol,flav=channel,ip=ip))
+                    for iy in xrange(1,nY+1):
+                        histo_pdfi_iy = infile.Get('x_W{ch}_{pol}_W{ch}_{flav}_Ybin_{iy}_pdf{ip}Up'.format(ch=charge,pol=pol,flav=channel,iy=iy,ip=ip))
+                        if histo_pdfi_iy: histo_pdfi.Add(histo_pdfi_iy)
+                    ratio_pdfi = copy.deepcopy(histo_pdfi)
+                    ratio_pdfi.Divide(histo_central)
+             
+                    for ib in xrange(1, ratio_pdfi.GetNbinsX()+1):
+                        ratio_pdfi.SetBinContent(ib, abs(1.-ratio_pdfi.GetBinContent(ib) if histo_central.GetBinContent(ib)>0 else 0))
+             
+                    title2D = 'W{ch} {pol} : pdf {ip}'.format(ip=ip,pol=pol,ch=chs)
+                    h2_backrolled_1 = dressed2D(ratio_pdfi,binning,title2D)
+                    h2_backrolled_1.GetZaxis().SetRangeUser(0.,0.02)
+                    ratios['syst_W{ch}_{pol}_W{ch}_{flav}_pdf{ip}'.format(ch=charge,pol=pol,flav=channel,ip=ip)] = h2_backrolled_1
+        else:
+            for proc in procs:
+                histo_central = infile.Get('x_%s'%proc)
+                for syst in systs:
+                    # use just Up if you expect the syst ~symmetric
+                    fullsyst = syst if any(sfx in syst for sfx in ['Up','Down']) else syst+"Up"
+                    k_syst = 'x_{proc}_{syst}'.format(proc=proc,syst=fullsyst)
+                    fulllist = list(i.GetName() for i in keylist)
+                    if k_syst in fulllist:
+                        print "Found ",k_syst, " in the histograms..."
+                        h_syst = infile.Get(k_syst)
+                        ratio = copy.deepcopy(h_syst)
+                        ratio.Divide(histo_central)
+                        for ib in xrange(1, ratio.GetNbinsX()+1):
+                            ratio.SetBinContent(ib, abs(1.-ratio.GetBinContent(ib) if histo_central.GetBinContent(ib)>0 else 0))
+                        title2D = '{proc} : variation={syst}'.format(proc=proc,syst=syst)
+                        h2_backrolled_1 = dressed2D(ratio,binning,title2D)
+                        h2_backrolled_1.GetZaxis().SetRangeUser(0.,0.50)
+                        ratios['syst_{proc}_{ch}_{flav}_{syst}'.format(proc=proc,ch=charge,flav=channel,syst=syst)] = h2_backrolled_1
+        print ratios
+        canv = ROOT.TCanvas()
+        for k,r in ratios.iteritems():
+            r.Draw('colz')
             for ext in ['pdf', 'png']:
-                canv.SaveAs('~/www/private/w-helicity-13TeV/pdfRatios/W{ch}_{pol}_W{ch}_el_pdfVariation{ip}.{ext}'.format(ch=charge,pol=pol,ip=ip,ext=ext))
-            
-## canv.Divide(1,2)
-## canv.cd(1)
-## h2.Draw('colz')
-## canv.cd(2)
-## h2backrolled.Draw('colz')
+                canv.SaveAs('{odir}/{name}.{ext}'.format(odir=outname,name=k,ext=ext))
