96081ce84cb6ac1b332f31e2a93f967de12a82c1
mdunser <marc.dunser@cern.ch>
2018-04-23 10:41:25 +0200

    checking in a bunch of muon stuff. and updated angular coeff extraction

--- WMass/python/plotter/w-helicity-13TeV/fractionReweighting/makeFractionsFromAngularCoeffs.py
+++ WMass/python/plotter/w-helicity-13TeV/fractionReweighting/makeFractionsFromAngularCoeffs.py
@@ -1,5 +1,9 @@
 import ROOT, os, copy
 
+
+## USAGE:
+## python makeFractionsFromAngularCoeffs.py -i /afs/cern.ch/user/m/mdunser/www/private/w-helicity-13TeV/angularCoefficients/2018-03-23//moments_muEl/NLO/ -c fractions.root
+
 def formatHisto(hist):
     hist.GetXaxis().SetTitleOffset(1.02)
     hist.GetXaxis().SetTitleSize(0.06)
@@ -14,7 +18,7 @@ def formatHisto(hist):
 
 def makeFraction(a0, a4, pol, ch):
     rethist = a0.Clone('fraction{p}_{ch}_sym'.format(p=pol,ch=ch))
-    rethist.SetTitle('fraction{p}_{ch}_sym_constructed'.format(p=pol,ch=ch))
+    rethist.SetTitle('f_{{{p}}} {ch} - constructed (sym)'.format(p=pol,ch=ch))
     rethist.SetName ('fraction{p}_{ch}_sym_constructed'.format(p=pol,ch=ch))
 
     chmult = 1. if ch == 'minus' else 1.  #### WWWWWAAAAHHHHHH
@@ -85,7 +89,9 @@ for ch in ['plus', 'minus']:
         tmp_a4 = copy.deepcopy(rootfiles['a4'].Get('a4_{ch}_sym'.format(ch=ch)))
 
         tmp_hist = makeFraction(tmp_a0, tmp_a4, pol, ch)
-        fractions['fraction{p}_{ch}_sym'.format(p=pol,ch=ch)] = tmp_hist
+        tmp_hist.GetXaxis().SetTitle('Y_{W}')
+        tmp_hist.GetYaxis().SetTitle('p_{T}^{W}')
+        fractions['fraction{p}_{ch}_sym_constr'.format(p=pol,ch=ch)] = tmp_hist
         tmp_hist.Draw('colz')
         canv.SaveAs(options.indir+'/'+tmp_hist.GetName()+'.pdf')
         canv.SaveAs(options.indir+'/'+tmp_hist.GetName()+'.png')
@@ -93,6 +99,7 @@ for ch in ['plus', 'minus']:
             tmp_compare = copy.deepcopy(comphists['fraction{p}_{ch}_sym'.format(p=pol,ch=ch)])
             tmp_ratio = tmp_hist.Clone('RATIO_fraction{p}_{ch}_sym'.format(p=pol,ch=ch))
             tmp_ratio.Reset()
+            tmp_ratio.SetTitle('f_{{{p}}} {ch} ratio: angular / fit'.format(p=pol,ch=ch))
             for ix in range(1,tmp_compare.GetNbinsX()+1):
                 for iy in range(1,tmp_compare.GetNbinsY()+1):
                     tmp_ratio.SetBinContent(ix, iy, tmp_hist.GetBinContent(ix, iy) / tmp_compare.GetBinContent(ix, iy) )
@@ -103,8 +110,25 @@ for ch in ['plus', 'minus']:
             canv.SaveAs(options.indir+'/'+tmp_ratio.GetName()+'.png')
             fractions['RATIO_fraction{p}_{ch}_sym'.format(p=pol,ch=ch)] = tmp_ratio
 
+    summedFractions = copy.deepcopy(fractions['fractionL_{ch}_sym_constr'.format(ch=ch)]).Clone('fractionSum_{ch}'.format(ch=ch))
+    summedFractions.Reset()
+    summedFractions.SetName('fractionSum_{ch}'.format(ch=ch))
+    summedFractions.SetTitle('#Sigma f_{{i}} {ch}'.format(ch=ch))
+    summedFractions.Add(fractions['fractionL_{ch}_sym_constr'.format(ch=ch)])
+    summedFractions.Add(fractions['fractionR_{ch}_sym_constr'.format(ch=ch)])
+    summedFractions.Add(fractions['fraction0_{ch}_sym_constr'.format(ch=ch)])
+    #summedFractions.SetAxisRange(0.9, 1.1, 'Z')
+    summedFractions.GetZaxis().SetRangeUser(0.9, 1.1)
+    summedFractions.Draw('colz')
+    summedFractions.GetZaxis().SetRangeUser(0.9, 1.1)
+    canv.Update()
+    canv.SaveAs(options.indir+'/'+summedFractions.GetName()+'.pdf')
+    canv.SaveAs(options.indir+'/'+summedFractions.GetName()+'.png')
+    fractions[summedFractions.GetName()] = copy.deepcopy(summedFractions)
+
 
 outfile = ROOT.TFile(options.indir+'/'+'fractions_constructed.root', 'recreate')
+print 'writing outfile', outfile
 for h in fractions.values():
     h.Write()
 outfile.Close()
