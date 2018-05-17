efec23746f171814e7bdf76291631023453f9ea7
Marco Cipriani <cippy91@gmail.com>
2018-05-17 14:47:20 +0200

    skip datacards with 0 observed events

--- WMass/python/plotter/runfitDiffXsec.sh
+++ WMass/python/plotter/runfitDiffXsec.sh
@@ -12,11 +12,16 @@ justPrint="n"
 log="p"  # p, t, s for print on stdout (default), throw away (to /dev/null) or save to a file
 saveOnlyhiggsCombineFile="y"
 
-dirToRunCombineFrom="${plotterDir}diffXsecFit_testScalLikelihood_freezeShapeNuis/"
+dirToRunCombineFrom="${plotterDir}diffXsecFit_testScalLikelihood_freezeShapeNuis_np50/"
 #combineOptions=" --saveFitResult "
-combineOptions=" --algo grid  --points 20 --keepFailures --setParameterRanges \"r=0.9,1.1\" --cminDefaultMinimizerType GSLMultiMin --cminDefaultMinimizerAlgo BFGS2  --freezeNuisanceGroups pdfs,scales,alphaS,wpt,frshape "
-#selectedbins=`seq 1 $nTotBins`
-selectedbins=(20 35 50 65 80 95 125 140 155 170 185 200 215 230 245 260 275 290 305 320 335 350 365 380 395 410 425 440 510 520)
+combineOptions=" --algo grid  --points 50 --keepFailures --setParameterRanges \"r=0.9,1.1\" --cminDefaultMinimizerType GSLMultiMin --cminDefaultMinimizerAlgo BFGS2  --freezeNuisanceGroups pdfs,scales,alphaS,wpt,frshape "
+selectedbins=()
+for i in `seq 1 $nTotBins`
+do
+    selectedbins+=(${i}) 
+done
+#echo "${selectedbins[@]}"
+#selectedbins=(20 35 50 65 80 95 125 140 155 170 185 200 215 230 245 260 275 290 305 320 335 350 365 380 395 410 425 440 510 520)
 #charges=("plus" "minus")
 charges=("plus")
 
@@ -55,7 +60,18 @@ do
 	    outlog=" &> ${dirToRunCombineFrom}combine${outTagName}.log "
 	fi
 
-	runCombine="combine ${cardDir}Wel_${charge}_shapes_addInclW_card_bin${nbin}.txt -M MultiDimFit -t -1 --expectSignal=1 ${combineOptions} -n ${outTagName} -v ${nVerbose} ${outlog}"
+	datacard="${cardDir}Wel_${charge}_shapes_addInclW_card_bin${nbin}.txt"
+	nobs=`cat ${datacard} | grep observation | awk '{ print $2}'`
+	nobsEqual0=`awk 'BEGIN { print ('${nobs}' == 0.0) ? "y" : "n" }'`
+	if [[ "${nobsEqual0}" == "y" ]]; then
+	    echo "###################################################################"
+	    echo "Card for charge = ${charge} and bin = ${nbin} has 0 observed events, so I will just skip it"
+	    echo "I assume this is an empty bin (like the transition region between EB and EE for electrons)"
+	    echo "###################################################################"
+	    continue
+	fi
+
+	runCombine="combine ${datacard} -M MultiDimFit -t -1 --expectSignal=1 ${combineOptions} -n ${outTagName} -v ${nVerbose} ${outlog}"
 
 	fitresName="multidimfit${outTagName}.root"
 
