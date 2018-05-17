2f43e767d73e9bba70b664080035281fcba05148
Giovanni <gpetruc@gmail.com>
2018-05-17 14:58:51 +0200

    Fix Flag_BadChargedCandidateFilter in susyCore_modules_cff

--- TTHAnalysis/python/analyzers/susyCore_modules_cff.py
+++ TTHAnalysis/python/analyzers/susyCore_modules_cff.py
@@ -74,7 +74,7 @@ eventFlagsAna = cfg.Analyzer(
         "eeBadScFilter" : [ "Flag_eeBadScFilter" ],
         "ecalBadCalibFilter" : [ "Flag_ecalBadCalibFilter" ],
         "BadPFMuonFilter" : [ "Flag_BadPFMuonFilter" ],
-        "BadChargedCandidateFilter" : [ "BadChargedCandidateFilter" ],
+        "BadChargedCandidateFilter" : [ "Flag_BadChargedCandidateFilter" ],
         }
     )
 
