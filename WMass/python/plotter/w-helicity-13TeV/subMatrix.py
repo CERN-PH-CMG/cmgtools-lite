df4c9a51b0b4d7d6cf2ab61ba21a67c3d6eac79d
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2018-05-21 18:09:10 +0200

    build the covariance matrix from the toy results. Filtering of bad fits with BGSF2 not yet final

--- WMass/python/plotter/w-helicity-13TeV/subMatrix.py
+++ WMass/python/plotter/w-helicity-13TeV/subMatrix.py
@@ -1,5 +1,5 @@
-import ROOT, os, datetime, re, operator
-
+import ROOT, os, datetime, re, operator, math
+ROOT.gROOT.SetBatch(True)
 
 ## ===================================================================
 ## USAGE:
@@ -37,12 +37,13 @@ if __name__ == "__main__":
     date = datetime.date.today().isoformat()
 
     from optparse import OptionParser
-    parser = OptionParser(usage='%prog [options] ')
-    parser.add_option('-i','--infile', dest='infile', default='', type='string', help='file with fitresult')
+    parser = OptionParser(usage='%prog workspace.root toys.root [options] ')
     parser.add_option('-o','--outdir', dest='outdir', default='', type='string', help='outdput directory to save the matrix')
     parser.add_option('-p','--params', dest='params', default='', type='string', help='parameters for which you want to show the correlation matrix. comma separated list of regexps')
     parser.add_option(     '--suffix', dest='suffix', default='', type='string', help='suffix for the correlation matrix')
     (options, args) = parser.parse_args()
+
+    if len(args)<2: raise RuntimeError, "Need at least workspace.root toys.root to run. Exiting."
 
     if options.outdir:
         ROOT.gROOT.SetBatch()
@@ -50,11 +51,83 @@ if __name__ == "__main__":
             os.system('mkdir -p {od}'.format(od=options.outdir))
         os.system('cp {pf} {od}'.format(pf='/afs/cern.ch/user/g/gpetrucc/php/index.php',od=options.outdir))
 
-    infile = ROOT.TFile(options.infile, 'read')
+    ### GET LIST OF PARAMETERS AND INITIAL VALUES FROM THE WORKSPACE
+    wsfile = ROOT.TFile(args[0], 'read')
+    rws = wsfile.Get('w')
+    pars = ROOT.RooArgList(rws.allVars())
+    params = list(pars.at(i).GetName() for i in xrange(len(pars)))
+    params = filter(lambda x: not x.endswith('_In'),params)
 
-    fitresult = infile.Get('fit_mdf')
+    toysfile = ROOT.TFile(args[1], 'read')
+    toys = toysfile.Get('limit')
+    
+    fitvals = {}
+    fiterrs = {}
+    floatParams = []
+    pois_regexps = list(options.params.split(','))
+    for p in params:
+        bname = 'trackedParam_{par}'.format(par=p)
+        if toys.GetBranch(bname) == None:
+            # print "WARING! Branch for variable ",p," not tracked in the toys!"
+            continue
+        if any([re.match(poi,p) for poi in pois_regexps]):
+            floatParams.append(p)
+            central_val = pars.find(p).getVal()
+            residual_thr = central_val*1E-03 if central_val!=0 else 1E-03 # to remove bad fits with BGSF2
+            toys.Draw('{branch}>>h_{par}'.format(branch=bname,par=p),'abs({branch}-{central})>{thr}'.format(branch=bname,central=central_val,thr=residual_thr))
+            h = ROOT.gROOT.FindObject('h_{par}'.format(par=p)).Clone()
+            fitvals[p] = h.GetMean()
+            fiterrs[p] = h.GetRMS()
+            print "Par: ",p,":\tInitial = ",central_val,"\tFit = ",fitvals[p]," +/- ",fiterrs[p]
 
-    h2_corr = fitresult.correlationHist()
+    print "===> Build covariance matrix from this set of params: ",floatParams
+
+    cov = {}
+    for i in xrange(len(floatParams)):
+        for j in xrange(i,len(floatParams)):
+            x=floatParams[i]; y=floatParams[j]
+            bnamex='trackedParam_'+x; bnamey='trackedParam_'+y
+            if toys.GetBranch(bnamex)==None or toys.GetBranch(bnamey)==None:
+                continue
+            print "Build covariance element: [",x,",",y,"]"
+            central_val_x = pars.find(x).getVal(); central_val_y = pars.find(y).getVal()
+
+            ### this gof criteria may screw up the corr matrix
+            residual_x = 'abs({bnamex}-{cx})'.format(bnamex=bnamex,cx=central_val_x); residual_y = 'abs({bnamey}-{cy})'.format(bnamey=bnamey,cy=central_val_y);
+            residual_thr_x = central_val_x*1E-02 if central_val_x!=0 else 1E-02; residual_thr_y = central_val_y*1E-02 if central_val_y!=0 else 1E-02
+            residual_uthr_x = central_val_x*0.1 if central_val_x!=0 else 3; residual_uthr_y = central_val_y*0.1 if central_val_y!=0 else 3
+            gof = '{resx}>{cutx} && {resy}>{cuty} && {resx}<{ucutx} && {resy}<{ucuty}'.format(cutx=residual_thr_x,resx=residual_x,cuty=residual_thr_y,resy=residual_y,ucutx=residual_uthr_x,ucuty=residual_uthr_y)
+            
+            # this uses the error, but the error comes from MINUIT still, so not reliable to filter fit status
+            # toys.Draw("trackedParamErr_{x}>>h_err_{x}".format(x=x)); herrx=ROOT.gROOT.FindObject('h_err_{x}'.format(x=x)).Clone()
+            # toys.Draw("trackedParamErr_{y}>>h_err_{y}".format(y=y)); herry=ROOT.gROOT.FindObject('h_err_{y}'.format(y=y)).Clone()
+            # herrx.Fit('gaus','Q'); mean_errx, sigma_errx = (herrx.GetFunction("gaus").GetParameter(1), herrx.GetFunction("gaus").GetParameter(2))
+            # herry.Fit('gaus','Q'); mean_erry, sigma_erry = (herry.GetFunction("gaus").GetParameter(1), herry.GetFunction("gaus").GetParameter(2))
+            # print "mean err x = ",mean_errx, " +/- ",sigma_errx
+            # print "mean err y = ",mean_erry, " +/- ",sigma_erry
+            # gof = 'abs(trackedParamErr_{x}-{mean_errx})<3*{sigma_errx} && abs(trackedParamErr_{y}-{mean_erry})<3*{sigma_erry}'.format(x=x,mean_errx=mean_errx,sigma_errx=sigma_errx,   
+            #                                                                                                                          y=y,mean_erry=mean_erry,sigma_erry=sigma_erry)
+
+            var = '({x}-{x0})*({y}-{y0})'.format(x='trackedParam_'+x,x0=fitvals[x],y='trackedParam_'+y,y0=fitvals[y])
+            toys.Draw('{var}>>h_{x}_{y}'.format(var=var,x=x,y=y),gof)
+            h = ROOT.gROOT.FindObject('h_{x}_{y}'.format(x=x,y=y)).Clone()
+            cov[(x,y)] = h.GetMean()
+    for i in xrange(len(floatParams)):
+        for j in xrange(i):
+            x=floatParams[i]; y=floatParams[j]
+            cov[(x,y)]=cov[(y,x)]
+
+    corr = {}
+    for x in floatParams:
+        for y in floatParams:
+            corr[(x,y)] = cov[(x,y)]/math.sqrt(cov[(x,x)])/math.sqrt(cov[(y,y)])
+            print x," - ",y," => cov = ",cov[(x,y)],"; sigma(x) = ",math.sqrt(cov[(x,x)]),"; sigma(y) = ",math.sqrt(cov[(y,y)])
+
+    ## sort the floatParams. alphabetically, except for pdfs, which are sorted by number
+    floatParams = sorted(floatParams, key= lambda x: int(x.split('_')[-1]) if 'norm' in x and '_Ybin_' in x else 0)
+    floatParams = sorted(floatParams, key= lambda x: int(x.replace('pdf','')) if 'pdf' in x else 0)
+            
+    print "sorted params = ",floatParams
 
     c = ROOT.TCanvas("c","",1200,800)
     c.SetGridx()
@@ -66,45 +139,22 @@ if __name__ == "__main__":
     c.SetRightMargin(0.11)
     c.SetBottomMargin(0.15)
 
-    ## use a list here, because dicts are unordered
-    binlabels = []
-    pois_regexps = list(options.params.split(','))
-
-    ## loop on all bin labels and store the name and index if it 
-    ## fits any of the specified regexps
-
-    for i in range(1,h2_corr.GetXaxis().GetNbins()+1):
-        tmp_label = h2_corr.GetXaxis().GetBinLabel(i)
-        for poi in pois_regexps:
-            if re.match(poi, tmp_label): 
-                ## x and y bin labels are different, so we need both
-                ## append a tuple of (label, x-index, y-index) to the list
-                binlabels.append( (tmp_label, i, h2_corr.GetNbinsX()+1-i) )
-
-    ## sort the binlabels. alphabetically, except for pdfs, which are sorted by number
-    binlabels = sorted(binlabels, key= lambda x: int(x[0].split('_')[-1]) if 'norm' in x[0] and '_Ybin_' in x[0] else 0)
-    binlabels = sorted(binlabels, key= lambda x: int(x[0].replace('pdf','')) if 'pdf' in x[0] else 0)
-        
-
     ## make the new, smaller TH2F correlation matrix
-    nbins = len(binlabels)
+    nbins = len(floatParams)
     th2_sub = ROOT.TH2F('sub_corr_matrix', 'small correlation matrix', nbins, 0., nbins, nbins, 0., nbins)
     th2_sub.GetXaxis().SetTickLength(0.)
     th2_sub.GetYaxis().SetTickLength(0.)
     
     ## pretty nested loop. enumerate the tuples
-    for i,(k1,vx1, vy1) in enumerate(binlabels):
-        for j,(k2,vx2,vy2) in enumerate(binlabels):
-
-            ## get the correct content from original histogram
-            tmp_cont = h2_corr.GetBinContent(vx1, vy2)
+    for i,x in enumerate(floatParams):
+        for j,y in enumerate(floatParams):
             ## set it into the new sub-matrix
-            th2_sub.SetBinContent(i+1, j+1, tmp_cont)
+            th2_sub.SetBinContent(i+1, j+1, corr[(x,y)])
             ## set the labels correctly
-            new_k1 = niceName(k1)
-            new_k2 = niceName(k2)
-            th2_sub.GetXaxis().SetBinLabel(i+1, new_k1)
-            th2_sub.GetYaxis().SetBinLabel(j+1, new_k2)
+            new_x = niceName(x)
+            new_y = niceName(y)
+            th2_sub.GetXaxis().SetBinLabel(i+1, new_x)
+            th2_sub.GetYaxis().SetBinLabel(j+1, new_y)
 
     th2_sub.GetZaxis().SetRangeUser(-1, 1)
     th2_sub.Draw('colz text')
