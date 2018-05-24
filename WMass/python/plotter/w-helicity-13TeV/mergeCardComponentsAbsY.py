556e323961c74d1989dc9f339279cbd2ff39fa5c
mdunser <marc.dunser@cern.ch>
2018-05-24 09:40:25 +0200

    making the t2w command work. one mask paramter is saved

--- WMass/python/plotter/w-helicity-13TeV/mergeCardComponentsAbsY.py
+++ WMass/python/plotter/w-helicity-13TeV/mergeCardComponentsAbsY.py
@@ -531,11 +531,24 @@ if __name__ == "__main__":
             signals = ['W{charge}_{pol}_W{charge}_{pol}_{channel}_Ybin_{yb}'.format(charge=charge,pol=pol,channel=channel,yb=yb) for pol in ['left','right'] for yb in xrange(len(ybins[pol])-1) ]
             signals += ['W{charge}_long'.format(charge=charge)]
             multisig = ' '.join(["--PO 'map=.*/{proc}$:r_{proc_nochan}[1,0,10]'".format(proc=proc,proc_nochan=proc.replace('_{channel}_'.format(channel=channel),'_')) for proc in signals])
-            txt2wsCmd = 'text2workspace.py {cf} -o {ws} --X-allow-no-signal --X-no-check-norm -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose {pos}'.format(cf=cardfile, ws=ws, pos=multisig)
+
+            cardfile_xsec = cardfile.replace('_card', '_card_withXsecMask')
+            chname = options.bin+'_{ch}'.format(ch=charge)
+            chname_xsec = chname+'_xsec'
+            ccCmd = 'combineCards.py {oc}={odc} {xc}={xdc} > {out}'.format(oc=chname,odc=cardfile,xc=chname_xsec,xdc=tmp_xsec_dc_name,out=cardfile_xsec)
+
+            newws = cardfile_xsec.replace('_card','_ws').replace('.txt','.root')
+
+            txt2wsCmd = 'text2workspace.py {cf} -o {ws} --X-allow-no-signal --X-no-check-norm -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose {pos} --channel-masks '.format(cf=cardfile, ws=newws, pos=multisig)
             #combineCmd = 'combine {ws} -M MultiDimFit    -t -1 -m 999 --saveFitResult --keepFailures --cminInitialHesse 1 --cminFinalHesse 1 --cminPreFit 1       --redefineSignalPOIs {pois} --floatOtherPOIs=0 -v 9'.format(ws=ws, pois=','.join(['r_'+p for p in signals]))
             combineCmd = 'combine {ws} -M MultiDimFit -t -1 -m 999 --saveFitResult {minOpts} --redefineSignalPOIs {pois} -v 9'.format(ws=ws, pois=','.join(['r_'+p for p in signals]),minOpts=minimizerOpts)
+        ## here running the combine cards command first
+        print ccCmd
+        os.system(ccCmd)
+        ## then running the t2w command afterwards
         print txt2wsCmd
         os.system(txt2wsCmd)
+        ## print out the command to run in combine
         print combineCmd
     # end of loop over charges
 
