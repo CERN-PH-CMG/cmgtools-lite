6991bafe41df3ea5d6d3292dd52fa1d9ecf3dd18
peruzzim <peruzzim@users.noreply.github.com>
2018-06-01 09:14:46 +0200

    cfg update

--- TTHAnalysis/python/plotter/ttH-multilepton/ttH_plots.py
+++ TTHAnalysis/python/plotter/ttH-multilepton/ttH_plots.py
@@ -14,14 +14,8 @@ P0="/afs/cern.ch/work/p/peruzzi/tthtrees"
 if 'cmsco01'   in os.environ['HOSTNAME']: P0="/data/peruzzi"
 if 'cmsphys10' in os.environ['HOSTNAME']: P0="/data1/g/gpetrucc"
 TREES = "--Fs {P}/1_recleaner_180518_v2 --Fs {P}/5_triggerDecision_230418_v1 --Fs {P}/7_tauTightSel_v2 --FMCs {P}/8_vtxWeight2017_v1 --FMCs {P}/6_bTagSF_v2"
-#TREESONLYSKIM = "-P /data/peruzzi/TREES_TTH_190418_Fall17_skim2lss3l --Fs {P}/2_eventVars_230418_v2 --Fs {P}/3_kinMVA_nov8_noMEM_230418_v2 --Fs {P}/10_BDT_rTT_v1_v2"
-TREESONLYSKIM = "-P "+P0+"/TREES_TTH_190418_Fall17_skim2lss3l --Fs {P}/2_eventVars_230418_v2 --Fs {P}/3_kinMVA_nov8_noMEM_230418_v2 "
+TREESONLYSKIM = "-P "+P0+"/TREES_TTH_190418_Fall17_skim2lss3l --Fs {P}/2_eventVars_230418_v2 --Fs {P}/3_kinMVA_noMEM_290518_v2 --Fs {P}/4_BDTv8_Hj_290518_v1 --Fs {P}/4_BDTrTT_Hj_290518_v1 --Fs {P}/4_BDThttTT_Hj_290518_v1"
 TREESONLYFULL = "-P "+P0+"/TREES_TTH_190418_Fall17"
-
-TREESONLYSKIM = TREESONLYSKIM+" --Fs {P}/4_BDTv8_Hj_290518_v1"
-TREESONLYSKIM = TREESONLYSKIM+" --Fs {P}/4_BDTrTT_Hj_290518_v1"
-TREESONLYSKIM = TREESONLYSKIM+" --Fs {P}/4_BDThttTT_Hj_290518_v1"
-
 
 def base(selection):
 
