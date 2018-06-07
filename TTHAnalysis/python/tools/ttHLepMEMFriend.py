201f4911dc86f51dee844be6aba386beb1264b89
Giovanni <gpetruc@gmail.com>
2018-06-07 17:50:26 +0200

    update setup for ttH multilep MEM

--- TTHAnalysis/python/tools/ttHLepMEMFriend.py
+++ TTHAnalysis/python/tools/ttHLepMEMFriend.py
@@ -1,4 +1,19 @@
+##########  Recipe  ####
+#
+#   git clone git@github.com:gpetruc/IPHCNtuple.git IPHCNtuple -b 94x-ichep18
+#   cd IPHCNtuple/
+#   git config core.sparsecheckout true
+#   echo '/MEM/' >> .git/info/sparse-checkout
+#   git read-tree -mu HEAD
+#   cd MEM
+#   ( cd Madgraph && bash SetupMadgraph.sh )
+#   ( cd src && make libMEM.so && cp libMEM.so $CMSSW_BASE/lib/$SCRAM_ARCH -v )
+#
+#####
+
+
 from CMGTools.TTHAnalysis.treeReAnalyzer import *
+import os
 
 def _MEMInit():
     if "libMEM.so" not in ROOT.gSystem.GetLibraries(): 
@@ -8,11 +23,12 @@ def _MEMInit():
         print "Loaded MEM interface"
 
 class ttHLepMEMFriend:
-    def __init__(self, config, blooseWP, recllabel='Recl'):
+    def __init__(self, config, blabel, blooseWP, recllabel='Recl'):
         self._procs = [ "TTLL", "TTHfl", "TTHsl", "TTW", "TTbarfl", "TTbarsl", "TTH" ]#, "TTWJJ", "TTH" ]
         self._posts = [ "", "_kinmaxint"]#, "_nHypAll", "_nNull", "_time", "_err", "_chi2" ]
         self.systsJEC = {0:"", 1:"_jecUp", -1:"_jecDown"} # not really used for the moment
         self.inputlabel = '_'+recllabel
+        self.blabel = blabel
         self.blooseWP = blooseWP
         _MEMInit()
         self.mem = ROOT.MEMFriendTreeCERN();
@@ -45,10 +61,10 @@ class ttHLepMEMFriend:
 
         _var = 0
         jets = [j for j in Collection(event,"JetSel"+self.inputlabel+self.systsJEC[_var],"nJetSel"+self.inputlabel+self.systsJEC[_var])]
-        jets.sort(key = lambda j : j.btagCSV, reverse=True)
+        jets.sort(key = lambda j : getattr(j,self.blabel), reverse=True)
 
         for i,jet in enumerate(jets):
-            print "Jet pt %.7f eta %.7f phi %.7f mass %.7f btag %.3f" % (jet.pt, jet.eta, jet.phi, jet.mass, jet.btagCSV)
+            print "Jet pt %.7f eta %.7f phi %.7f mass %.7f btag %.3f" % (jet.pt, jet.eta, jet.phi, jet.mass, getattr(jet,self.blabel))
 
         if len(jets) < 2: return null_output
 
@@ -58,12 +74,12 @@ class ttHLepMEMFriend:
             print "DEBUG: multilepton->FillParticle(\"lepton\", %d, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (lep.pdgId, lep.p4().X(), lep.p4().Y(), lep.p4().Z(), lep.p4().T())
             self.mem.addLepton(lep.p4(), lep.pdgId)
 
-        bjets = filter(lambda j : j.btagCSV>self.blooseWP, jets)[:2]
+        bjets = filter(lambda j : getattr(j,self.blabel)>self.blooseWP, jets)[:2]
         ljets = filter(lambda j : j not in bjets, jets)
         for j in bjets: 
             #print 'chosen b-jet:',j.pt
-            self.mem.addBJet(j.p4(), j.btagCSV)
-            print "DEBUG: multilepton->FillParticle(\"bjet\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (j.btagCSV, j.p4().X(), j.p4().Y(), j.p4().Z(), j.p4().T())
+            self.mem.addBJet(j.p4(), getattr(j,self.blabel))
+            print "DEBUG: multilepton->FillParticle(\"bjet\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(j,self.blabel), j.p4().X(), j.p4().Y(), j.p4().Z(), j.p4().T())
 
         if len(bjets) >= 1:
             nb = "2b" if (len(bjets) >= 2) else "1b"
@@ -76,16 +92,16 @@ class ttHLepMEMFriend:
                     print 'choosing among %d pairs'%len(pairs)
                     if len(pairs)==0: return
                     pairs.sort(key = lambda (j1,j2,m) : abs(m-80.419))
-                    self.mem.addJet("jetClosestMw", pairs[0][0].p4(), pairs[0][0].btagCSV)
-                    self.mem.addJet("jetClosestMw", pairs[0][1].p4(), pairs[0][1].btagCSV)
-                    print "DEBUG: multilepton->FillParticle(\"jetClosestMw\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (pairs[0][0].btagCSV, pairs[0][0].p4().X(), pairs[0][0].p4().Y(), pairs[0][0].p4().Z(), pairs[0][0].p4().T())
-                    print "DEBUG: multilepton->FillParticle(\"jetClosestMw\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (pairs[0][1].btagCSV, pairs[0][1].p4().X(), pairs[0][1].p4().Y(), pairs[0][1].p4().Z(), pairs[0][1].p4().T())
+                    self.mem.addJet("jetClosestMw", pairs[0][0].p4(), getattr(pairs[0][0],self.blabel))
+                    self.mem.addJet("jetClosestMw", pairs[0][1].p4(), getattr(pairs[0][1],self.blabel))
+                    print "DEBUG: multilepton->FillParticle(\"jetClosestMw\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(pairs[0][0],self.blabel), pairs[0][0].p4().X(), pairs[0][0].p4().Y(), pairs[0][0].p4().Z(), pairs[0][0].p4().T())
+                    print "DEBUG: multilepton->FillParticle(\"jetClosestMw\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(pairs[0][1],self.blabel), pairs[0][1].p4().X(), pairs[0][1].p4().Y(), pairs[0][1].p4().Z(), pairs[0][1].p4().T())
                     #print 'chosen close mw pair:',pairs[0][0].pt,pairs[0][1].pt
                     pairs.sort(key = lambda (j1,j2,m) : m)
-                    self.mem.addJet("jetLowestMjj", pairs[0][0].p4(), pairs[0][0].btagCSV)
-                    self.mem.addJet("jetLowestMjj", pairs[0][1].p4(), pairs[0][1].btagCSV)
-                    print "DEBUG: multilepton->FillParticle(\"jetLowestMjj\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (pairs[0][0].btagCSV, pairs[0][0].p4().X(), pairs[0][0].p4().Y(), pairs[0][0].p4().Z(), pairs[0][0].p4().T())
-                    print "DEBUG: multilepton->FillParticle(\"jetLowestMjj\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (pairs[0][1].btagCSV, pairs[0][1].p4().X(), pairs[0][1].p4().Y(), pairs[0][1].p4().Z(), pairs[0][1].p4().T())
+                    self.mem.addJet("jetLowestMjj", pairs[0][0].p4(), getattr(pairs[0][0],self.blabel))
+                    self.mem.addJet("jetLowestMjj", pairs[0][1].p4(), getattr(pairs[0][1],self.blabel))
+                    print "DEBUG: multilepton->FillParticle(\"jetLowestMjj\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(pairs[0][0],self.blabel), pairs[0][0].p4().X(), pairs[0][0].p4().Y(), pairs[0][0].p4().Z(), pairs[0][0].p4().T())
+                    print "DEBUG: multilepton->FillParticle(\"jetLowestMjj\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(pairs[0][1],self.blabel), pairs[0][1].p4().X(), pairs[0][1].p4().Y(), pairs[0][1].p4().Z(), pairs[0][1].p4().T())
                     #print 'chosen low mjj pair:',pairs[0][0].pt,pairs[0][1].pt
                     return ([pairs[0][0],pairs[0][1]])
 
@@ -95,17 +111,17 @@ class ttHLepMEMFriend:
                 #pairs = [x for x in pairs if (x[0] not in chosen_mw and x[1] not in chosen_mw)]
                 #fillTwoPairs(pairs)
                 ljByPt = ljets[:]; ljByPt.sort(key = lambda j : -j.pt)
-                self.mem.addJet("jetHighestPt", ljByPt[0].p4(), ljByPt[0].btagCSV)
-                self.mem.addJet("jetHighestPt", ljByPt[1].p4(), ljByPt[1].btagCSV)
-                print "DEBUG: multilepton->FillParticle(\"jetHighestPt\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (ljByPt[0].btagCSV, ljByPt[0].p4().X(), ljByPt[0].p4().Y(), ljByPt[0].p4().Z(), ljByPt[0].p4().T())
-                print "DEBUG: multilepton->FillParticle(\"jetHighestPt\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (ljByPt[1].btagCSV, ljByPt[1].p4().X(), ljByPt[1].p4().Y(), ljByPt[1].p4().Z(), ljByPt[1].p4().T())
+                self.mem.addJet("jetHighestPt", ljByPt[0].p4(), getattr(ljByPt[0],self.blabel))
+                self.mem.addJet("jetHighestPt", ljByPt[1].p4(), getattr(ljByPt[1],self.blabel))
+                print "DEBUG: multilepton->FillParticle(\"jetHighestPt\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(ljByPt[0],self.blabel), ljByPt[0].p4().X(), ljByPt[0].p4().Y(), ljByPt[0].p4().Z(), ljByPt[0].p4().T())
+                print "DEBUG: multilepton->FillParticle(\"jetHighestPt\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(ljByPt[1],self.blabel), ljByPt[1].p4().X(), ljByPt[1].p4().Y(), ljByPt[1].p4().Z(), ljByPt[1].p4().T())
             elif len(ljets)==1:
                 print "CATEGORY: ","3l_"+nb+"_1j"
                 ok = self.mem.setCategory("3l_"+nb+"_1j")
                 if not ok: raise RuntimeError, "Hypothesis not found"
                 #print 'chosen highest pt jet:',jets[2].pt
-                self.mem.addJet("jetHighestPt", ljets[0].p4(), ljets[0].btagCSV)
-                print "DEBUG: multilepton->FillParticle(\"jetHighestPt\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (ljets[0].btagCSV, ljets[0].p4().X(), ljets[0].p4().Y(), ljets[0].p4().Z(), ljets[0].p4().T())
+                self.mem.addJet("jetHighestPt", ljets[0].p4(), getattr(ljets[0],self.blabel))
+                print "DEBUG: multilepton->FillParticle(\"jetHighestPt\", 0, %.10g, 0,0,0,0, TLorentzVector(%.10g,%.10g,%.10g,%.10g));" %  (getattr(ljets[0],self.blabel), ljets[0].p4().X(), ljets[0].p4().Y(), ljets[0].p4().Z(), ljets[0].p4().T())
                 #return dict([("MEM_"+p,0) for p in self._procs ]) # FIXME
             elif len(ljets)==0:
                 if (nb == "1b"): return null_output # MEM not implemented for 3l_1b_0j
@@ -151,7 +167,7 @@ if __name__ == '__main__':
     class Tester(Module):
         def __init__(self, name):
             Module.__init__(self,name,None)
-            self.sf = ttHLepMEMFriend(argv[3], blooseWP = 0.5426)
+            self.sf = ttHLepMEMFriend(argv[3], blabel="btagDeepCSV", blooseWP = 0.1522)
             if len(argv) > 4: self.sf.selectEvents( map(int,argv[4:]) )
         def analyze(self,ev):
             self.sf(ev)
@@ -159,6 +175,6 @@ if __name__ == '__main__':
     el.loop([tree], maxEvents = 500)
 
 MODULES = [ 
-    ( 'MEM_3l', lambda : ttHLepMEMFriend("/afs/cern.ch/work/g/gpetrucc/ttH/CMSSW_8_0_25/src/CMGTools/TTHAnalysis/python/tools/ttHLepMEMFriend_memcfg.cfg",
-                                         blooseWP = 0.5426) ),
+    ( 'MEM_3l', lambda : ttHLepMEMFriend("%s/src/CMGTools/TTHAnalysis/python/tools/ttHLepMEMFriend_memcfg.cfg" % os.environ['CMSSW_BASE'],
+                                         blabel="btagDeepCSV", blooseWP = 0.1522) ),
 ]
