aed589ec6f250fe2cee7fc14d3d9b6acaa51b4d1
Marco Cipriani <cippy91@gmail.com>
2018-06-16 22:55:50 +0200

    adding functions and other fixes

--- WMass/python/plotter/utilityMacros/interface/centerOfMassEnergy.h
+++ WMass/python/plotter/utilityMacros/interface/centerOfMassEnergy.h
@@ -3,7 +3,7 @@
 
 static Bool_t use8TeVSample = false;
 static Int_t centerOfMassEnergy = use8TeVSample ? 8 : 13;
-static Double_t intLumi = use8TeVSample ? 19.7 : 35.9; //  19.3 without 2016 G and H
+static Double_t intLumi = use8TeVSample ? 19.7 : 30.9; //  30.9 for electrons, 35.9 for muons (for electrons, 19.3 without 2016 G and H)
 // following are more for the macro that loops on events
 // static Bool_t useTrackMet = true;
 // static Bool_t useAbsIso = false;  // rel iso or abs iso to cut (abs iso is rel iso times lepton pT)          
