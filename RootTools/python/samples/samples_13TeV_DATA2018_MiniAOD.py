602f97ce89e4d38c00adbd986d21c9164417e80c
peruzzim <peruzzim@users.noreply.github.com>
2019-05-15 14:47:25 +0200

    Use 22Jan2019 lumi-recovered versions of 2018D PromptReco datasets

--- RootTools/python/samples/samples_13TeV_DATA2018_MiniAOD.py
+++ RootTools/python/samples/samples_13TeV_DATA2018_MiniAOD.py
@@ -42,8 +42,8 @@ dataSamples_Run2018C_17Sep2018 = [JetHT_Run2018C_17Sep2018, MET_Run2018C_17Sep20
 # ----------------------------- Run2017D PromptReco  ----------------------------------------
 JetHT_Run2018D_PromptReco_v2 = kreator.makeDataComponent("JetHT_Run2018D_PromptReco_v2", "/JetHT/Run2018D-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
 MET_Run2018D_PromptReco_v2 = kreator.makeDataComponent("MET_Run2018D_PromptReco_v2", "/MET/Run2018D-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
-EGamma_Run2018D_PromptReco_v2 = kreator.makeDataComponent("EGamma_Run2018D_PromptReco_v2", "/EGamma/Run2018D-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
-SingleMuon_Run2018D_PromptReco_v2 = kreator.makeDataComponent("SingleMuon_Run2018D_PromptReco_v2", "/SingleMuon/Run2018D-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
+EGamma_Run2018D_PromptReco_v2 = kreator.makeDataComponent("EGamma_Run2018D_PromptReco_v2", "/EGamma/Run2018D-22Jan2019-v2/MINIAOD", "CMS", ".*root", json) # 22Jan2019 to recover lumi lost in /EGamma/Run2018D-22Jan2019-v2/MINIAOD
+SingleMuon_Run2018D_PromptReco_v2 = kreator.makeDataComponent("SingleMuon_Run2018D_PromptReco_v2", "/SingleMuon/Run2018D-22Jan2019-v2/MINIAOD", "CMS", ".*root", json) # 22Jan2019 to recover lumi lost in /SingleMuon/Run2018D-PromptReco-v2/MINIAOD
 DoubleMuon_Run2018D_PromptReco_v2 = kreator.makeDataComponent("DoubleMuon_Run2018D_PromptReco_v2", "/DoubleMuon/Run2018D-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
 MuonEG_Run2018D_PromptReco_v2 = kreator.makeDataComponent("MuonEG_Run2018D_PromptReco_v2", "/MuonEG/Run2018D-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
 Tau_Run2018D_PromptReco_v2 = kreator.makeDataComponent("Tau_Run2018D_PromptReco_v2", "/Tau/Run2018D-PromptReco-v2/MINIAOD", "CMS", ".*root", json)
