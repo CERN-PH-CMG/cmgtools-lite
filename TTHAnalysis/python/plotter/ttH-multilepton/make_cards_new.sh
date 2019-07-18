72cfc0af876cbc6b7a9cbb3084ebef9ffee28726
sesanche <s.scruz.312@gmail.com>
2019-07-18 12:10:37 +0200

    Adding stuff for cards

--- TTHAnalysis/python/plotter/ttH-multilepton/make_cards_new.sh
+++ TTHAnalysis/python/plotter/ttH-multilepton/make_cards_new.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-TREES_ZERO=TREES_TTH_190418_Fall17_skim2lss3l
+TREES_ZERO=NanoTrees_TTH_300519_v5pre_skim2LSS
+
 case $HOSTNAME in
     cmsco01.cern.ch) ORIGIN=/data/peruzzi; J=8 ;;
     cmsphys10) ORIGIN=/data1/g/gpetrucc; J=8 ;;
     gpetrucc-vm2.cern.ch) ORIGIN=/data/gpetrucc; J=4 ;;
+    fanae*) ORIGIN=/pool/cienciasrw/userstorage/sscruz/NanoAOD/; J=88;;
+    gae*) ORIGIN=/pool/cienciasrw/userstorage/sscruz/NanoAOD/; J=88;;
     *)
-        ORIGIN=/afs/cern.ch/work/p/peruzzi; J=4
+        ORIGIN=/afs/cern.ch/work/p/peruzzi; J=1
         test -d /tmp/$USER/TREES_TTH_190418_Fall17_skim2lss3l && ORIGIN=/tmp/$USER;;
 esac;
-
-T2L=" -P $ORIGIN/TREES_TTH_190418_Fall17_skim2lss3l --Fs {P}/1_recleaner_180518_v2 --Fs {P}/5_triggerDecision_230418_v1 --Fs {P}/7_tauTightSel_v2 --FMCs {P}/8_vtxWeight2017_v1 --FMCs {P}/6_bTagSF_v2 --Fs {P}/2_eventVars_230418_v2 --Fs {P}/3_kinMVA_noMEM_200618_v5"
-T3L=" -P $ORIGIN/TREES_TTH_190418_Fall17_skim2lss3l --Fs {P}/1_recleaner_180518_v2 --Fs {P}/5_triggerDecision_230418_v1 --Fs {P}/7_tauTightSel_v2 --FMCs {P}/8_vtxWeight2017_v1 --FMCs {P}/6_bTagSF_v2 --Fs {P}/2_eventVars_230418_v2 --Fs {P}/3_kinMVA_withMEM_200618_v5"
-T4L=${T2L}
 #if test -d $ORIGIN/TREES_TTH_190418_Fall17_skim4l ; then
 #    T4L=${T4L/TREES_TTH_190418_Fall17_skim2lss3l/TREES_TTH_190418_Fall17_skim4l};
 #fi
 
 if [[ "X$1" == "X" ]]; then echo "Provide output directory name!"; exit; fi
 OUTNAME=$1; shift;
-if [[ "X$1" == "X" ]]; then echo "Provide luminosity!"; exit; fi
-LUMI="$1"; shift
+if [[ "X$1" == "X" ]]; then echo "Provide year!"; exit; fi
+YEAR="$1"; shift
+case $YEAR in
+    2016) LUMI="35.9";;
+    2017) LUMI="41.4";;
+    2018) LUMI="59.7";;
+    *) echo "Wrong year" $YEAR;;
+esac
 echo "Normalizing to ${LUMI}/fb";
-OPTIONS=" --tree treeProducerSusyMultilepton --s2v -j $J -l ${LUMI} -f --WA prescaleFromSkim"
+OPTIONS=" --tree NanoAOD --s2v -j $J -l ${LUMI} -f --WA prescaleFromSkim"
 test -d cards/$OUTNAME || mkdir -p cards/$OUTNAME
 OPTIONS="${OPTIONS} --od cards/$OUTNAME ";
+
+T2L="-P $ORIGIN/NanoTrees_TTH_300519_v5pre_skim2LSS/${YEAR} --xf THQ_LHE,THW_LHE,TTWW,TTTW,TTWH -F Friends {P}/3_recleaner_v2/{cname}_Friend.root -F Friends {P}/2_triggerSequence_v2/{cname}_Friend.root --FMCs {P}/4_btag_v2 -F Friends {P}/1_lepJetBTagDeepFlav_v1/{cname}_Friend.root --FMCs {P}/4_leptonSFs_v0 --FMCs {P}/0_mcFlags_v0" #FIXME
+T3L=${T2L}
+T4L=${T2L}
+
 
 SYSTS="--unc ttH-multilepton/systsUnc.txt --amc"
 case $1 in
@@ -35,11 +45,12 @@ esac;
 BLoose=" -E ^BLoose "
 BTight=" -E ^BTight "
 
-#SPLITDECAYS=""
-SPLITDECAYS="-splitdecays"
+SPLITDECAYS=""
+#SPLITDECAYS="-splitdecays"
 
 PROMPTSUB="--plotgroup data_fakes+=.*_promptsub"
-OPTIONS="${OPTIONS} -L ttH-multilepton/functionsTTH.cc --mcc ttH-multilepton/lepchoice-ttH-FO.txt ${PROMPTSUB} --neg" # neg necessary for subsequent rebin
+echo "We are using the asimov dataset"
+OPTIONS="${OPTIONS} -L ttH-multilepton/functionsTTH.cc --mcc ttH-multilepton/lepchoice-ttH-FO.txt ${PROMPTSUB} --neg --asimov signal" # neg necessary for subsequent rebin
 CATPOSTFIX=""
 
 FUNCTION_2L="OurBin2l(kinMVA_2lss_ttbar_withBDTrTT,kinMVA_2lss_ttV_withHj_rTT) [0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5]"
@@ -50,8 +61,7 @@ FUNCTION_SVA_3L="mass_3_cheap(LepGood1_pt,LepGood1_eta,LepGood2_pt,LepGood2_eta,
 ONEBIN="1 1,0.5,1.5"
 
 MCASUFFIX="mcdata-frdata"
-
-SVA="false"
+SVA="true"
 if [[ "$1" == "SVA" ]]; then
 FUNCTION_2L=${FUNCTION_SVA_2L}
 FUNCTION_3L=${FUNCTION_SVA_3L}
@@ -68,9 +78,11 @@ if [[ "$2" == "regularize" ]]; then
 DOFILE="--regularize"
 fi
 
+echo $FUNCTION_2L
+
 if [[ "$1" == "all" || "$1" == "2lss" || "$1" == "2lss_3j" ]]; then
 #    test -d $ORIGIN/TREES_TTH_190418_Fall17_skim2lss_3j_2b1B && T2L="${T2L/TREES_TTH_190418_Fall17_skim2lss3l/TREES_TTH_190418_Fall17_skim2lss_3j_2b1B}";
-    OPT_2L="${T2L} ${OPTIONS} -W vtxWeight2017*eventBTagSF*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_eta[iLepFO_Recl[0]],2)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],LepGood_eta[iLepFO_Recl[1]],2)*triggerSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],nLepTight_Recl,0)"
+    OPT_2L="${T2L} ${OPTIONS} -W puWeight*btagSF_shape*leptonSF_2lss*triggerSF_2lss"
     CATPOSTFIX=""
 
     if [[ "$1" == "2lss_3j" ]]; then
@@ -93,7 +105,7 @@ if [[ "$1" == "all" || "$1" == "2lss" || "$1" == "2lss_3j" ]]; then
 	fi
     fi
 
-    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-2lss-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/2lss_tight.txt ${FUNCTION_2L} $SYSTS $OPT_2L --binname ttH_2lss --categorize $CATFUNC $CATBINS $CATNAMES;
+    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-2lss-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/2lss_tight.txt ${FUNCTION_2L} $SYSTS $OPT_2L --binname ttH_2lss_${YEAR} --categorize $CATFUNC $CATBINS $CATNAMES --year ${YEAR};
 
     echo "Done at $(date)"
 
@@ -101,7 +113,7 @@ fi
 
 if [[ "$1" == "all" || "$1" == "3l" ]]; then
     test -d $ORIGIN/TREES_TTH_190418_Fall17_skim_3l_2j_2b1B_Zveto_presc && T3L="${T3L/TREES_TTH_190418_Fall17_skim2lss3l/TREES_TTH_190418_Fall17_skim_3l_2j_2b1B_Zveto_presc}";
-    OPT_3L="${T3L} ${OPTIONS} -W vtxWeight2017*eventBTagSF*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_eta[iLepFO_Recl[0]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],LepGood_eta[iLepFO_Recl[1]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[2]],LepGood_pt[iLepFO_Recl[2]],LepGood_eta[iLepFO_Recl[2]],3)*triggerSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],nLepTight_Recl,0)"
+    OPT_3L="${T3L} ${OPTIONS} -W puWeight*btagSF_shape*triggerSF_3l*leptonSF_3l"
     CATPOSTFIX=""
 
     echo "3l";
@@ -115,7 +127,7 @@ if [[ "$1" == "all" || "$1" == "3l" ]]; then
     CATNAMES="$(echo {lj,hj}_{neg,pos}${CATPOSTFIX} | sed 's/ /,/g')"
     fi
 
-    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-3l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/3l_tight.txt ${FUNCTION_3L} $SYSTS $OPT_3L --binname ttH_3l --categorize $CATFUNC $CATBINS $CATNAMES;
+    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-3l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/3l_tight.txt ${FUNCTION_3L} $SYSTS $OPT_3L --binname ttH_3l_${YEAR} --categorize $CATFUNC $CATBINS $CATNAMES --year ${YEAR};
 
     echo "Done at $(date)"
 fi
@@ -123,7 +135,7 @@ fi
 if [[ "$1" == "3l_zpeak" || "$1" == "3l_zpeak_btight" ]]; then
     test -d $ORIGIN/TREES_TTH_190418_Fall17_skim_3l_2j_2b1B_Zpeak_presc && T3L="${T3L/TREES_TTH_190418_Fall17_skim2lss3l/TREES_TTH_190418_Fall17_skim_3l_2j_2b1B_Zpeak_presc}";
 
-    OPT_3L="${T3L} ${OPTIONS} -W vtxWeight2017*eventBTagSF*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_eta[iLepFO_Recl[0]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],LepGood_eta[iLepFO_Recl[1]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[2]],LepGood_pt[iLepFO_Recl[2]],LepGood_eta[iLepFO_Recl[2]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[3]],LepGood_pt[iLepFO_Recl[3]],LepGood_eta[iLepFO_Recl[3]],3)*triggerSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],nLepTight_Recl,0)"
+    OPT_3L="${T3L} ${OPTIONS} -W puWeight*btagSF_shape*leptonSF_3l*triggerSF_3l"
     OPT_3L="${OPT_3L} -I ^Zveto"
     CATPOSTFIX="_zpeak"
     echo "3l on-Z";
@@ -143,7 +155,7 @@ if [[ "$1" == "3l_zpeak" || "$1" == "3l_zpeak_btight" ]]; then
     CATBINS="[10.5,11.5,12.5,13.5,14.5]"
     CATNAMES="$(echo {lj,hj}_{neg,pos}${CATPOSTFIX} | sed 's/ /,/g')"
     fi
-    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-3l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/3l_tight.txt ${FUNCTION_3L} $SYSTS $OPT_3L --binname ttH_3l --categorize $CATFUNC $CATBINS $CATNAMES;
+    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-3l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/3l_tight.txt ${FUNCTION_3L} $SYSTS $OPT_3L --binname ttH_3l_${YEAR} --categorize $CATFUNC $CATBINS $CATNAMES --year ${YEAR};
 
     echo "Done at $(date)"
 fi
@@ -151,17 +163,17 @@ fi
 if [[ "$1" == "3l_crwz" ]]; then
     #test -d $ORIGIN/TREES_TTH_190418_Fall17_skim3l_2j_no2b1B_Zpeak_presc && T3L="${T3L/TREES_TTH_190418_Fall17_skim2lss3l/TREES_TTH_190418_Fall17_skim3l_2j_no2b1B_Zpeak_presc}";
     T3L="${T3L/3_kinMVA_withMEM_200618_v5/3_kinMVA_noMEM_200618_v5}" # MEM not needed here
-    OPT_3L="${T3L} ${OPTIONS} -W vtxWeight2017*eventBTagSF*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_eta[iLepFO_Recl[0]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],LepGood_eta[iLepFO_Recl[1]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[2]],LepGood_pt[iLepFO_Recl[2]],LepGood_eta[iLepFO_Recl[2]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[3]],LepGood_pt[iLepFO_Recl[3]],LepGood_eta[iLepFO_Recl[3]],3)*triggerSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],nLepTight_Recl,0)"
+    OPT_3L="${T3L} ${OPTIONS} -W puWeight*btagSF_shape*leptonSF_3l*triggerSF_3l"
     OPT_3L="${OPT_3L} -I ^Zveto -I ^2b1B"
     echo "3l WZ";
 
-    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-3l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/3l_tight.txt ${ONEBIN} $SYSTS $OPT_3L --binname ttH_3l_crwz;
+    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-3l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/3l_tight.txt ${ONEBIN} $SYSTS $OPT_3L --binname ttH_3l_crwz_${YEAR} --year ${YEAR};
 
     echo "Done at $(date)"
 fi
 
 if [[ "$1" == "all" || "$1" == "4l" || "$1" == "4l_crzz"  ]]; then
-    OPT_4L="${T4L} ${OPTIONS} -W vtxWeight2017*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_eta[iLepFO_Recl[0]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],LepGood_eta[iLepFO_Recl[1]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[2]],LepGood_pt[iLepFO_Recl[2]],LepGood_eta[iLepFO_Recl[2]],3)*leptonSF_ttH(LepGood_pdgId[iLepFO_Recl[3]],LepGood_pt[iLepFO_Recl[3]],LepGood_eta[iLepFO_Recl[3]],3)*triggerSF_ttH(LepGood_pdgId[iLepFO_Recl[0]],LepGood_pt[iLepFO_Recl[0]],LepGood_pdgId[iLepFO_Recl[1]],LepGood_pt[iLepFO_Recl[1]],nLepTight_Recl,0)*eventBTagSF"
+    OPT_4L="${T4L} ${OPTIONS} -W puWeight*btagSF_shape*leptonSF_4l*triggerSF_3l"
     CATPOSTFIX=""
 
     if [[ "$1" == "4l_crzz" ]]; then
@@ -170,7 +182,7 @@ if [[ "$1" == "all" || "$1" == "4l" || "$1" == "4l_crzz"  ]]; then
     fi;
 
     echo "4l${CATPOSTFIX}";
-    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-4l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/4l_tight.txt ${ONEBIN} $SYSTS $OPT_4L --binname ttH_4l${CATPOSTFIX};
+    python makeShapeCardsNew.py ${DOFILE} ttH-multilepton/mca-4l-${MCASUFFIX}${SPLITDECAYS}.txt ttH-multilepton/4l_tight.txt ${ONEBIN} $SYSTS $OPT_4L --binname ttH_4l${CATPOSTFIX}_${YEAR} --year ${YEAR};
 
    echo "Done at $(date)"
 fi
