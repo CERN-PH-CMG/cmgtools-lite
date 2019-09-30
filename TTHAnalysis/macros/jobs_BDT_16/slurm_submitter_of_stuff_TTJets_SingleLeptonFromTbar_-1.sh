d270abd4f280ab77bdb9eb2941bcb5cfe125bf9b
helfaham <44893068+helfaham@users.noreply.github.com>
2019-09-30 12:48:50 +0200

    Delete slurm_submitter_of_stuff_TTJets_SingleLeptonFromTbar_-1.sh

--- TTHAnalysis/macros/jobs_BDT_16/slurm_submitter_of_stuff_TTJets_SingleLeptonFromTbar_-1.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-#SBATCH --ntasks=8
-
-srun -N1 -n1 -c1 --exclusive python /home/users/e/l/elfaham/CMSSW_10_4_0/src/CMGTools/TTHAnalysis/macros/prepareEventVariablesFriendTree.py -j 0 -N 500000 -t NanoAOD /nfs/user/pvischia/tth/v5pre/NanoTrees_TTH_300519_v5pre_skim2LSS/2016/ /nfs/user/pvischia/tth/v5pre/NanoTrees_TTH_300519_v5pre_skim2LSS/2016/6_BDThtt -d TTJets_SingleLeptonFromTbar -c -1   -F  Friends /nfs/user/pvischia/tth/v5pre/NanoTrees_TTH_300519_v5pre_skim2LSS/2016/3_recleaner_v2/{cname}_Friend.root   -I  'CMGTools.TTHAnalysis.tools.nanoAOD.ttH_modules' 'BDThttTT_Hj'  --compression 'ZLIB:3'  &
-wait
-
