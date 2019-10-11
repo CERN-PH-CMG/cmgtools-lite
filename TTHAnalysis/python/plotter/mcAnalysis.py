1d4d84419d1c44b300daae5b5e2f58c6e13c113c
sesanche <s.scruz.312@gmail.com>
2019-10-11 22:12:36 +0200

    Changing genEventSumw_ default variable

--- TTHAnalysis/python/plotter/mcAnalysis.py
+++ TTHAnalysis/python/plotter/mcAnalysis.py
@@ -237,7 +237,7 @@ class MCAnalysis:
             cnames = [ x.strip() for x in field[1].split("+") ]
             total_w = 0.; to_norm = False; ttys = [];
             genWeightName = extra["genWeightName"] if "genWeightName" in extra else "genWeight"
-            genSumWeightName = extra["genSumWeightName"] if "genSumWeightName" in extra else "genEventSumw"
+            genSumWeightName = extra["genSumWeightName"] if "genSumWeightName" in extra else "genEventSumw_"
             is_w = -1
             pname0 = pname
             for cname in cnames:
