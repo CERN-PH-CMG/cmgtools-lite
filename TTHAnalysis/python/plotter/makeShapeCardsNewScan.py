316d69a05ab679d5c32622996c897084cebfda3c
cramonal <clara.ramon.alvarez@cern.ch>
2019-10-15 16:49:45 +0200

    fix regexpresion

--- TTHAnalysis/python/plotter/makeShapeCardsNewScan.py
+++ TTHAnalysis/python/plotter/makeShapeCardsNewScan.py
@@ -16,7 +16,7 @@ parser.add_option("--infile", dest="infile", action="store_true", default=False,
 parser.add_option("--savefile", dest="savefile", action="store_true", default=False, help="Save histos to file")
 parser.add_option("--categorize", dest="categ", type="string", nargs=3, default=None, help="Split in categories. Requires 3 arguments: expression, binning, bin labels")
 parser.add_option("--regularize", dest="regularize", action="store_true", default=False, help="Regularize templates")
-parser.add_option("--scanregex", dest="scanregex", type="string", default="ct_(?P<p1>.*?)_cv_(?P<p2>.*?)", help="Regex expression to parse parameters of the scan")
+parser.add_option("--scanregex", dest="scanregex", type="string", default="ct_(?P<p1>.*)_cv_(?P<p2>.*)", help="Regex expression to parse parameters of the scan")                      
 parser.add_option("--params", dest="params", type="string", default="p1,p2", help="List of parameters in the regex, separated by commas")
 (options, args) = parser.parse_args()
 options.weight = True
@@ -34,14 +34,15 @@ outdir  = options.outdir+"/" if options.outdir else ""
 if not os.path.exists(outdir): os.mkdir(outdir)
 
 scanpoints = []
+print(options.params)
 pattern = re.compile( options.scanregex ) 
 for psig in mca.listSignals(True):
     match = pattern.search( psig ) 
     if not match: 
         raise RuntimeError("Signal %s does not match the regexp"%psig)
     point = [ match.group( p ) for p in options.params.split(',') ]
-    if point not in scanpoints: scanpoints.append(  point ) 
-
+    if point not in scanpoints: scanpoints.append( point ) 
+    print(point)
 
 report={}
 if options.infile:
