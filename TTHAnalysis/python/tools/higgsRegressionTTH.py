5e724615abb9b38ce7d0d63690f57a03d7d4ab8f
Pietro Vischia <pietro.vischia@gmail.com>
2019-11-14 13:08:44 +0100

    Added explicit deltaRs to higgsRegression

--- TTHAnalysis/python/tools/higgsRegressionTTH.py
+++ TTHAnalysis/python/tools/higgsRegressionTTH.py
@@ -17,13 +17,24 @@ class HiggsRegressionTTH(Module):
             'Lep0_pt', 'Lep0_eta', 'Lep0_phi', 'Lep0_m',
             'Lep1_pt', 'Lep1_eta', 'Lep1_phi', 'Lep1_m',
             'Lep2_pt', 'Lep2_eta', 'Lep2_phi', 'Lep2_m',
-            'Jet0_pt','Jet0_eta','Jet0_phi','Jet0_m','Jet0_btagdiscr',#'Jet1_mass',
-            'Jet1_pt','Jet1_eta','Jet1_phi','Jet1_m','Jet1_btagdiscr',#'Jet1_mass',
-            'Jet2_pt','Jet2_eta','Jet2_phi','Jet2_m','Jet2_btagdiscr',#'Jet2_mass',
-            'Jet3_pt','Jet3_eta','Jet3_phi','Jet3_m','Jet3_btagdiscr',#'Jet2_mass',
-            'Jet4_pt','Jet4_eta','Jet4_phi','Jet4_m','Jet4_btagdiscr',#'Jet2_mass',
-            'Jet5_pt','Jet5_eta','Jet5_phi','Jet5_m','Jet5_btagdiscr',#'Jet2_mass',
-            'Jet6_pt','Jet6_eta','Jet6_phi','Jet6_m','Jet6_btagdiscr',#'Jet2_mass',
+
+            'DeltaRl0j0', 'DeltaRl0j1', 'DeltaRl0j2', 'DeltaRl0j3', 'DeltaRl0j4', 'DeltaRl0j5', 
+            'DeltaRl1j0', 'DeltaRl1j1', 'DeltaRl1j2', 'DeltaRl1j3', 'DeltaRl1j4', 'DeltaRl1j5', 
+
+            'DeltaRj0j0', 'DeltaRj0j1', 'DeltaRj0j2', 'DeltaRj0j3', 'DeltaRj0j4', 'DeltaRj0j5', 
+            'DeltaRj1j0', 'DeltaRj1j1', 'DeltaRj1j2', 'DeltaRj1j3', 'DeltaRj1j4', 'DeltaRj1j5', 
+            'DeltaRj2j0', 'DeltaRj2j1', 'DeltaRj2j2', 'DeltaRj2j3', 'DeltaRj2j4', 'DeltaRj2j5', 
+            'DeltaRj3j0', 'DeltaRj3j1', 'DeltaRj3j2', 'DeltaRj3j3', 'DeltaRj3j4', 'DeltaRj3j5', 
+            'DeltaRj4j0', 'DeltaRj4j1', 'DeltaRj4j2', 'DeltaRj4j3', 'DeltaRj4j4', 'DeltaRj4j5', 
+            'DeltaRj5j0', 'DeltaRj5j1', 'DeltaRj5j2', 'DeltaRj5j3', 'DeltaRj5j4', 'DeltaRj5j5', 
+
+            'Jet0_pt','Jet0_eta','Jet0_phi','Jet0_m','Jet0_btagdiscr',
+            'Jet1_pt','Jet1_eta','Jet1_phi','Jet1_m','Jet1_btagdiscr',
+            'Jet2_pt','Jet2_eta','Jet2_phi','Jet2_m','Jet2_btagdiscr',
+            'Jet3_pt','Jet3_eta','Jet3_phi','Jet3_m','Jet3_btagdiscr',
+            'Jet4_pt','Jet4_eta','Jet4_phi','Jet4_m','Jet4_btagdiscr',
+            'Jet5_pt','Jet5_eta','Jet5_phi','Jet5_m','Jet5_btagdiscr',
+            'Jet6_pt','Jet6_eta','Jet6_phi','Jet6_m','Jet6_btagdiscr',
             'HadTop_pt','HadTop_eta','HadTop_phi','HadTop_m',
             'TopScore',
             'met','met_phi',
@@ -118,6 +129,9 @@ class HiggsRegressionTTH(Module):
                     ret['Lep%s_m%s'   %(l, self.systsJEC[var])] = lp4.M()
                     evt_tag *= lepsFO[l].pdgId
                 
+                    for j, jp4 in [(ix,x.p4()) for ix,x in enumerate(jets)]:
+                        if len(jets) <6: # fix this
+                          ret['DeltaRl%sj%s]' %(l,j)] = lp4.DeltaR(jp4)
             
             ret["evt_tag%s"     %self.systsJEC[var]] = evt_tag
 
@@ -131,7 +145,10 @@ class HiggsRegressionTTH(Module):
                     ret['Jet%s_m%s'   %(j, self.systsJEC[var])] = jp4.M()
                     ret['Jet%s_btagdiscr%s' %(j, self.systsJEC[var])] = jets[j].btagDeepB
 
-
+                    for jo, jpo4 in [(ixo,xo.p4()) for ixo,xo in enumerate(jets)]:
+                        if len(jets) <6: # fix this
+                          ret['DeltaRj%sj%s]' %(j,jo)] = jp4.DeltaR(jpo4)
+  
             ret["met%s"     %self.systsJEC[var]] = met
             ret["met_phi%s" %self.systsJEC[var]] = met_phi
             ret["HTXS_Higgs_pt%s"%self.systsJEC[var]] = getattr(event,"HTXS_Higgs_pt")
