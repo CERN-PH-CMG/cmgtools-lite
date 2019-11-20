54f35b0a272d13508f11cd11e3f1312d4550e910
Pietro Vischia <pietro.vischia@gmail.com>
2019-11-20 10:50:34 +0100

    Added decay mode

--- TTHAnalysis/python/tools/higgsRecoTTH.py
+++ TTHAnalysis/python/tools/higgsRecoTTH.py
@@ -16,7 +16,11 @@ class HiggsRecoTTH(Module):
         self.label = label
         self.branches = []
         self.systsJEC = {0:"", 1:"_jesTotalCorrUp", -1:"_jesTotalCorrDown"} if doSystJEC else {0:""}
-        for var in self.systsJEC: self.branches.extend(["Hreco_%s%s"%(x,self.systsJEC[var]) for x in ["minDRlj","visHmass","Wmass","lepIdx","j1Idx","j2Idx","pTHvis","matchedpartons","bothmatchedpartons","mismatchedtoptaggedjets","pTHgen","delR_H_partons","delR_H_j1j2","BDThttTT_eventReco_mvaValue","delR_H_q1l", "delR_H_q2l", "delR_H_j1l", "delR_H_j2l"]]) # added new branches here
+        for var in self.systsJEC: self.branches.extend(["Hreco_%s%s"%(x,self.systsJEC[var]) for x in ["minDRlj","visHmass","Wmass","lepIdx","j1Idx","j2Idx","pTHvis",
+                                                                                                      "matchedpartons","bothmatchedpartons","mismatchedtoptaggedjets",
+                                                                                                      "pTHgen","delR_H_partons","delR_H_j1j2","BDThttTT_eventReco_mvaValue",
+                                                                                                      "delR_H_q1l", "delR_H_q2l", "delR_H_j1l", "delR_H_j2l",
+                                                                                                      "nQFromWFromH","nLFromWFromH","nQFromWFromT","nLFromWFromT",]]) # added new branches here
         self.cut_BDT_rTT_score = cut_BDT_rTT_score
         self.cuts_mW_had = cuts_mW_had
         self.cuts_mH_vis = cuts_mH_vis
@@ -92,7 +96,7 @@ class HiggsRecoTTH(Module):
                         if self.debug: print "the mother of this W is a Top"
                         LFromWFromT.append(part)
 
-        print '--------------------------------'
+
         # loop over gen jets 
         # ------------------ 
         #for jet in genjet:
@@ -182,4 +186,9 @@ class HiggsRecoTTH(Module):
             ret["Hreco_delR_H_q2l%s"                  %self.systsJEC[var]] = delR_H_q2l if best else -99
             ret["Hreco_delR_H_j1l%s"                  %self.systsJEC[var]] = delR_H_j1l if best else -99 
             ret["Hreco_delR_H_j2l%s"                  %self.systsJEC[var]] = delR_H_j2l if best else -99 
+            ret['nQFromWFromH%s'                      %self.systsJEC[var]] = len(QFromWFromH)
+            ret['nLFromWFromH%s'                      %self.systsJEC[var]] = len(LFromWFromH)
+            ret['nQFromWFromT%s'                      %self.systsJEC[var]] = len(QFromWFromT)
+            ret['nLFromWFromT%s'                      %self.systsJEC[var]] = len(LFromWFromT)
+
         return ret
