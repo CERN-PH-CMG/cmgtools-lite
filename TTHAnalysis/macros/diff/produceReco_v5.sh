66311e5548dcbf7430167a312282ae80f2a20401
helfaham <hesham.elfaham@gmail.com>
2020-01-28 10:38:49 +0100

    removing empty lines

--- TTHAnalysis/macros/diff/produceReco_v5.sh
+++ TTHAnalysis/macros/diff/produceReco_v5.sh
@@ -22,7 +22,3 @@ done
 #do
 #python ../prepareEventVariablesFriendTree.py -t NanoAOD -d $i /nfs/user/pvischia/tth/v5pre/NanoTrees_TTH_300519_v5pre_skim2LSS/2016/ /nfs/user/elfaham/104X/2016/2lss_diff_NoTop-tagged -I CMGTools.TTHAnalysis.tools.nanoAOD.ttH_modules higgsRecoTTH_v5NoTopTagger -j 4 -F Friends {P}/0_jmeUnc_v1/{cname}_Friend.root -F Friends {P}/3_recleaner_v1/{cname}_Friend.root -F Friends {P}/3_recleaner_v2/{cname}_Friend.root -F Friends {P}/4_btag/{cname}_Friend.root -F Friends {P}/4_btag_v2/{cname}_Friend.root -F Friends {P}/0_mcFlags_v0/{cname}_Friend.root -F Friends {P}/2_triggerSequence_v1/{cname}_Friend.root -F Friends {P}/2_triggerSequence_v2/{cname}_Friend.root -F Friends {P}/6_BDThtt/{cname}_Friend.root -F Friends {P}/1_lepJetBTagDeepFlav_v1/{cname}_Friend.root --env uclouvain -q cp3
 #done
-
-
-
-
