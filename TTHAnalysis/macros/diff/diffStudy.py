a59928de245418dd2c534fa728670ff0406835bc
helfaham <hesham.elfaham@gmail.com>
2020-01-30 14:20:00 +0100

    more updates for diffStudy

--- TTHAnalysis/macros/diff/diffStudy.py
+++ TTHAnalysis/macros/diff/diffStudy.py
@@ -7,7 +7,7 @@ from optparse import OptionParser
 parser = OptionParser(usage="%prog [options]")
 # common options, independent of the flavour chosen
 parser.add_option("-i", "--inputFile", dest="inputFile",  type="string", default="./skimmedTrees_16/2lss_diff_Top-tagged/TTHnobb_fxfx_Friend.root", help="Friend tree with the needed information");
-parser.add_option("-o", "--outputDir", dest="outputDir",  type="string", default="./rootplots/", help="Friend tree with the needed information");
+parser.add_option("-o", "--outputDir", dest="outputDir",  type="string", default="./rootplots", help="Friend tree with the needed information");
 (options, args) = parser.parse_args()
 
 if not os.path.isdir(options.outputDir):
@@ -42,12 +42,26 @@ comparisonplotlist2 = [
 comparisonplotlist3 = [
     ["Hreco_delR_H_j2l" , "Hreco_delR_H_j2l>=0 && Hreco_nmatchedpartons ==1" , "Hreco_delR_H_j2l" ,"Hreco_delR_H_j2l>=0 && Hreco_nmatchedpartons ==2" ,"delR_j2l" , 100, 0., 10.],
 ]
-
 comparisonplotlist4 = [
     ["Hreco_delR_H_q1l" , "Hreco_delR_H_q1l>=0" , "Hreco_delR_H_j1l" ,"Hreco_delR_H_j1l>=0 && Hreco_nmatchedpartons==2 " ,"delR_q1j1l" , 100, 0., 10.],
 ]
-
 comparisonplotlist5 = [
+    ["Hreco_delR_H_q2l" , "Hreco_delR_H_q2l>=0" , "Hreco_delR_H_j2l" ,"Hreco_delR_H_j2l>=0 && Hreco_nmatchedpartons==2 " ,"delR_q2j2l" , 100, 0., 10.],
+]
+## another lists for scatter
+scatterplotlist1 = [
+    ["Hreco_delR_H_j1j2", "Hreco_delR_H_j1j2>=0 && Hreco_nmatchedpartons ==1", "Hreco_delR_H_j1j2","Hreco_delR_H_j1j2>=0 && Hreco_nmatchedpartons ==2","delR_j1j2_diff_cuts", 100, 0., 10.],
+]
+scatterplotlist2 = [
+    ["Hreco_delR_H_j1l" , "Hreco_delR_H_j1l>=0 && Hreco_nmatchedpartons ==1" , "Hreco_delR_H_j1l" ,"Hreco_delR_H_j1l>=0 && Hreco_nmatchedpartons ==2" ,"delR_j1l_diff_cuts" , 100, 0., 10.],
+]
+scatterplotlist3 = [
+    ["Hreco_delR_H_j2l" , "Hreco_delR_H_j2l>=0 && Hreco_nmatchedpartons ==1" , "Hreco_delR_H_j2l" ,"Hreco_delR_H_j2l>=0 && Hreco_nmatchedpartons ==2" ,"delR_j2l_diff_cuts" , 100, 0., 10.],
+]
+scatterplotlist4 = [
+    ["Hreco_delR_H_q1l" , "Hreco_delR_H_q1l>=0" , "Hreco_delR_H_j1l" ,"Hreco_delR_H_j1l>=0 && Hreco_nmatchedpartons==2 " ,"delR_q1j1l" , 100, 0., 10.],
+]
+scatterplotlist5 = [
     ["Hreco_delR_H_q2l" , "Hreco_delR_H_q2l>=0" , "Hreco_delR_H_j2l" ,"Hreco_delR_H_j2l>=0 && Hreco_nmatchedpartons==2 " ,"delR_q2j2l" , 100, 0., 10.],
 ]
 
@@ -59,7 +73,7 @@ def draw_plot(var,cut,fname,nbins,lowbin, highbin):
     theplot.Draw()
     c.Print("%s/%s.png"%(options.outputDir,fname)) 
 
-def draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin):
+def draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin): #TODO the hist title is always the first var--> confusing when comparing two different vars
     c   = TCanvas()
     leg = TLegend(0.5,0.6,0.9,0.9)
     c.cd()
@@ -79,12 +93,14 @@ def draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin):
 
 def draw_scatter(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin):
     c   = TCanvas()
-    leg = TLegend(0.5,0.6,0.9,0.9) 
     c.cd()
     theplot_scat = TH2F(var2,var2, nbins, lowbin, highbin, nbins, lowbin, highbin)
+    theplot_scat.GetXaxis().SetTitle("%s"%var1)
+    theplot_scat.GetYaxis().SetTitle("%s"%var2)
     tr.Draw("%s:%s>>%s"%(var1,var2,var2),cut1)
-    theplot_scat.Draw()
-    c.Print("%s/%s_scat.png"%(options.outputDir,fname))
+    theplot_scat.Draw("COL")
+    theplot_scat.SetTitle("%s_Vs_%s"%(var1,var2))
+    c.Print("%s/%s_2D.png"%(options.outputDir,fname))
 
 
 for var, cut, fname, nbins, lowbin, highbin in plotlist:
@@ -92,16 +108,30 @@ for var, cut, fname, nbins, lowbin, highbin in plotlist:
 
     for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in comparisonplotlist1:
         draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin )
-    draw_scatter(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin)
 
     for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in comparisonplotlist2:
         draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin )
-
+    
     for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in comparisonplotlist3:
         draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin )
-
+    
     for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in comparisonplotlist4:
         draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin )
-
+    
     for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in comparisonplotlist5:
         draw_comparison(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin )
+
+        for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in scatterplotlist1:
+            draw_scatter(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin)
+
+        for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in scatterplotlist2:
+            draw_scatter(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin)
+        
+        for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in scatterplotlist3:
+            draw_scatter(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin)
+        
+        for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in scatterplotlist4:
+            draw_scatter(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin)
+        
+        for var1, cut1, var2, cut2, fname, nbins, lowbin, highbin  in scatterplotlist5:
+            draw_scatter(var1, cut1, var2, cut2, fname, nbins, lowbin, highbin)
