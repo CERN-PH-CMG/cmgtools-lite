3fa157cf75e5786f1f68804497d8f627dbe44703
Pietro Vischia <pietro.vischia@gmail.com>
2020-04-09 13:13:10 +0200

    Rationalized diffCompShape

--- TTHAnalysis/macros/diff/diffCompShape.py
+++ TTHAnalysis/macros/diff/diffCompShape.py
@@ -1,5 +1,6 @@
 import os
 from ROOT import TCanvas, TROOT, TH1D, TH1F, TH2F, TFile, TTree, gROOT, kRed, kGreen, kBlack, kMagenta, kBlue, TLegend, gStyle
+from copy import deepcopy
 
 gROOT.SetBatch(True)
 f1  = TFile("./skimmedTrees_16/2lss_diff_Top-tagged/TTHnobb_fxfx_Friend.root")
@@ -8,135 +9,120 @@ if not f1:
 tr = f1.Get("Friends")
 if not tr:
     raise ValueError('Tree not loaded')
-#comparisonplotlist1 = [
-    #[   "Hreco_pTHvis"                  , "Hreco_pTHvis > 0                                                 && Hreco_nmatchedpartons == 1"         , 
-        #"Hreco_pTHgen"                  , "Hreco_pTHgen > 0             && Hreco_pTHvis             >= 0    && Hreco_nmatchedpartons == 1"         ,
-        #"Hreco_pTVisPlusNu"             , "Hreco_pTVisPlusNu > 0                                            && Hreco_nmatchedpartons == 1"         , 
-        #"Hreco_pTTrueGenPlusNu"         , "Hreco_pTTrueGenPlusNu > 0    && Hreco_pTHvis             >= 0    && Hreco_nmatchedpartons == 1"         ,
-        #"Hreco_pTTrueGen"               , "Hreco_pTTrueGen > 0          && Hreco_pTHvis             >= 0    && Hreco_nmatchedpartons == 1"         ,
-        #"Hreco_pTVis_jets_match"        , "Hreco_pTVis_jets_match > 0   && Hreco_pTVis_jets_match   >= 0    && Hreco_nmatchedpartons == 1"         ,
-        #"pTH"                   ,
-         #200, 0., 400.   ],
-#]
-comparisonplotlist1 = [
-    [   "Hreco_pTHvis"                  , "Hreco_pTHvis > 0                                                 "         , 
-        "Hreco_pTHgen"                  , "Hreco_pTHgen > 0             && Hreco_pTHvis             >= 0    "         ,
-        "Hreco_pTVisPlusNu"             , "Hreco_pTVisPlusNu > 0                                            "         , 
-        "Hreco_pTTrueGenPlusNu"         , "Hreco_pTTrueGenPlusNu > 0    && Hreco_pTHvis             >= 0    "         ,
-        "Hreco_pTTrueGen"               , "Hreco_pTTrueGen > 0          && Hreco_pTHvis             >= 0    "         ,
-        "Hreco_pTVis_jets_match"        , "Hreco_pTVis_jets_match > 0   && Hreco_pTVis_jets_match   >= 0    "         ,
-        "pTH"                   ,
-         200, 0., 400.   ],
-]
-comparisonplotlist2 = [
-    [   "Hreco_pTHvis"          , "Hreco_pTHvis >= 0             && Hreco_pTHvis < 60"                                 ,
-        "Hreco_pTHgen"          , "Hreco_pTHgen >= 0             && Hreco_pTHgen < 60            && Hreco_pTHvis >= 0" ,
-        "Hreco_pTVisPlusNu"     , "Hreco_pTVisPlusNu >= 0        && Hreco_pTVisPlusNu < 60"                            ,
-        "Hreco_pTTrueGenPlusNu" , "Hreco_pTTrueGenPlusNu >= 0    && Hreco_pTTrueGenPlusNu < 60   && Hreco_pTHvis >= 0" ,
-        "Hreco_pTTrueGen"       , "Hreco_pTTrueGen >= 0          && Hreco_pTTrueGen < 60         && Hreco_pTHvis >= 0" ,
-        "pTH_0_60"              ,
-         40, 0., 60. ], 
+colours = {
+    "Hreco_pTHvis"                  : kRed, 
+    "Hreco_pTHgen"                  : kGreen, 
+    "Hreco_pTVisPlusNu"             : kBlack, 
+    "Hreco_pTTrueGenPlusNu"         : kMagenta, 
+    "Hreco_pTTrueGen"               : kBlue, 
+    "Hreco_pTVis_jets_match"        : kRed-5,
+} # :add more if needed
+
+comparisonplotlists = [
+    {
+        'vars' : {
+            "Hreco_pTHvis"                  : [ "Hreco_pTHvis > 0                                                 " ,  "reco"                           ], 
+            "Hreco_pTHgen"                  : [ "Hreco_pTHgen > 0             && Hreco_pTHvis             >= 0    " ,  "gen"                            ],
+            "Hreco_pTVisPlusNu"             : [ "Hreco_pTVisPlusNu > 0                                            " ,  "reco+gen(nu)"                   ], 
+            "Hreco_pTTrueGenPlusNu"         : [ "Hreco_pTTrueGenPlusNu > 0    && Hreco_pTHvis             >= 0    " ,  "gen(q1)+gen(q2)+gen(l)+gen(nu)" ],
+            "Hreco_pTTrueGen"               : [ "Hreco_pTTrueGen > 0          && Hreco_pTHvis             >= 0    " ,  "gen(q1)+gen(q2)+gen(l)"         ],
+            "Hreco_pTVis_jets_match"        : [ "Hreco_pTVis_jets_match > 0   && Hreco_pTVis_jets_match   >= 0    " ,  "jm1+jm2+best_lep"               ],
+        },
+        'pars' : {"pTH"                   :        [200, 0., 400.]}
+    },
+    {
+        'vars' : {
+            "Hreco_pTHvis"          : ["Hreco_pTHvis >= 0             && Hreco_pTHvis < 60"                                , "reco"                          ],
+            "Hreco_pTHgen"          : ["Hreco_pTHgen >= 0             && Hreco_pTHgen < 60            && Hreco_pTHvis >= 0", "gen"                           ],
+            "Hreco_pTVisPlusNu"     : ["Hreco_pTVisPlusNu >= 0        && Hreco_pTVisPlusNu < 60"                           , "reco+gen(nu)"                  ],
+            "Hreco_pTTrueGenPlusNu" : ["Hreco_pTTrueGenPlusNu >= 0    && Hreco_pTTrueGenPlusNu < 60   && Hreco_pTHvis >= 0", "gen(q1)+gen(q2)+gen(l)+gen(nu)"],
+            "Hreco_pTTrueGen"       : ["Hreco_pTTrueGen >= 0          && Hreco_pTTrueGen < 60         && Hreco_pTHvis >= 0", "gen(q1)+gen(q2)+gen(l)"        ],
+        },
+        'pars' : {"pTH_0_60" : [40, 0., 60.] } 
+    },
+    {
+        'vars' : {
+        "Hreco_pTHvis"          : ["Hreco_pTHvis >= 60           && Hreco_pTHvis < 120"                                ,  "reco"                          ],
+            "Hreco_pTHgen"          : ["Hreco_pTHgen >= 60           && Hreco_pTHgen < 120            && Hreco_pTHvis >= 0",  "gen"                           ],
+            "Hreco_pTVisPlusNu"     : ["Hreco_pTVisPlusNu >= 60      && Hreco_pTVisPlusNu < 120"                           ,  "reco+gen(nu)"                  ],
+            "Hreco_pTTrueGenPlusNu" : ["Hreco_pTTrueGenPlusNu >= 60  && Hreco_pTTrueGenPlusNu < 120   && Hreco_pTHvis >= 0",  "gen(q1)+gen(q2)+gen(l)+gen(nu)"],
+            "Hreco_pTTrueGen"       : ["Hreco_pTTrueGen >= 60        && Hreco_pTTrueGen < 120         && Hreco_pTHvis >= 0",  "gen(q1)+gen(q2)+gen(l)"        ],
+        },
+    'pars' : {"pTH_60_120" : [40, 60., 120. ] },
+    },
+    {
+        'vars' : {
+            "Hreco_pTHvis"          : [ "Hreco_pTHvis >= 120          && Hreco_pTHvis < 200"                                 , "reco"                          ],
+            "Hreco_pTHgen"          : [ "Hreco_pTHgen >= 120          && Hreco_pTHgen < 200            && Hreco_pTHvis >= 0" , "gen"                           ],
+            "Hreco_pTVisPlusNu"     : [ "Hreco_pTVisPlusNu  >= 120    && Hreco_pTVisPlusNu < 200"                            , "reco+gen(nu)"                  ],
+        "Hreco_pTTrueGenPlusNu" : [ "Hreco_pTTrueGenPlusNu >= 120 && Hreco_pTTrueGenPlusNu < 200   && Hreco_pTHvis >= 0" , "gen(q1)+gen(q2)+gen(l)+gen(nu)"],
+            "Hreco_pTTrueGen"       : [ "Hreco_pTTrueGen >= 120       && Hreco_pTTrueGen < 200         && Hreco_pTHvis >= 0" , "gen(q1)+gen(q2)+gen(l)"        ],
+        },
+        'pars' : {"pTH_120_200" : [40, 120., 200.] },
+    },
+    {
+        'vars' : {
+            "Hreco_pTHvis"          : [ "Hreco_pTHvis >= 200          && Hreco_pTHvis < 300"                                 , "reco"                          ],
+            "Hreco_pTHgen"          : [ "Hreco_pTHgen >= 200          && Hreco_pTHgen < 300            && Hreco_pTHvis >= 0" , "gen"                           ],
+            "Hreco_pTVisPlusNu"     : [ "Hreco_pTVisPlusNu  >= 200    && Hreco_pTVisPlusNu < 300"                            , "reco+gen(nu)"                  ],
+            "Hreco_pTTrueGenPlusNu" : [ "Hreco_pTTrueGenPlusNu >= 200 && Hreco_pTTrueGenPlusNu < 300   && Hreco_pTHvis >= 0" , "gen(q1)+gen(q2)+gen(l)+gen(nu)"],
+            "Hreco_pTTrueGen"       : [ "Hreco_pTTrueGen >= 200       && Hreco_pTTrueGen < 300         && Hreco_pTHvis >= 0" , "gen(q1)+gen(q2)+gen(l)"        ],
+        },
+        'pars' : { "pTH_200_300" : [40, 200., 300.]},
+    },
+    {
+        'vars' : {
+            "Hreco_pTHvis"          : ["Hreco_pTHvis >= 300          && Hreco_pTHvis < 450"                                 , "reco"                          ], 
+            "Hreco_pTHgen"          : ["Hreco_pTHgen >= 300          && Hreco_pTHgen < 450            && Hreco_pTHvis >= 0" , "gen"                           ],
+            "Hreco_pTVisPlusNu"     : ["Hreco_pTVisPlusNu  >= 300    && Hreco_pTVisPlusNu < 450"                            , "reco+gen(nu)"                  ],
+            "Hreco_pTTrueGenPlusNu" : ["Hreco_pTTrueGenPlusNu >= 300 && Hreco_pTTrueGenPlusNu < 450   && Hreco_pTHvis >= 0" , "gen(q1)+gen(q2)+gen(l)+gen(nu)"],
+            "Hreco_pTTrueGen"       : ["Hreco_pTTrueGen >= 300       && Hreco_pTTrueGen < 450         && Hreco_pTHvis >= 0" , "gen(q1)+gen(q2)+gen(l)"        ],
+        },
+        'pars' : { "pTH_300_450" : [40, 300., 450. ]},
+    }
 ]
 
-comparisonplotlist3 = [
-    [   "Hreco_pTHvis"          , "Hreco_pTHvis >= 60           && Hreco_pTHvis < 120"                                  ,
-        "Hreco_pTHgen"          , "Hreco_pTHgen >= 60           && Hreco_pTHgen < 120            && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTVisPlusNu"     , "Hreco_pTVisPlusNu >= 60      && Hreco_pTVisPlusNu < 120"                             ,
-        "Hreco_pTTrueGenPlusNu" , "Hreco_pTTrueGenPlusNu >= 60  && Hreco_pTTrueGenPlusNu < 120   && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTTrueGen"       , "Hreco_pTTrueGen >= 60        && Hreco_pTTrueGen < 120         && Hreco_pTHvis >= 0"  ,
-        "pTH_60_120"            ,
-         40, 60., 120. ],
-]
+def draw_comparison(args):
+    fname = args['pars'].keys()[0]
+    nbins, lowbin, highbin = args['pars'].values()[0]
 
-comparisonplotlist4 = [
-    [   "Hreco_pTHvis"          , "Hreco_pTHvis >= 120          && Hreco_pTHvis < 200"                                  ,
-        "Hreco_pTHgen"          , "Hreco_pTHgen >= 120          && Hreco_pTHgen < 200            && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTVisPlusNu"     , "Hreco_pTVisPlusNu  >= 120    && Hreco_pTVisPlusNu < 200"                             ,
-        "Hreco_pTTrueGenPlusNu" , "Hreco_pTTrueGenPlusNu >= 120 && Hreco_pTTrueGenPlusNu < 200   && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTTrueGen"       , "Hreco_pTTrueGen >= 120       && Hreco_pTTrueGen < 200         && Hreco_pTHvis >= 0"  ,
-        "pTH_120_200"           ,
-         40, 120., 200. ],
-]
-
-comparisonplotlist5 = [
-    [   "Hreco_pTHvis"          , "Hreco_pTHvis >= 200          && Hreco_pTHvis < 300"                                  ,
-        "Hreco_pTHgen"          , "Hreco_pTHgen >= 200          && Hreco_pTHgen < 300            && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTVisPlusNu"     , "Hreco_pTVisPlusNu  >= 200    && Hreco_pTVisPlusNu < 300"                             ,
-        "Hreco_pTTrueGenPlusNu" , "Hreco_pTTrueGenPlusNu >= 200 && Hreco_pTTrueGenPlusNu < 300   && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTTrueGen"       , "Hreco_pTTrueGen >= 200       && Hreco_pTTrueGen < 300         && Hreco_pTHvis >= 0"  ,
-        "pTH_200_300"           ,
-         40, 200., 300. ],
-]
-
-comparisonplotlist6 = [
-    [   "Hreco_pTHvis"          , "Hreco_pTHvis >= 300          && Hreco_pTHvis < 450"                                  , 
-        "Hreco_pTHgen"          , "Hreco_pTHgen >= 300          && Hreco_pTHgen < 450            && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTVisPlusNu"     , "Hreco_pTVisPlusNu  >= 300    && Hreco_pTVisPlusNu < 450"                             ,
-        "Hreco_pTTrueGenPlusNu" , "Hreco_pTTrueGenPlusNu >= 300 && Hreco_pTTrueGenPlusNu < 450   && Hreco_pTHvis >= 0"  ,
-        "Hreco_pTTrueGen"       , "Hreco_pTTrueGen >= 300       && Hreco_pTTrueGen < 450         && Hreco_pTHvis >= 0"  ,
-        "pTH_300_450"           ,
-         40, 300., 450. ],
-]
-def draw_comparison(var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, var6, cut6, fname, nbins, lowbin, highbin):
     gStyle.SetOptTitle(0)
     gStyle.SetOptStat(0) 
-    c   = TCanvas()
+
+    c   = TCanvas('c', 'c', 800, 800)
     leg = TLegend(0.6,0.7,0.89,0.89)
     c.cd()
-    theplot_1 = TH1F(var1,var1, nbins, lowbin, highbin)
-    tr.Draw("%s>>%s"%(var1,var1),cut1)
-    theplot_2 = TH1F("%s_2"%var2,var2, nbins, lowbin, highbin) # Avoid issues with same names
-    tr.Draw("%s>>%s_2"%(var2,var2),cut2)
-    theplot_3 = TH1F("%s_3"%var3,var3, nbins, lowbin, highbin) # Avoid issues with same name
-    tr.Draw("%s>>%s_3"%(var3,var3),cut3)
-    theplot_4 = TH1F("%s_4"%var4,var4, nbins, lowbin, highbin) # Avoid issues with same name
-    tr.Draw("%s>>%s_4"%(var4,var4),cut4)
-    theplot_5 = TH1F("%s_5"%var5,var5, nbins, lowbin, highbin) # Avoid issues with same name
-    tr.Draw("%s>>%s_5"%(var5,var5),cut5)
-    theplot_6 = TH1F("%s_6"%var6,var6, nbins, lowbin, highbin) # Avoid issues with same name
-    tr.Draw("%s>>%s_6"%(var6,var6),cut6)
-    theplot_2.SetLineColor(kRed)
-    theplot_3.SetLineColor(kGreen)
-    theplot_4.SetLineColor(kBlack)
-    theplot_5.SetLineColor(kMagenta)
-    theplot_6.SetLineColor(kBlue)
-    theplot_1.GetXaxis().SetTitle("pTH(GeV)")
-    theplot_1.GetYaxis().SetTitle("a.u.")
-    theplot_1.Draw("HIST")
-    theplot_1.Scale(1./theplot_1.Integral()) if theplot_1.Integral() != 0 else -99
-    theplot_2.Draw("HIST SAME")
-    theplot_2.Scale(1./theplot_2.Integral()) if theplot_2.Integral() != 0 else -99
-    theplot_3.Draw("HIST SAME")
-    theplot_3.Scale(1./theplot_3.Integral()) if theplot_3.Integral() != 0 else -99
-    theplot_4.Draw("HIST SAME")
-    theplot_4.Scale(1./theplot_4.Integral()) if theplot_4.Integral() != 0 else -99
-    theplot_5.Draw("HIST SAME")
-    theplot_5.Scale(1./theplot_5.Integral()) if theplot_5.Integral() != 0 else -99
-    theplot_6.Draw("HIST SAME")
-    theplot_6.Scale(1./theplot_6.Integral()) if theplot_6.Integral() != 0 else -99
-    leg.AddEntry(theplot_1,"reco")
-    leg.AddEntry(theplot_2,"gen")
-    leg.AddEntry(theplot_3,"reco+gen(nu)")
-    leg.AddEntry(theplot_4,"gen(q1)+gen(q2)+gen(l)+gen(nu)")
-    leg.AddEntry(theplot_5,"gen(q1)+gen(q2)+gen(l)")
-    leg.AddEntry(theplot_6,"jm1+jm2+best_lep")
+    ps = [] # This is needed because ROOT is a mess
+    h=TH1F('h', '', nbins, lowbin, highbin)
+    h.Draw("ICE")
+    ip=0
+    ymax=0
+    for var, vals in args['vars'].items():
+        ps.append(TH1F(var, var, nbins, lowbin, highbin))
+        tr.Draw("%s>>%s"%(var,var),vals[0])
+        ps[ip].GetXaxis().SetTitle("%s [GeV]"%fname)
+        ps[ip].GetYaxis().SetTitle("a.u.")
+        ps[ip].SetLineColor(colours[var])
+        ps[ip].Scale(1./ps[ip].Integral()) if ps[ip].Integral() != 0 else -99
+        if ps[ip].GetMaximum() > ymax:
+            ymax=ps[ip].GetMaximum()
+        leg.AddEntry(ps[ip],vals[1])
+        ip+=1
+    # Find the maximum dynamically
+    for p in ps:
+        p.GetYaxis().SetRangeUser(0, 1.1*ymax)
+        p.Draw("HIST SAME")
+
     leg.Draw()
+    print('ymax', ymax)
+    #c.SetLogy()
+    c.Modified()
+    c.Update()
     c.Print("%s/%s_comp.png"%("./test_compare_stxs/",fname)) # Avoid overwriting single var plots
 
-for var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, var6, cut6, fname, nbins, lowbin, highbin in comparisonplotlist1:
-    draw_comparison(var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, var6, cut6, fname, nbins, lowbin, highbin )
 
-#for var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin  in comparisonplotlist2:
-    #draw_comparison(var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin )
+draw_comparison(comparisonplotlists[0])
 
-#for var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin  in comparisonplotlist3:
-    #draw_comparison(var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin )
+#for l in comparisonplotlists:
+#    draw_comparison(l)
 
-#for var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin  in comparisonplotlist4:
-    #draw_comparison(var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin )
-
-#for var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin  in comparisonplotlist5:
-    #draw_comparison(var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin )
-
-#for var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin  in comparisonplotlist6:
-    #draw_comparison(var1, cut1, var2, cut2, var3, cut3, var4, cut4, var5, cut5, fname, nbins, lowbin, highbin )
