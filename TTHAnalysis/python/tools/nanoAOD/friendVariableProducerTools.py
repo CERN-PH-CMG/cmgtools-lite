26540d87c48c6366a3937e3c4d0a5a8c5e55f2a6
Sergio Sanchez Cruz <sergio.sanchez.cruz@cern.ch>
2022-09-09 11:18:26 +0200

    Rearranging functions

--- TTHAnalysis/python/tools/nanoAOD/friendVariableProducerTools.py
+++ TTHAnalysis/python/tools/nanoAOD/friendVariableProducerTools.py
@@ -1,3 +1,5 @@
+import ROOT 
+from copy import deepcopy
 
 def declareOutput(module, wrappedOutputTree, branchList):
     for b in branchList:
@@ -16,3 +18,12 @@ def writeOutput(module, outputs):
     for k,v in outputs.iteritems():
         wrappedOutputTree.fillBranch(k, v) 
     
+
+def loadHisto(fil, hist):
+    tf = ROOT.TFile.Open(fil)
+    if not tf: raise RuntimeError("No such file %s"%fil)
+    hist = tf.Get(hist)
+    if not hist: raise RuntimeError("No such object %s in %s"%(hist,fil))
+    ret = deepcopy(hist)
+    tf.Close()
+    return ret
